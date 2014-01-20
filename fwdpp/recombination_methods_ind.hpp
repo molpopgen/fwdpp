5a5ac129dd2f3d41b363fb42547498e0ebada3d9
Kevin R. Thornton <krthornt@uci.edu>
2014-01-20 12:26:52 -0800

    Code cleanup.  Removed commented-out lines from previous attempts to get the genetics101 policy working as simply as possible

--- fwdpp/recombination_methods_ind.hpp
+++ fwdpp/recombination_methods_ind.hpp
@@ -54,19 +54,18 @@ Greg
   template<typename gamete_iterator_type,
 	   typename gamete_list_type_allocator,
 	   template<typename,typename> class gamete_list_type,
-	   typename rec_pos_generator>//,
-  //typename gamete_insertion_policy>
+	   typename rec_pos_generator>
   unsigned operator()( gamete_iterator_type & g1,
 		       gamete_iterator_type & g2,
 		       gamete_list_type< typename gamete_iterator_type::value_type, gamete_list_type_allocator > * gametes,
 		       const double & littler,
 		       gsl_rng * r,
 		       const rec_pos_generator & rp) const
-  //const gamete_insertion_policy & gpol ) const
   {
     typedef gamete_list_type< typename gamete_iterator_type::value_type, gamete_list_type_allocator > glist_t;
     unsigned NREC = 0;
-    if( g1 != g2 && gsl_rng_uniform(r) <= 0.5 ) //then a non-parental type is inherited from p1 and p1 has two different gametes
+    if( g1 != g2 && gsl_rng_uniform(r) <= 0.5 )
+      //then a non-parental type is inherited from p1 and p1 has two different gametes
       {
 	NREC += recombine_gametes(r,littler,gametes,g1,g2,rp,
 				  boost::bind(update_if_exists_insert<typename gamete_iterator_type::value_type,glist_t>,_1,gametes));	  
