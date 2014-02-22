2b54fcd07b840e79e0db706b0d5d39f3f78e05a5
Kevin R. Thornton <krthornt@uci.edu>
2014-02-22 09:34:26 -0800

    new files added

---
+++ examples/gsl_rng_test2.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <fwdpp/diploid.hh>
+
+#include <boost/unordered_set.hpp>
+
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+using namespace std;
+
+typedef boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps > lookup_table_type;
+
+int main( int argc, char ** argv )
+{
+  unsigned seed = atoi(argv[1]);
+  unsigned reps = atoi(argv[2]);
+
+  gsl_rng * r =  gsl_rng_alloc(gsl_rng_ranlxs2);
+  gsl_rng_set(r,seed);
+
+  double sum = 0.;
+  for( unsigned i = 0 ; i < reps ; ++i )
+    {
+      lookup_table_type l;
+      long unsigned NCALLS = 0;
+      bool done = false;
+      while(! done )
+	{
+	  double p = gsl_rng_uniform(r);
+	  ++NCALLS;
+	  if( l.find(p) != l.end() )
+	    {
+	      done = true;
+	      break;
+	    }
+	  else
+	    {
+	      l.insert(p);
+	    }
+	}
+      sum += double(NCALLS);
+    }
+  cout << "On average, you get the same value every " << sum/double(reps) << " calls to gsl_rng_uniform\n";
+}
