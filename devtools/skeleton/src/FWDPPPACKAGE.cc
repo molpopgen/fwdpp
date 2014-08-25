0330e872b0afdd6906247f07bd0535d79d63e3ce
Kevin R. Thornton <krthornt@uci.edu>
2014-08-25 07:25:29 -0700

    added code

--- devtools/skeleton/src/FWDPPPACKAGE.cc
+++ devtools/skeleton/src/FWDPPPACKAGE.cc
@@ -1,6 +1,38 @@
-//Single linkage group
+/*
+  Some basic documentation should go here
 
+  This is a MINIMAL .cc file.
+ */
 #include <fwdpp/diploid.hh>
+
+//If you make a custom mutation type,
+//you need to define it above,
+//and adjust this using declaration:
+using mtype = KTfwd::mutation;
+
+//Dealing w/result of configure script
+#ifdef USE_STANDARD_CONTAINERS
+#include <vector>
+#include <list>
+#include <unordered_set>
+#include <functional>
+using mlist = std::container<mtype>;
+using gtype = KTfwd::gamete_base<mtype>;
+using glist = std::list<gtype>;
+using lookup_table_type = std::unordered_set<double,std::hash<double>,KTfwd::equal_eps >;
+#else
+#include <boost/container/vector.hpp>
+#include <boost/container/list.hpp>
+#include <boost/pool/pool_alloc.hpp>
+#include <boost/unordered_set.hpp>
+#include <boost/functional/hash.hpp>
+using mut_allocator =  boost::pool_allocator<mtype>;
+using mlist = boost::container::list<mtype,mut_allocator >;
+using gtype = KTfwd::gamete_base<mtype,mlist>;
+using gam_allocator = boost::pool_allocator<gtype>;
+using glist = boost::container::list<gtype,gam_allocator >;
+using lookup_table_type = boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps >;
+#endif
 
 using namespace std;
 
