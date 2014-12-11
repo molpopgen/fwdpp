f07b2964f4122868dbeadd274f82b2b9465f7b85
Kevin R. Thornton <krthornt@uci.edu>
2014-12-11 10:47:16 -0800

    single and double x overs tested

--- unit/crossoverTest.cc
+++ unit/crossoverTest.cc
@@ -17,7 +17,7 @@ using gtype = KTfwd::gamete;
 
 BOOST_AUTO_TEST_CASE( simple_test_1 )
 {
-  //Neutral mutations at positions 0.1 and 0.9
+  //Neutral mutations at positions 0.1 and 0.9, resp.
   gtype g1(1),g2(1);
   std::list<mut> mlist;
 
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
   /*
     Let's make a single x-over at position 0.5
   */
-  std::vector<double> rec_positions = {0.5};
+  std::vector<double> rec_positions = {0.5, std::numeric_limits<double>::max()};
 
   //We need a "gamete pool"
   std::vector<gtype> gametes = { g1, g2 };
@@ -47,11 +47,21 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
 			     []( std::list<mut>::iterator __m ) {
 			       return __m->pos == 0.1;
 			     } ) != g1_itr->mutations.end() );
+  BOOST_CHECK( std::find_if( g1_itr->mutations.begin(),
+			     g1_itr->mutations.end(),
+			     []( std::list<mut>::iterator __m ) {
+			       return __m->pos == 0.0;
+			     } ) == g1_itr->mutations.end() );
   BOOST_CHECK( std::find_if( g2_itr->mutations.begin(),
 			     g2_itr->mutations.end(),
 			     []( std::list<mut>::iterator __m ) {
 			       return __m->pos == 0.9;
 			     } ) != g2_itr->mutations.end() );
+  BOOST_CHECK( std::find_if( g2_itr->mutations.begin(),
+			     g2_itr->mutations.end(),
+			     []( std::list<mut>::iterator __m ) {
+			       return __m->pos == 0.1;
+			     } ) == g2_itr->mutations.end() );
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 1 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
@@ -64,12 +74,74 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
 					    ng1,ng2 );
 
   /*
-    Now, ng1 must have a mutation at position 0.9,
-    and ng2 must have a mutation at 0.1
+    Now, ng1 must have both mutations,
+    and ng2 must be empty
   */
   BOOST_CHECK( std::find_if( ng1.mutations.begin(),
 			     ng1.mutations.end(),
 			     []( std::list<mut>::iterator __m ) {
 			       return __m->pos == 0.9;
 			     } ) != ng1.mutations.end() );
+
+  BOOST_CHECK( std::find_if( ng1.mutations.begin(),
+			     ng1.mutations.end(),
+			     []( std::list<mut>::iterator __m ) {
+			       return __m->pos == 0.1;
+			     } ) != ng1.mutations.end() );
+
+  BOOST_CHECK( ng2.mutations.empty() );
+}
+
+BOOST_AUTO_TEST_CASE( three_point_cross_1 )
+{
+  //g1: neutral muts at 0.1,0.5
+  //g2: neutral muts at 0.9
+  gtype g1(1),g2(1);
+  std::list<mut> mlist;
+
+  auto mitr = mlist.insert(mlist.end(),mut(0.1,0.,1));
+  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
+mitr = mlist.insert(mlist.end(),mut(0.5,0.,1));
+  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
+  mitr = mlist.insert(mlist.end(),mut(0.9,0.,1));
+  KTfwd::fwdpp_internal::add_new_mutation(mitr,g2);
+
+  BOOST_CHECK_EQUAL( g1.mutations.size(), 2 );
+  BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
+
+  std::vector<double> rec_positions = {0.25, 0.75, std::numeric_limits<double>::max()};
+
+  //We need a "gamete pool"
+  std::vector<gtype> gametes = { g1, g2 };
+
+  //get pointers/iterators to our two existing gametes
+  auto g1_itr = gametes.begin(),
+    g2_itr = g1_itr+1;
+
+  gtype ng1(1),ng2(1);
+  
+  KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
+					    g1_itr, g2_itr,
+					    ng1,ng2 );
+
+
+  /*
+    This was a double x-over.  ng1 must have a mutation at 0.1
+    and ng2 must have the mutations at 0.5 and 0.9
+  */
+  BOOST_CHECK( std::find_if( ng1.mutations.begin(),
+			     ng1.mutations.end(),
+			     []( std::list<mut>::iterator __m ) {
+			       return __m->pos == 0.1;
+			     } ) != ng1.mutations.end() );
+
+  for( auto d : {0.5,0.9} )
+    {
+      BOOST_CHECK( std::find_if( ng2.mutations.begin(),
+				 ng2.mutations.end(),
+				 [d]( std::list<mut>::iterator __m ) {
+				   return __m->pos == d;
+				 } ) != ng2.mutations.end() );
+
+    }
 }
