3cd9a4fd3b1e57c16ea90e7db573baad207f5caf
Kevin R. Thornton <krthornt@uci.edu>
2014-12-10 16:08:05 -0800

    starting unit test development

---
+++ unit/mutateTest.cc
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE mutateTest
+#define BOOST_TEST_DYN_LINK 
+
+#include <fwdpp/diploid.hh>
+#include <fstream>
+#include <boost/test/unit_test.hpp>
+#include <unistd.h>
+#include <iterator>
+
+BOOST_AUTO_TEST_CASE( make_mutation1 )
+{
+
+}
