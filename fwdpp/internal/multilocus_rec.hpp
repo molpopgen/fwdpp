6b67092f47c13ebb528bf3096399625bf80c87f0
molpopgen <krthornt@uci.edu>
2015-01-19 16:24:07 -0800

    cleanup

--- fwdpp/internal/multilocus_rec.hpp
+++ fwdpp/internal/multilocus_rec.hpp
@@ -22,7 +22,6 @@ namespace KTfwd {
 				gamete_itr_t & parental_gamete_2,
 				bool & g1, bool & LO )
     {
-      //unsigned temp = rec_policies[i]( p1c[i].first, p1c[i].second );
       unsigned temp = rec( parental_gamete_1,parental_gamete_2 );
       if ( i > 0 )
 	{
@@ -31,20 +30,8 @@ namespace KTfwd {
 	  g1 = (LO) ? !g1 : g1;
 	  g1 = (obw) ? !g1 : g1;
 	}
-      //(ptr2cdip+i)->first = (p1g1) ? p1c[i].first : p1c[i].second;
-      //offspring_gamete = (g1) ? parental_gamete_1 : parental_gamete_2;
       LO = (temp % 2 != 0.) ? true : false;
       return (g1) ? parental_gamete_1 : parental_gamete_2;
-      // temp = rec_policies[i]( p2c[i].first, p2c[i].second );
-      // if ( i > 0 )
-      //   {
-      //     unsigned nrbw = blrf(r,r_between_loci[i-1]);
-      //     bool obw = (nrbw%2!=0) ? true : false;
-      //     p2g1 = (LO2) ? !p2g1 : p2g1;
-      //     p2g1 = (obw) ? !p2g1 : p2g1;
-      //   }
-      // (ptr2cdip+i)->second = (p2g1) ? p2c[i].first : p2c[i].second;
-      // LO2 = (temp % 2 != 0.) ? true : false;
     }
   }
 }
