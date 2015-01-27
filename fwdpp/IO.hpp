000cfc66876a6fa6b9a23b71633f100d0f97298b
molpopgen <krthornt@uci.edu>
2015-01-27 14:30:18 -0800

    prototypes + dox

--- fwdpp/IO.hpp
+++ fwdpp/IO.hpp
@@ -10,7 +10,7 @@ namespace KTfwd
     
     \param gametes  The vector of gametes
     \param mutations The list of mutations
-    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be prov
+    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be provided by the library user.
     \param buffer An ouptut stream into which the population is written.  This is the "return value" of the function.  The stream must support a write() function
     akin to those found in the std::ostream classes.
     
@@ -37,7 +37,7 @@ namespace KTfwd
 
     \param gametes  The vector of vector gametes for each deme in metapop
     \param mutations The list of mutations
-    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be prov
+    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be provided by the library user.
     \param buffer An ouptut stream into which the population is written.  This is the "return value" of the function.  The stream must support a write() function
     akin to those found in the std::ostream classes.
 
@@ -112,7 +112,7 @@ namespace KTfwd
     \param gametes  The vector of lists gametes for each deme in metapop
     \param mutations The list of mutations
     \param diploids The vector of diploids
-    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be prov
+    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be provided by the library user.
     \param buffer An ouptut stream into which the population is written.  This is the "return value" of the function.  The stream must support a write() function
     akin to those found in the std::ostream classes.
 
@@ -136,6 +136,8 @@ namespace KTfwd
 			  vector_type_allocator > * diploids,
 			  const mutation_writer_type & mw,
 			  ostreamtype & buffer);
+
+
 
   /*! \brief Read the population back from a binary-format file for individual-based simulations
 
@@ -163,13 +165,73 @@ namespace KTfwd
 			  const mutation_reader_type & mr,
 			  istreamtype & in);
 
+  /*! \brief Write the population to a binary-format file for individual-based multilocus simulations.
+    \param mlocus_gametes A container of gametes for a multilocus simulation
+    \param mutation A linked list of mutation objects
+    \param diploids A container of individuals in the simulation
+    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be provided by the library user.
+    \param ostreamtype An object whose public interface is compatible with std::ostream
+   */
+  template< typename gamete_type,
+	    typename gamete_list_type_allocator,
+	    template<typename,typename> class gamete_list_type,
+	    typename mlocus_vector_type_allocator,
+	    template<typename,typename> class mlocus_vector_type,
+	    typename mutation_type,
+	    typename mutation_list_type_allocator,
+	    template<typename,typename> class mutation_list_type,
+	    typename vector_type_allocator,
+	    template<typename,typename> class diploid_vector_type,
+	    typename diploid_vv_type_allocator,
+	    template<typename,typename> class diploid_vv_type,
+	    typename mutation_writer_type,
+	    typename ostreamtype>
+  void write_binary_pop ( const mlocus_vector_type< gamete_list_type< gamete_type, gamete_list_type_allocator >, mlocus_vector_type_allocator> * mlocus_gametes,
+			  const mutation_list_type< mutation_type, mutation_list_type_allocator > * mutations,
+			  const diploid_vv_type < diploid_vector_type< std::pair< typename gamete_list_type< gamete_type, gamete_list_type_allocator >::iterator,
+			  typename gamete_list_type< gamete_type, gamete_list_type_allocator >::iterator >,
+			  vector_type_allocator >,
+			  diploid_vv_type_allocator > * diploids,
+			  const mutation_writer_type & mw,
+			  ostreamtype & buffer);
+
+  /*! \brief Read the population back from a binary-format file for individual-based multilocus simulations
+    \param mlocus_gametes A container of gametes for a multilocus simulation
+    \param mutation A linked list of mutation objects
+    \param diploids A container of individuals in the simulation
+    \param mr A function object taking a input stream as argument, and reads a mutation object from the stream. Must be provided by the library user.
+    \param istreamtype An object whose public interface is compatible with std::ostream
+   */
+  template< typename gamete_type,
+	    typename gamete_list_type_allocator,
+	    template<typename,typename> class gamete_list_type,
+	    typename mlocus_vector_type_allocator,
+	    template<typename,typename> class mlocus_vector_type,
+	    typename mutation_type,
+	    typename mutation_list_type_allocator,
+	    template<typename,typename> class mutation_list_type,
+	    typename vector_type_allocator,
+	    template<typename,typename> class diploid_vector_type,
+	    typename diploid_vv_type_allocator,
+	    template<typename,typename> class diploid_vv_type,
+	    typename mutation_reader_type,
+	    typename istreamtype>
+  void read_binary_pop ( mlocus_vector_type< gamete_list_type< gamete_type, gamete_list_type_allocator >, mlocus_vector_type_allocator> * mlocus_gametes,
+			 mutation_list_type< mutation_type, mutation_list_type_allocator > * mutations,
+			 diploid_vv_type < diploid_vector_type< std::pair< typename gamete_list_type< gamete_type, gamete_list_type_allocator >::iterator,
+			 typename gamete_list_type< gamete_type, gamete_list_type_allocator >::iterator >,
+			 vector_type_allocator >,
+			 diploid_vv_type_allocator > * diploids,
+			 const mutation_reader_type & mr,
+			 istreamtype & in);
+
   /*! \brief Write the metapopulation to a compact binary-format output file for individual-based simulations.
     Write the metapopulation to a compact binary-format output file.
     
     \param metapop  The vector of lists gametes for each deme in metapop
     \param mutations The list of mutations
     \param diploids The vector of vectors of diploids.
-    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be prov
+    \param mw A function object taking a mutation and an ostreamtype as arguments. Must be provided by the library user.
     \param buffer An ouptut stream into which the population is written.  This is the "return value" of the function.  The stream must support a write() function
     akin to those found in the std::ostream classes.
 
