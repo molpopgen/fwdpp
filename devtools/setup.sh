14c5c9627602f257e15e8a6a1554acb697fe1aaf
molpopgen <krthornt@uci.edu>
2015-01-30 09:17:35 -0800

    making the setup.sh script more user-friendly

--- devtools/setup.sh
+++ devtools/setup.sh
@@ -1,7 +1,26 @@
-#!sh
+#!/usr/bin/env bash
 
-DIR=$1
-PROJECT=$2
+usage(){
+    >&2 echo "Usage: $0 options"
+    >&2 echo "Mandatory options are:"
+    >&2 echo " -d/--dir root path to place the project (DIR)"
+    >&2 echo " -p/--project Name for project directory (PROJECT)"
+    >&2 echo "Together, the -p and -d options will create a skeleton package in DIR/PROJECT."
+    >&2 echo "For example: ./setup.sh -p ~/src -d my_fwdpp_project"
+    exit 1
+}
+while true; do
+    case "$1" in
+	-d | --dir ) DIR="$2"; shift 2;;
+	-p | --project ) PROJECT="$2"; shift 2;;
+        -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+##VALIDATE THE INPUT PARAMS
+if [ -z ${DIR+x} ]; then >&2 echo "Error: no base path name specified"; usage; else echo "Base path set to '$DIR'"; fi
+if [ -z ${PROJECT+x} ]; then >&2 echo "Error: project name specified"; usage; else echo "Project name set to '$PROJECT'"; fi
 
 if [ ! -d $DIR ]
 then
