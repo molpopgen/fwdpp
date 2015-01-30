6664920c5e7322ba18f7887afef538c5517c35d3
molpopgen <krthornt@uci.edu>
2015-01-30 11:11:12 -0800

    sed magic enabled.  Allow user to specify url

--- devtools/setup.sh
+++ devtools/setup.sh
@@ -7,12 +7,15 @@ usage(){
     >&2 echo " -p/--project Name for project directory (PROJECT)"
     >&2 echo "Together, the -p and -d options will create a skeleton package in DIR/PROJECT."
     >&2 echo "For example: ./setup.sh -p ~/src -d my_fwdpp_project"
+    >&2 echo "Optional options are:"
+    >&2 echo " -u/--url project url.  This must be single-quoted with special characters escaped, e.g. -u 'https:\/\/github.com\/molpopgen\/fwdpp'"
     exit 1
 }
 while true; do
     case "$1" in
 	-d | --dir ) DIR="$2"; shift 2;;
 	-p | --project ) PROJECT="$2"; shift 2;;
+	-u | --url ) URL="$2"; shift 2;;
         -- ) shift; break ;;
     * ) break ;;
   esac
@@ -43,4 +46,6 @@ fi
 
 cp -r skeleton/* $DIR/$PROJECT
 
-cd $DIR/$PROJECT
+mv $DIR/$PROJECT/src/FWDPPPACKAGE.cc $DIR/$PROJECT/src/$PROJECT.cc
+sed -i '' "s/FWDPPPACKAGE/$PROJECT/" $DIR/$PROJECT/configure.ac
+sed -i '' "s/FWDPPPROJECTURL/$URL/" $DIR/$PROJECT/configure.ac
