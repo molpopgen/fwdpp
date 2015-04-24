2e0aef38933203598c8380374fe11f493401aae2
molpopgen <krthornt@uci.edu>
2015-04-24 07:42:05 -0400

    added doxygen tags

--- fwdpp/sugar/GSLrng_t.hpp
+++ fwdpp/sugar/GSLrng_t.hpp
@@ -1,3 +1,7 @@
+/*! \file GSLrng_t.hpp
+  \brief Wrapper for gsl_rng *
+  \ingroup sugar
+ */
 #ifndef __FWDPP_SUGAR_GSLRNG_T_HPP__
 #define __FWDPP_SUGAR_GSLRNG_T_HPP__
 
@@ -6,9 +10,22 @@
 
 namespace KTfwd {
 
+  //! Distpatch tag to signal GSLrng_t to instantiate in terms of gsl_rng_mt19937
   using GSL_RNG_MT19937 = sugar::GSL_RNG_TYPE_TAG<sugar::GSL_RNG_TYPE::MT19937>;
+    //! Distpatch tag to signal GSLrng_t to instantiate in terms of gsl_rng_taus2
   using GSL_RNG_TAUS2 = sugar::GSL_RNG_TYPE_TAG<sugar::GSL_RNG_TYPE::TAUS2>;
-  
+
+  /*!
+    A wrapper around gsl_rng * objects.
+
+    The template instantiation type must be a model of 
+    KTfwd::sugar::GSL_RNG_TYPE_TAG, which specifies the
+    gsl_rng type.
+
+    This type holds an object of type KTfwd::sugar::gsl_rng_ptr_t,
+    which is a smart pointer that manages freeing the gsl_rng * upon
+    destruction.
+   */
   template<typename T>
   class GSLrng_t {
   private:
@@ -23,12 +40,16 @@ namespace KTfwd {
       return sugar::gsl_rng_ptr_t(gsl_rng_alloc(gsl_rng_mt19937));
     }
   public:
+    //! Smart pointer wrapping the gsl_rng *
     sugar::gsl_rng_ptr_t r;
 
+    /*!
+      Construction requires a seed
+     */
     GSLrng_t(const unsigned & seed) : r(setup(T())) {
       gsl_rng_set(r.get(),seed);
     }
-    /*
+    /*!
       boost python requires that things be copy-constructable
       However, the gsl_rng_ptr_t is not, so we gotta write
       a manual copy-constructor
@@ -36,7 +57,7 @@ namespace KTfwd {
     GSLrng_t( const GSLrng_t & __rng) : r(gsl_rng_clone(__rng.r.get()))
     {
     }
-    //Allow explicit conversiont
+    //!Allow explicit conversion to gsl_rng *
     operator gsl_rng *() const { return r.get(); }
   };
 }
