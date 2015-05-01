7a27e03356378fb7ae878ad24cfa464b7a4f88bb
Kevin R. Thornton <krthornt@uci.edu>
2015-05-01 15:11:57 -0700

    documented

--- fwdpp/sugar/infsites.hpp
+++ fwdpp/sugar/infsites.hpp
@@ -10,6 +10,17 @@ namespace KTfwd
 {
   struct infsites
   {
+    /*!
+      \param r A gsl_rng *
+      \param generation Generation when this mutation is happening
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param posmaker A policy that returns the position of the new mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
+     */
     template<typename mlist_t,
 	     typename lookup_table_t,
 	     typename position_t,
@@ -41,8 +52,19 @@ namespace KTfwd
     }
 
     /*!
-      Overload for different position generators for neutral and non-neutral variants
-    */
+      \brief Overload for different position distributions for neutral and non-neutral variants
+
+      \param r A gsl_rng *
+      \param generation Generation when this mutation is happening
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param nposmaker A policy that returns the position of new neutral mutation
+      \param sposmaker A policy that returns the position of new non-neutral mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
+     */
     template<typename mlist_t,
 	     typename lookup_table_t,
 	     typename nposition_t,
@@ -81,6 +103,17 @@ namespace KTfwd
       return typename mlist_t::value_type(pos,0.,0.,generation,1);
     }
 
+    /*!
+      \param r A gsl_rng *
+      \param generation Generation when this mutation is happening
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param posmaker A policy that returns the position of the new mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
+     */
     template<typename mlist_t,
 	     typename lookup_table_t,
 	     typename position_t,
@@ -141,7 +174,18 @@ namespace KTfwd
     }
 
     /*!
-      Overload for different position distributions for neutral and non-neutral variants
+      \brief Overload for different position distributions for neutral and non-neutral variants
+
+      \param r A gsl_rng *
+      \param generation Generation when this mutation is happening
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param nposmaker A policy that returns the position of new neutral mutation
+      \param sposmaker A policy that returns the position of new non-neutral mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
     */
     template<typename mlist_t,
 	     typename lookup_table_t,
