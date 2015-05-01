229c37a16573b9306e6ee87b82eb48fc7119a941
Kevin R. Thornton <krthornt@uci.edu>
2015-05-01 15:18:30 -0700

    added to group

--- fwdpp/sugar/poptypes/tags.hpp
+++ fwdpp/sugar/poptypes/tags.hpp
@@ -5,15 +5,15 @@
 
 namespace KTfwd {
   namespace sugar {
-    //! Types of populations supported by sugar layer
+    //! Types of populations supported by sugar layer \ingroup sugar
     enum class FWDPP_SUGAR_POPTYPE { SINGLE, META, MULTILOC };
-    //! Dispatch tag template for population types supported by sugar layer
+    //! Dispatch tag template for population types supported by sugar layer \ingroup sugar
     template<FWDPP_SUGAR_POPTYPE> struct FWDPP_SUGAR_POPTAG {};
-    //! Single-population simulations
+    //! Single-population simulations \ingroup sugar
     using SINGLEPOP_TAG = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::SINGLE>;
-    //! Meta-population simulations
+    //! Meta-population simulations \ingroup sugar
     using METAPOP_TAG = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::META>;
-    //! Single-population, multi-locus simulations
+    //! Single-population, multi-locus simulations \ingroup sugar
     using MULTILOCPOP_TAG = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::MULTILOC>;
   }
 }
