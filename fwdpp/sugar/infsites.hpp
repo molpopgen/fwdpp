e347cc519ed3a26af7ce8733c0446ddb2bfa3a5f
molpopgen <krthornt@uci.edu>
2015-05-02 12:15:54 -0700

    missing documentation addded

--- fwdpp/sugar/infsites.hpp
+++ fwdpp/sugar/infsites.hpp
@@ -18,6 +18,8 @@ namespace KTfwd
   {
     /*!
       \param r A gsl_rng *
+      \param mutations The (doubly-linked) list of mutations in the population
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
       \param generation Generation when this mutation is happening
       \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
       \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
@@ -64,6 +66,8 @@ namespace KTfwd
       \brief Overload for different position distributions for neutral and non-neutral variants
 
       \param r A gsl_rng *
+      \param mutations The (doubly-linked) list of mutations in the population
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
       \param generation Generation when this mutation is happening
       \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
       \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
@@ -117,6 +121,8 @@ namespace KTfwd
 
     /*!
       \param r A gsl_rng *
+      \param mutations The (doubly-linked) list of mutations in the population
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
       \param generation Generation when this mutation is happening
       \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
       \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
@@ -159,6 +165,18 @@ namespace KTfwd
       return typename mlist_t::value_type(pos,0.,0.,*generation,1);
     }
 
+    /*!
+      \param r A gsl_rng *
+      \param mutations The (doubly-linked) list of mutations in the population
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param posmaker A policy that returns the position of the new mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
+     */
     template<typename mlist_t,
 	     typename lookup_table_t,
 	     typename position_t,
@@ -195,7 +213,8 @@ namespace KTfwd
       \brief Overload for different position distributions for neutral and non-neutral variants
 
       \param r A gsl_rng *
-      \param generation Generation when this mutation is happening
+      \param mutations The (doubly-linked) list of mutations in the population
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
       \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
       \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
       \param nposmaker A policy that returns the position of new neutral mutation
