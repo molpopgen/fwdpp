abf0a82e2c2a3f4ef550dc2e73087ba8bb8a48c2
Kevin R. Thornton <krthornt@uci.edu>
2015-05-11 09:35:29 -0700

    support for custom diploids.  fewer typedefs cluttering namespace

--- fwdpp/sugar/multiloc.hpp
+++ fwdpp/sugar/multiloc.hpp
@@ -16,24 +16,23 @@ namespace KTfwd
   template<typename mtype> using multiloc_mlist_t = boost::container::list<mtype,boost::pool_allocator<mtype> >;
   template<typename mtype> using multiloc_gamete_t = gamete_base<mtype,multiloc_mlist_t<mtype>>;
   template<typename mtype> using multiloc_glist_t = boost::container::list<multiloc_gamete_t<mtype>, boost::pool_allocator<multiloc_gamete_t<mtype>>>;
-  template<typename mtype> using multiloc_dipvector_t = boost::container::vector< boost::container::vector<
-										    std::pair<typename multiloc_glist_t<mtype>::iterator,
-											      typename multiloc_glist_t<mtype>::iterator> >
-										  >;
 
   /*!
     \brief Single population, multilocus simulation without serialization.  Cannot be copied, etc.
     See @ref md_md_sugar for rationale, etc.
     \ingroup sugar
   */
-  template<typename mtype> using multiloc = sugar::multiloc<mtype,
-							    multiloc_mlist_t<mtype>,
-							    multiloc_glist_t<mtype>,
-							    multiloc_dipvector_t<mtype>,
-							    boost::container::vector<multiloc_glist_t<mtype>>,
-							    boost::container::vector<mtype>,
-							    boost::container::vector<unsigned>,
-							    boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>>;
+  template<typename mtype,
+	   typename diploid_t = std::pair<typename multiloc_glist_t<mtype>::iterator,
+					  typename multiloc_glist_t<mtype>::iterator> >
+  using multiloc = sugar::multiloc<mtype,
+				   multiloc_mlist_t<mtype>,
+				   multiloc_glist_t<mtype>,
+				   boost::container::vector<boost::container::vector<diploid_t>>,
+				   boost::container::vector<multiloc_glist_t<mtype>>,
+				   boost::container::vector<mtype>,
+				   boost::container::vector<unsigned>,
+				   boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>>;
 
   /*!
     \brief Single population, multilocus simulation with serialization.  Can be copied, etc.
@@ -41,14 +40,17 @@ namespace KTfwd
   */
   template<typename mtype,
 	   typename mwriter,
-	   typename mreader> using multiloc_serialized = sugar::multiloc_serialized<mtype,mwriter,mreader,
-										    multiloc_mlist_t<mtype>,
-										    multiloc_glist_t<mtype>,
-										    multiloc_dipvector_t<mtype>,
-										    boost::container::vector<multiloc_glist_t<mtype>>,
-										    boost::container::vector<mtype>,
-										    boost::container::vector<unsigned>,
-										    boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>>;
+	   typename mreader,
+	   typename diploid_t = std::pair<typename multiloc_glist_t<mtype>::iterator,
+					  typename multiloc_glist_t<mtype>::iterator> >
+  using multiloc_serialized = sugar::multiloc_serialized<mtype,mwriter,mreader,
+							 multiloc_mlist_t<mtype>,
+							 multiloc_glist_t<mtype>,
+							 boost::container::vector<boost::container::vector<diploid_t>>,
+							 boost::container::vector<multiloc_glist_t<mtype>>,
+							 boost::container::vector<mtype>,
+							 boost::container::vector<unsigned>,
+							 boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>>;
 }
 
 #else
@@ -62,25 +64,24 @@ namespace KTfwd
   template<typename mtype> using multiloc_mlist_t = std::list<mtype>;
   template<typename mtype> using multiloc_gamete_t = gamete_base<mtype,multiloc_mlist_t<mtype>>;
   template<typename mtype> using multiloc_glist_t = std::list<multiloc_gamete_t<mtype>>;
-  template<typename mtype> using multiloc_dipvector_t = std::vector< std::vector<
-								       std::pair<typename multiloc_glist_t<mtype>::iterator,
-										 typename multiloc_glist_t<mtype>::iterator> >
-								     >;
 
   /*!
     \brief Single population, multilocus simulation without serialization.  Cannot be copied, etc.
     See @ref md_md_sugar for rationale, etc.
     \ingroup sugar
   */
-  template<typename mtype> using multiloc = sugar::multiloc<mtype,
-							    multiloc_mlist_t<mtype>,
-							    multiloc_glist_t<mtype>,
-							    multiloc_dipvector_t<mtype>,
-							    std::vector<multiloc_glist_t<mtype>>,
-							    std::vector<mtype>,
-							    std::vector<unsigned>,
-							    std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>>;
-
+  template<typename mtype,
+	   typename diploid_t = std::pair<typename multiloc_glist_t<mtype>::iterator,
+					  typename multiloc_glist_t<mtype>::iterator> >
+  using multiloc = sugar::multiloc<mtype,
+				   multiloc_mlist_t<mtype>,
+				   multiloc_glist_t<mtype>,
+				   std::vector<std::vector<diploid_t>>,
+				   std::vector<multiloc_glist_t<mtype>>,
+				   std::vector<mtype>,
+				   std::vector<unsigned>,
+				   std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>>;
+  
   /*!
     \brief Single population, multilocus simulation with serialization.  Can be copied, etc.
     See @ref md_md_sugar for rationale, etc.
@@ -88,14 +89,17 @@ namespace KTfwd
   */
   template<typename mtype,
 	   typename mwriter,
-	   typename mreader> using multiloc_serialized = sugar::multiloc_serialized<mtype,mwriter,mreader,
-										    multiloc_mlist_t<mtype>,
-										    multiloc_glist_t<mtype>,
-										    multiloc_dipvector_t<mtype>,
-										    std::vector<multiloc_glist_t<mtype>>,
-										    std::vector<mtype>,
-										    std::vector<unsigned>,
-										    std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>>;
+	   typename mreader,
+	   typename diploid_t = std::pair<typename multiloc_glist_t<mtype>::iterator,
+					  typename multiloc_glist_t<mtype>::iterator> >
+  using multiloc_serialized = sugar::multiloc_serialized<mtype,mwriter,mreader,
+							 multiloc_mlist_t<mtype>,
+							 multiloc_glist_t<mtype>,
+							 std::vector<std::vector<diploid_t>>,
+							 std::vector<multiloc_glist_t<mtype>>,
+							 std::vector<mtype>,
+							 std::vector<unsigned>,
+							 std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>>;
 }
 #endif
 
