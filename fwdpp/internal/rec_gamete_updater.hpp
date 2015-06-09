51bb674c00c662e1e590053b194839bbabdf783d
Kevin R. Thornton <krthornt@uci.edu>
2015-06-09 10:50:35 -0700

    cleanup

--- fwdpp/internal/rec_gamete_updater.hpp
+++ fwdpp/internal/rec_gamete_updater.hpp
@@ -9,42 +9,17 @@ namespace KTfwd
 {
   namespace fwdpp_internal
   {
-    /*
-      The code used thru 0.3.1 is kept here
-     */
     template< typename itr_type,
 	      typename cont_type >
     itr_type rec_gam_updater( itr_type & __first, itr_type & __last,
 			      cont_type & m1, cont_type & m2,
 			      const short & SWITCH, const double & val )
     {
-      /*
-      typename itr_type::difference_type __dist = std::distance(__first,__last);
-      if ( __dist <= 250 )
-	{
-	  //At most __last - _first comparisons of double + equal number of emplace_backs
-	  while(__first != __last && (*__first)->pos < val)
-	    {
-	      if( SWITCH )
-		{
-		  m1.emplace_back(*__first);
-		}
-	      else
-		{
-		  m2.emplace_back(*__first);
-		}
-	      ++__first;
-	    }
-	  return __first;
-	}
-      */
       //O(log_2) comparisons of double plus at most __last - __first copies
       itr_type __ub = std::lower_bound(__first,__last,
 				       std::cref(val),
 				       [](const typename itr_type::value_type & __mut,const double & __val) {
-				       //[](const double & __val,const typename itr_type::value_type & __mut) {
 					 return __mut->pos < __val;
-					 //return __val < __mut->pos;
 				       });
       if (SWITCH)
 	std::copy(__first,__ub,std::back_inserter(m1));
