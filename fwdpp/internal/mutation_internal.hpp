89f9366715976acab088d1ee8fa4a831ada9b4eb
Kevin R. Thornton <krthornt@uci.edu>
2015-06-09 10:32:34 -0700

    streamlining

--- fwdpp/internal/mutation_internal.hpp
+++ fwdpp/internal/mutation_internal.hpp
@@ -10,14 +10,9 @@ namespace KTfwd {
   {
     /*!
       Mechanics of adding a new mutation.
-      We do this odd choosing between
-      find_if and upper_bound 
-      b/c of the way the GCC STL is implemented.
-      
-      Objectively, upper_bound should always  be preferred,
-      but that is not the case in practice because the GNU STL
-      implements find_if in a very clever way for
-      small containers.
+
+      Ensures that it is always added into a vector
+      in less-than-sorted order according to position
     */
     template< typename gamete_type,
 	      typename mutation_iterator>
@@ -26,30 +21,16 @@ namespace KTfwd {
     {
       if(mitr->neutral)
 	{
-	  new_gamete.mutations.insert( (new_gamete.mutations.size()<=250) ? 
-				       std::find_if(new_gamete.mutations.begin(),
-						    new_gamete.mutations.end(),
-						    [&](const mutation_iterator & mut_itr)
-						    {
-						      return mut_itr->pos > mitr->pos;
-						    }) :
-				       std::upper_bound(new_gamete.mutations.begin(),
-							new_gamete.mutations.end(),mitr->pos,
-							[](const double & __value,const mutation_iterator & __mut){ return __value < __mut->pos;}),
-				       mitr );
+	  new_gamete.mutations.insert(std::lower_bound(new_gamete.mutations.begin(),
+						       new_gamete.mutations.end(),mitr->pos,
+						       [](const mutation_iterator & __mut,const double & __value){ return __mut->pos < __value;}),
+				      mitr );
 	}
       else
 	{
-	  new_gamete.smutations.insert( (new_gamete.smutations.size() <= 250) ? 
-					std::find_if(new_gamete.smutations.begin(),
-						     new_gamete.smutations.end(),
-						     [&](const mutation_iterator & mut_itr)
-						     {
-						       return mut_itr->pos > mitr->pos;
-						     }) :
-					std::upper_bound(new_gamete.smutations.begin(),
-							 new_gamete.smutations.end(),mitr->pos,
-							 [](const double & __value,const mutation_iterator & __mut){ return __value < __mut->pos;}),
+	  new_gamete.smutations.insert(std::lower_bound(new_gamete.smutations.begin(),
+							new_gamete.smutations.end(),mitr->pos,
+							[](const mutation_iterator & __mut,const double & __value){ return __mut->pos < __value;}),
 					mitr );
 	}
     }
