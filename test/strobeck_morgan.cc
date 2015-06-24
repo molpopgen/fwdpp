891f9f0c92374affdd1e09b059ea12a4d48672ac
Kevin R. Thornton <krthornt@uci.edu>
2015-06-24 11:47:09 -0700

    update to latest API

--- test/strobeck_morgan.cc
+++ test/strobeck_morgan.cc
@@ -1,8 +1,8 @@
 #include <config.h>
 #include <fwdpp/diploid.hh>
 #include <Sequence/SimData.hpp>
+#include <unordered_set>
 #include <boost/unordered_set.hpp>
-
 #include <boost/container/list.hpp>
 #include <boost/container/vector.hpp>
 #include <boost/pool/pool_alloc.hpp>
@@ -31,20 +31,20 @@ struct mutation_with_age : public KTfwd::mutation_base
 
 //compiling the code with -DUSE_STANDARD_CONTAINERS will use std::vector and std::list instead of the boost alternatives
 typedef mutation_with_age mtype;
-#if defined(HAVE_BOOST_VECTOR) && defined(HAVE_BOOST_LIST) && defined(HAVE_BOOST_UNORDERED_SET) && defined(HAVE_BOOST_POOL_ALLOC) && defined(HAVE_BOOST_HASH) && !defined(USE_STANDARD_CONTAINERS)
+#if defined(USE_BOOST_CONTAINERS)
 typedef boost::pool_allocator<mtype> mut_allocator;
 typedef boost::container::list<mtype,mut_allocator > mlist;
 typedef KTfwd::gamete_base<mtype,mlist> gtype;
 typedef boost::pool_allocator<gtype> gam_allocator;
 typedef boost::container::list<gtype,gam_allocator > glist;
+typedef boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps > lookup_table_type;
 #else
 typedef std::list<mtype> mlist;
 typedef KTfwd::gamete_base<mtype,mlist> gtype;
-typedef std::list<gtype > list;
+typedef std::list<gtype > glist;
+typedef std::unordered_set<double,std::hash<double>,KTfwd::equal_eps> lookup_table_type;
 #endif
 
-
-typedef boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps > lookup_table_type;
 mutation_with_age neutral_mutations_inf_sites(gsl_rng * r,const unsigned * generation,mlist * mutations,
 					      lookup_table_type * lookup, const double & beg)
 {
@@ -62,7 +62,7 @@ struct no_selection_multi
 {
   typedef double result_type;
   template< typename iterator_type >
-  inline double operator()( const std::vector< std::pair<iterator_type,iterator_type> > & diploid ) const
+  inline double operator()( const iterator_type ) const
   {
     return 1.;
   }
@@ -114,26 +114,30 @@ int main(int argc, char ** argv)
     yet mutations will fall within them, satisfying the assumptions of Strobeck 
     and Morgan.
    */
-  boost::function<double(void)> recmap = std::bind(mslike_map,r,0.,L),
+  std::function<double(void)> recmap = std::bind(mslike_map,r,0.,L),
     recmap2 = std::bind(mslike_map,r,1.,L),
     recmap3 = std::bind(mslike_map,r,2.,L);
 
   //need vectors of recombination maps, mutation policies, and fitness models
-  std::vector< boost::function<unsigned(glist::iterator &, glist::iterator &)> > recpols(3);
-  std::vector< boost::function<mtype(mlist *)> > mmodels(3);
+  // std::vector< boost::function<unsigned(glist::iterator &, glist::iterator &,
+  // 					decltype()> > recpols(3);
+  std::vector< std::function<mtype(mlist *)> > mmodels(3);
 
   const double rbw[2] = {1./4e3,1./4e3}; //rho b/w each locus pair = 1
   ostringstream buffer;
   while(nreps--)
     {
-      std::vector< glist > gametes (3, glist(1,gtype(twoN) ));
-      std::vector< std::pair< glist::iterator, glist::iterator > > idip(3);
-      idip[0] = std::make_pair( gametes[0].begin(),gametes[0].begin() );
-      idip[1] = std::make_pair( gametes[1].begin(),gametes[1].begin() );
-      idip[2] = std::make_pair( gametes[2].begin(),gametes[2].begin() );
+      std::cerr << nreps << '\n';
+      //std::vector< glist > gametes (3, glist(1,gtype(twoN) ));
+      glist gametes(1,gtype(twoN));
+      std::vector< std::pair< glist::iterator, glist::iterator > > idip(3,std::make_pair(gametes.begin(),gametes.begin()));
+      //idip[0] = std::make_pair( gametes[0].begin(),gametes[0].begin() );
+      //idip[1] = std::make_pair( gametes[1].begin(),gametes[1].begin() );
+      //idip[2] = std::make_pair( gametes[2].begin(),gametes[2].begin() );
 
       std::vector< std::vector< std::pair< glist::iterator, glist::iterator > > > diploids(N,idip);
 
+      gtype::mutation_container neutral,selected;
       mlist mutations;  
       std::vector<mtype> fixations;  
       std::vector<unsigned> fixation_times;
@@ -141,9 +145,10 @@ int main(int argc, char ** argv)
       double wbar;
       lookup_table_type lookup;
 
-      recpols[0] = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,&gametes[0],littler,r,recmap);
-      recpols[1] = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,&gametes[1],littler,r,recmap2);
-      recpols[2] = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,&gametes[2],littler,r,recmap3);
+      auto recpol1 = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,std::placeholders::_3,std::ref(neutral),std::ref(selected),&gametes,littler,r,recmap);
+      auto recpol2 = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,std::placeholders::_3,std::ref(neutral),std::ref(selected),&gametes,littler,r,recmap2);
+      auto recpol3 = std::bind(KTfwd::genetics101(),std::placeholders::_1,std::placeholders::_2,std::placeholders::_3,std::ref(neutral),std::ref(selected),&gametes,littler,r,recmap3);
+      std::vector<decltype(recpol1)> recpols( { recpol1,recpol2,recpol3});
       mmodels[0] = std::bind(neutral_mutations_inf_sites,r,&generation,std::placeholders::_1,&lookup,0.);
       mmodels[1] = std::bind(neutral_mutations_inf_sites,r,&generation,std::placeholders::_1,&lookup,1.);
       mmodels[2] = std::bind(neutral_mutations_inf_sites,r,&generation,std::placeholders::_1,&lookup,2.);
@@ -158,7 +163,7 @@ int main(int argc, char ** argv)
 				 &mus[0],
 	  			 mmodels,
 	  			 recpols,
-	  			 rbw,
+	  			 &rbw[0],
 				 [](gsl_rng * __r, const double & d) { return gsl_ran_poisson(__r,d); },
 	  			 std::bind(KTfwd::insert_at_end<mtype,mlist>,std::placeholders::_1,std::placeholders::_2),
 	  			 std::bind(KTfwd::insert_at_end<gtype,glist>,std::placeholders::_1,std::placeholders::_2),
