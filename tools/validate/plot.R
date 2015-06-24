9a2ae10887fb31973e1b72ec6162e9996224d549
Kevin R. Thornton <krthornt@uci.edu>
2015-06-24 09:07:03 -0700

    test runs in < 30 mins on my dev machine

--- tools/validate/plot.R
+++ tools/validate/plot.R
@@ -11,6 +11,7 @@ plot(ecdf(m$S),add=TRUE,col="red",do.points=F)
 plot(ecdf(f$pi),main="pi",do.points=F)
 plot(ecdf(m$pi),add=TRUE,col="red",do.points=F)
 
+
 plot(ecdf(f$next.),main="derived singletons",do.points=F)
 plot(ecdf(m$next.),add=TRUE,col="red",do.points=F)
 
@@ -23,3 +24,13 @@ plot(ecdf(m$nhaps),add=TRUE,col="red",do.points=F)
 plot(ecdf(f$rm),main="minrec (H&K)",do.points=F)
 plot(ecdf(m$rm),add=TRUE,col="red",do.points=F)
 dev.off()
+
+##p-values
+s.pv = ks.test(f$S,m$S)$p.value
+pi.pv = ks.test(f$pi,m$pi)$p.value
+next.pv = ks.test(f$next.,m$next.)$p.value
+hdiv.pv = ks.test(f$hdiv,m$hdiv)$p.value
+nhaps.pv = ks.test(f$nhaps,m$nhaps)$p.value
+rm.pv = ks.test(f$rm,m$rm)$p.value
+
+print(paste(s.pv,pi.pv,next.pv,hdiv.pv,nhaps.pv,rm.pv,sep=" "))
