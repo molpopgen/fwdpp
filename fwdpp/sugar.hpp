4fcda5b72e1ba87ee571292ed39aaf8b442c3659
Kevin R. Thornton <krthornt@uci.edu>
2015-07-14 09:23:07 -0700

    convenience header for sugar stuff.  #23

---
+++ fwdpp/sugar.hpp
@@ -0,0 +1,6 @@
+#ifndef __FWDPP_SUGAR_HPP__
+#define __FWDPP_SUGAR_HPP__
+
+#include <fwdpp/sugar/sugar.hpp>
+
+#endif
---
+++ fwdpp/sugar/sugar.hpp
@@ -0,0 +1,13 @@
+#ifndef __FWDPP_SUGAR_SUGAR_HPP__
+#define __FWDPP_SUGAR_SUGAR_HPP__
+
+#include<fwdpp/sugar/GSLrng_t.hpp>
+#include<fwdpp/sugar/infsites.hpp>
+#include<fwdpp/sugar/metapop.hpp>
+#include<fwdpp/sugar/multiloc.hpp>
+#include<fwdpp/sugar/popgenmut.hpp>
+#include<fwdpp/sugar/serialization.hpp>
+#include<fwdpp/sugar/singlepop.hpp>
+#include<fwdpp/sugar/sugar.hpp>
+
+#endif
