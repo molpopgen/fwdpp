420d7857135d1379e8af4e81f1857c32317c803e
Kevin R. Thornton <krthornt@uci.edu>
2015-07-21 10:06:55 -0700

    speed up the build of libseq

--- install_libseq.sh
+++ install_libseq.sh
@@ -8,4 +8,6 @@
 wget http://github.com/molpopgen/libsequence/archive/1.8.5.tar.gz
 tar xzf 1.8.5.tar.gz
 cd libsequence-1.8.5
-./configure && make check && sudo make install
+##We don't compile the unit tests here. 
+## We'll rely on libseq's own travis-ci setup to handle that in the future
+./configure && make && sudo make install
