23742045e91860bd7c64b8e901ebc7960e672b46
Kevin R. Thornton <krthornt@uci.edu>
2015-07-22 16:17:50 -0700

    inlined fitness distpach functions

--- fwdpp/internal/diploid_fitness_dispatch.hpp
+++ fwdpp/internal/diploid_fitness_dispatch.hpp
@@ -9,22 +9,22 @@ namespace KTfwd {
     //! Standard diploids
     template<typename fitness_policy_type,
 	     typename diploid_itr_t>
-    double diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::false_type ) {
+    inline double diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::false_type ) {
       return fp(d->first,d->second);
     }
-
+    
     //! Custom diploids
     template<typename fitness_policy_type,
 	     typename diploid_itr_t>
-    double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::true_type ) { 
+    inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::true_type ) { 
       return fp(d);
     }
 
     //! Custom diploids
     template<typename fitness_policy_type,
 	     typename diploid_itr_t>
-    double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::true_type,
-				      typename std::enable_if< traits::is_custom_diploid_t<typename diploid_itr_t::value_type>::type >::type()) { 
+    inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d, std::true_type,
+					     typename std::enable_if< traits::is_custom_diploid_t<typename diploid_itr_t::value_type>::type >::type()) { 
       return fp(*d);
     }
     
