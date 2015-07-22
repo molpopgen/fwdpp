05b95bea401016f246968121cf7767359d8ae067
Kevin R. Thornton <krthornt@uci.edu>
2015-07-22 12:57:13 -0700

    mod install_libseq.sh for local install

--- install_libseq.sh
+++ install_libseq.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env sh
 
-## This script may help a user install libsequence, 
-## but the main purpose is to make sure this dependency
-## is met for travis-ci builds
-
 ##Install libseq 1.8.5
 wget http://github.com/molpopgen/libsequence/archive/1.8.5.tar.gz
 tar xzf 1.8.5.tar.gz
 cd libsequence-1.8.5
-##We don't compile the unit tests here. 
-## We'll rely on libseq's own travis-ci setup to handle that in the future
-./configure && make && sudo make install
+./configure --prefix=$HOME && make install
