664d14f264d80729935197cfbea25cd91e3a0e07
Kevin R. Thornton <krthornt@uci.edu>
2015-08-05 09:19:11 -0700

    keyit fxn added to make it more clear what's going on.  const refs passed instead of copies of iterators

--- fwdpp/internal/gamete_lookup_table.hpp
+++ fwdpp/internal/gamete_lookup_table.hpp
@@ -27,11 +27,14 @@ namespace KTfwd {
       using inner_t = typename lookup_table_t::value_type;
       lookup_table_t lookup_table;
 
-      inline void update_details( gcont_t_itr g ) 
+      inline double keyit( const typename gcont_t_itr::value_type::mutation_container & mc ) const
       {
-	double npos0 = ((g->mutations.empty()) ? -std::numeric_limits<double>::max() : g->mutations[0]->pos);
-	double spos0 = ((g->smutations.empty()) ? -std::numeric_limits<double>::max() : g->smutations[0]->pos);
-	lookup_table.insert( std::make_pair( npos0*double(g->mutations.size()) + spos0*double(g->smutations.size()), g ) );
+	return (mc.empty()) ? -std::numeric_limits<double>::max() : mc[0]->pos;
+      }
+      
+      inline void update_details( const gcont_t_itr & g ) 
+      {
+	lookup_table.emplace( std::make_pair( keyit(g->mutations)*double(g->mutations.size()) + keyit(g->smutations)*double(g->smutations.size()), g) );
       }
 
       explicit gamete_lookup(gcont_t * gametes) : lookup_table( lookup_table_t() )
@@ -44,12 +47,10 @@ namespace KTfwd {
 
       inline result_type lookup( const typename gcont_t::value_type & g ) 
       {
-	double npos0 = ((g.mutations.empty()) ? -std::numeric_limits<double>::max() : g.mutations[0]->pos);
-	double spos0 = ((g.smutations.empty()) ? -std::numeric_limits<double>::max() : g.smutations[0]->pos);
-	return lookup_table.equal_range(  npos0*double(g.mutations.size()) + spos0*double(g.smutations.size()) );
+	return lookup_table.equal_range(  keyit(g.mutations)*double(g.mutations.size()) + keyit(g.smutations)*double(g.smutations.size()) );
       }
 
-      inline void update( gcont_t_itr g ) 
+      inline void update( const gcont_t_itr & g ) 
       {
 	update_details(g);
       }
