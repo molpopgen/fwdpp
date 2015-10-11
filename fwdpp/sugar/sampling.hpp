76a1c3107353474776b67ce07729e8145038d8f6
Kevin R. Thornton <krthornt@uci.edu>
2015-10-11 11:42:16 -0700

    move impl. details into subdir.  update autotools stuff to install new header

--- fwdpp/sugar/sampling.hpp
+++ fwdpp/sugar/sampling.hpp
@@ -8,7 +8,7 @@
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/metapop.hpp>
 #include <fwdpp/sugar/multiloc.hpp>
-
+#include <fwdpp/sugar/sampling/sampling_details.hpp>
 /*
   TODO:
 
@@ -17,112 +17,6 @@
 */
 namespace KTfwd
 {
-  using sample_t = std::vector< std::pair<double,std::string> >;
-  using sep_sample_t = std::pair<sample_t,sample_t>;
-
-  enum class treat_neutral {ALL,NEUTRAL,SELECTED};
-
-  template<typename vec_mutation_t>
-  void add_fixations( sample_t * sample,
-		      const vec_mutation_t & fixations,
-		      const unsigned nsam,
-		      const treat_neutral treat )
-  {
-    for( const auto & f : fixations)
-      {
-	if( treat == treat_neutral::ALL )
-	  {
-	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
-	  }
-	else if (treat == treat_neutral::NEUTRAL && f.neutral ) //only add neutral mutations
-	  {
-	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
-	  }
-	else if (treat == treat_neutral::SELECTED && !f.neutral ) //only add selected mutations
-	  {
-	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
-	  }
-      }
-  }
-
-  //Single-region, single-deme
-  template<typename poptype>
-  sample_t sample_details( gsl_rng * r,
-			   const poptype & p,
-			   const unsigned nsam,
-			   const bool removeFixed,
-			   std::true_type)
-  {
-    sample_t rv =  ms_sample(r,&p.diploids,nsam,removeFixed);
-    if(!removeFixed)
-      add_fixations(&rv,p.fixations,nsam,treat_neutral::ALL);
-    return rv;
-  }
-
-  //Multi-locus, single-deme
-  template<typename poptype>
-  sample_t sample_details( gsl_rng * r,
-				   const poptype & p,
-			   const unsigned nsam,
-			   const bool removeFixed,
-			   std::false_type)
-  {
-    sample_t rv = ms_sample(r,&p.diploids,nsam,removeFixed);
-    if(!removeFixed)
-      add_fixations(&rv,p.fixations,nsam,treat_neutral::ALL);
-    return rv;
-  }
-
-  //Single-region, single-deme
-  template<typename poptype>
-  sep_sample_t sample_sep_details( gsl_rng * r,
-				   const poptype & p,
-				   const unsigned nsam,
-				   const bool removeFixed,
-				   std::true_type)
-  {
-    sep_sample_t rv = ms_sample_separate(r,&p.diploids,nsam,removeFixed);
-    if(! removeFixed)
-      {
-	add_fixations(&rv.first,p.fixations,nsam,treat_neutral::NEUTRAL);
-	add_fixations(&rv.second,p.fixations,nsam,treat_neutral::SELECTED);
-      }
-    return rv;
-  }
-
-  template<typename poptype>
-  sep_sample_t sample_sep_details( gsl_rng * r,
-				   const poptype & p,
-				   const std::vector<unsigned> & individuals,
-				   const bool removeFixed,
-				   std::true_type)
-  {
-    sep_sample_t rv = ms_sample_separate_single_deme(r,&p.diploids,individuals,individuals.size(),removeFixed);
-    if(! removeFixed)
-      {
-	add_fixations(&rv.first,p.fixations,individuals.size(),treat_neutral::NEUTRAL);
-	add_fixations(&rv.second,p.fixations,individuals.size(),treat_neutral::SELECTED);
-      }
-    return rv;
-  }
-
-  //Multi-locus, single-deme
-  template<typename poptype>
-  sep_sample_t sample_sep_details( gsl_rng * r,
-				   const poptype & p,
-				   const unsigned nsam,
-				   const bool removeFixed,
-				   std::false_type)
-  {
-    sep_sample_t rv =  ms_sample_separate(r,&p.diploids,nsam,removeFixed);
-    if(! removeFixed)
-      {
-	add_fixations(&rv.first,p.fixations,nsam,treat_neutral::NEUTRAL);
-	add_fixations(&rv.second,p.fixations,nsam,treat_neutral::SELECTED);
-      }
-    return rv;
-  }
-
   template<typename poptype>
   sample_t sample( gsl_rng * r,
 		   const poptype & p,
@@ -184,9 +78,6 @@ namespace KTfwd
       }
     return ms_sample_separate(r,&p.diploids[deme],nsam,removeFixed);
   }
-
-
-
 }
 
 #endif
---
+++ fwdpp/sugar/sampling/sampling_details.hpp
@@ -0,0 +1,113 @@
+#ifndef __FWDPP_SUGAR_SAMPLING_SAMPLING_DETAILS_HPP__
+#define __FWDPP_SUGAR_SAMPLING_SAMPLING_DETAILS_HPP__
+
+namespace KTfwd
+{
+  namespace sugar
+  {
+    enum class treat_neutral {ALL,NEUTRAL,SELECTED};
+  }
+  using sample_t = std::vector< std::pair<double,std::string> >;
+  using sep_sample_t = std::pair<sample_t,sample_t>;
+
+  template<typename vec_mutation_t>
+  void add_fixations( sample_t * sample,
+		      const vec_mutation_t & fixations,
+		      const unsigned nsam,
+		      const sugar::treat_neutral treat )
+  {
+    for( const auto & f : fixations)
+      {
+	if( treat == sugar::treat_neutral::ALL )
+	  {
+	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
+	  }
+	else if (treat == sugar::treat_neutral::NEUTRAL && f.neutral ) //only add neutral mutations
+	  {
+	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
+	  }
+	else if (treat == sugar::treat_neutral::SELECTED && !f.neutral ) //only add selected mutations
+	  {
+	    sample->emplace_back( std::make_pair(f.pos,std::string(nsam,'1')) );
+	  }
+      }
+  }
+
+  //Single-region, single-deme
+  template<typename poptype>
+  sample_t sample_details( gsl_rng * r,
+			   const poptype & p,
+			   const unsigned nsam,
+			   const bool removeFixed,
+			   std::true_type)
+  {
+    sample_t rv =  ms_sample(r,&p.diploids,nsam,removeFixed);
+    if(!removeFixed)
+      add_fixations(&rv,p.fixations,nsam,sugar::treat_neutral::ALL);
+    return rv;
+  }
+
+  //Multi-locus, single-deme
+  template<typename poptype>
+  sample_t sample_details( gsl_rng * r,
+				   const poptype & p,
+			   const unsigned nsam,
+			   const bool removeFixed,
+			   std::false_type)
+  {
+    sample_t rv = ms_sample(r,&p.diploids,nsam,removeFixed);
+    if(!removeFixed)
+      add_fixations(&rv,p.fixations,nsam,sugar::treat_neutral::ALL);
+    return rv;
+  }
+
+  //Single-region, single-deme
+  template<typename poptype>
+  sep_sample_t sample_sep_details( gsl_rng * r,
+				   const poptype & p,
+				   const unsigned nsam,
+				   const bool removeFixed,
+				   std::true_type)
+  {
+    sep_sample_t rv = ms_sample_separate(r,&p.diploids,nsam,removeFixed);
+    if(! removeFixed)
+      {
+	add_fixations(&rv.first,p.fixations,nsam,sugar::treat_neutral::NEUTRAL);
+	add_fixations(&rv.second,p.fixations,nsam,sugar::treat_neutral::SELECTED);
+      }
+    return rv;
+  }
+
+  template<typename poptype>
+  sep_sample_t sample_sep_details( gsl_rng * r,
+				   const poptype & p,
+				   const std::vector<unsigned> & individuals,
+				   const bool removeFixed,
+				   std::true_type)
+  {
+    sep_sample_t rv = ms_sample_separate_single_deme(r,&p.diploids,individuals,individuals.size(),removeFixed);
+    if(! removeFixed)
+      {
+	add_fixations(&rv.first,p.fixations,individuals.size(),sugar::treat_neutral::NEUTRAL);
+	add_fixations(&rv.second,p.fixations,individuals.size(),sugar::treat_neutral::SELECTED);
+      }
+    return rv;
+  }
+
+  //Multi-locus, single-deme
+  template<typename poptype>
+  sep_sample_t sample_sep_details( gsl_rng * r,
+				   const poptype & p,
+				   const unsigned nsam,
+				   const bool removeFixed,
+				   std::false_type)
+  {
+    sep_sample_t rv =  ms_sample_separate(r,&p.diploids,nsam,removeFixed);
+    if(! removeFixed)
+      {
+	add_fixations(&rv.first,p.fixations,nsam,sugar::treat_neutral::NEUTRAL);
+	add_fixations(&rv.second,p.fixations,nsam,sugar::treat_neutral::SELECTED);
+      }
+    return rv;
+  }
+}
