a699c424a305b7921927e2ec4d8484f837b991d1
Kevin R. Thornton <krthornt@uci.edu>
2015-11-16 11:12:29 -0800

    namespace in documentation

--- fwdpp/sugar/generalmut.hpp
+++ fwdpp/sugar/generalmut.hpp
@@ -56,10 +56,10 @@ namespace KTfwd
 
     This type represents a mutation with a number of 's' and 'h' parameters that are determined at run time.
 
-    This type complements generalmut.  The use cases will be similar.  This type will show better performance
+    This type complements KTfwd::generalmut.  The use cases will be similar.  This type will show better performance
     for large numbers of categories (e.g., large sizes of member variables s and h).
 
-    This type differs from generalmut in that the sizes of the s and h vectors do not have to be the same.
+    This type differs from KTfwd::generalmut in that the sizes of the s and h vectors do not have to be the same.
    */
   struct generalmut_vec : public mutation_base
   {
