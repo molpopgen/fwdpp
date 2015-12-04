bfa19d414a54d81d9303a7362e26e036e4fbebd5
Kevin R. Thornton <krthornt@uci.edu>
2015-12-04 13:32:02 -0800

    move fxn to internal

--- fwdpp/internal/recycling.hpp
+++ fwdpp/internal/recycling.hpp
@@ -28,6 +28,33 @@ namespace KTfwd
 	}
       return rv;
     }
+
+    template<typename iterator_type,
+	     typename glist_t,
+	     typename queue_t,
+	     typename lookup_type>
+    inline void recycle_gamete( iterator_type & g1,
+				glist_t * gametes,
+				queue_t & gamete_recycling_bin, lookup_type & gamete_lookup,
+				typename iterator_type::value_type::mutation_container & neutral,
+				typename iterator_type::value_type::mutation_container & selected )
+    {
+      //Try to recycle
+      if( ! gamete_recycling_bin.empty() )
+	{
+	  g1 = gamete_recycling_bin.front();
+	  assert(!g1->n);
+	  g1->n=0u;
+	  std::swap(g1->mutations,neutral);
+	  std::swap(g1->smutations,selected);
+	  gamete_recycling_bin.pop();
+	}
+      else
+	{
+	  g1 = gametes->emplace(gametes->end(),0u,std::move(neutral),std::move(selected));
+	}
+      gamete_lookup.update(g1);
+    }
   }
 }
 
