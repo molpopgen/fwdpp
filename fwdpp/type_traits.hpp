a4e61c83df38a8ecf3a9dea871e6859e1e75a47a
Kevin R. Thornton <krthornt@uci.edu>
2015-12-20 11:37:44 -0800

    delete several templates that are no longer needed

--- fwdpp/type_traits.hpp
+++ fwdpp/type_traits.hpp
@@ -6,22 +6,6 @@
 #include <fwdpp/tags/diploid_tags.hpp>
 namespace KTfwd {
   namespace traits {
-
-    //! Evaluates to std::true_type if F is a function taking no arguments
-    template<typename F>
-    using is_nullary_t = typename internal::is_nullary<F>::type;
-
-    //! Evaluates to std::true_type if F is a function a single argument of type A1
-    template<typename F,typename A1>
-    using is_unary_t = typename internal::is_unary<F,A1>::type;
-   
-    /*! Evaluates to std::true_type if F is a function taking arguments of types A1 and A2
-      \note Due to the vaguaries of std::bind, distinguishing a binary function from a unary one 
-      requires something like: is_binary_t<F,A1,A2>::value && !(is_unary_t<F,A1>::value||is_unary_t<F,A2>::value)
-     */
-    template<typename F,typename A1,typename A2>
-    using is_binary_t = typename internal::is_binary<F,A1,A2>::type;
-
     //! Evaluates to std::true_type if T publicly inherits from KTfwd::tags::custom_diploid_t
     template<typename T>
     using is_custom_diploid_t = typename std::is_base_of<KTfwd::tags::custom_diploid_t,T>::type; 
