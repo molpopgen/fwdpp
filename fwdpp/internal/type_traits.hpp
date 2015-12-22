e6197f65442f6529b5a9172da573f1d674da51a5
molpopgen <krthornt@uci.edu>
2015-12-22 06:30:21 -0800

    new file added to bury the macros in an internal namespace

---
+++ fwdpp/internal/type_traits.hpp
@@ -0,0 +1,33 @@
+#ifndef FWDPP_INTERNAL_TYPE_TRAITS_HPP
+#define FWDPP_INTERNAL_TYPE_TRAITS_HPP
+
+namespace KTfwd {
+  namespace traits {
+    namespace internal {
+      //Based on http://stackoverflow.com/questions/11813940/possible-to-use-type-traits-sfinae-to-find-if-a-class-defines-a-member-typec
+    
+      template<class T>
+      struct void_t {
+	typedef void type;
+      };
+
+      /* 
+	 This macro is used to generate 
+	 types able to determine if a class type
+	 has a particular member type name
+      */
+#define HAS_TYPE(NAME)							\
+      template<typename,typename = void>				\
+      struct has_##NAME: std::false_type				\
+      {};								\
+      template<typename T>						\
+      struct has_##NAME<T,typename void_t<typename T::NAME>::type >: std::true_type {}; 
+
+      HAS_TYPE(gamete_tag)
+      HAS_TYPE(mutation_list_type);
+      HAS_TYPE(mutation_type);
+    }
+  }
+}
+
+#endif
