a7aa739ae9401a2c1c44f7f3a03c3f455f910a09
Kevin R. Thornton <krthornt@uci.edu>
2016-01-05 13:15:17 -0800

    remove _serialized

--- fwdpp/sugar/singlepop.hpp
+++ fwdpp/sugar/singlepop.hpp
@@ -35,37 +35,13 @@ namespace KTfwd
 				     boost::container::vector<unsigned>,
 				     boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>
 				     >;
-
-  /*!
-    \brief Single locus, single population with serialization.  Can be copied, etc.
-    See @ref md_md_sugar for rationale, etc.
-    \ingroup sugar
-  */
-  template<typename mtype,
-	   typename mwriter_t,
-	   typename mreader_t,
-	   typename diploid_t = std::pair<typename singlepop_glist_t<mtype>::iterator,
-					  typename singlepop_glist_t<mtype>::iterator>,
-	   typename diploid_writer_t = diploidIOplaceholder,
-	   typename diploid_reader_t = diploidIOplaceholder>
-  using singlepop_serialized = sugar::singlepop_serialized<mtype,
-							   mwriter_t,mreader_t,
-							   singlepop_mlist_t<mtype>,
-							   singlepop_glist_t<mtype>,
-							   boost::container::vector< diploid_t >,
-							   boost::container::vector<mtype>,
-							   boost::container::vector<uint_t>,
-							   boost::unordered_set<double,boost::hash<double>,KTfwd::equal_eps>,
-							   diploid_writer_t,
-							   diploid_reader_t
-							   >;
 }
 #else
 
 #include <list>
 #include <vector>
 #include <unordered_set>
-#include <fwdpp/fwd_functional.hpp> 
+#include <fwdpp/fwd_functional.hpp>
 #include <fwdpp/sugar/singlepop/singlepop.hpp>
 
 
@@ -90,30 +66,6 @@ namespace KTfwd
 				       std::vector<uint_t>,
 				       std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>
 				       >;
-
-  /*! 
-    \brief Single locus, single population with serialization.  Can be copied, etc.
-    See @ref md_md_sugar for rationale, etc.
-    \ingroup sugar
-  */
-  template<typename mtype,
-	   typename mwriter_t,
-	   typename mreader_t,
-	   typename diploid_t = std::pair<typename singlepop_glist_t<mtype>::iterator,
-					  typename singlepop_glist_t<mtype>::iterator>,
-	   typename diploid_writer_t = diploidIOplaceholder,
-	   typename diploid_reader_t = diploidIOplaceholder>
-  using singlepop_serialized = sugar::singlepop_serialized<mtype,
-							   mwriter_t,mreader_t,
-							   singlepop_mlist_t<mtype>,
-							   singlepop_glist_t<mtype>,
-							   std::vector< diploid_t >,
-							   std::vector<mtype>,
-							   std::vector<uint_t>,
-							   std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>,
-							   diploid_writer_t,
-							   diploid_reader_t
-							   >;
 }
 #endif
 
