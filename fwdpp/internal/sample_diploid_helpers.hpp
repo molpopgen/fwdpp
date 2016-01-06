bc435e66c6116f6dd6439e552fcb69fa4838a07e
Kevin R. Thornton <krthornt@uci.edu>
2016-01-05 16:52:28 -0800

    cleanup

--- fwdpp/internal/sample_diploid_helpers.hpp
+++ fwdpp/internal/sample_diploid_helpers.hpp
@@ -7,31 +7,6 @@ namespace KTfwd
 {
   namespace fwdpp_internal
   {
-    template<typename iterator_type>
-    void adjust_mutation_counts( iterator_type & g , const unsigned & n)
-    /*!
-      Update the counts of each mutation.
-
-      This function is called by KTfwd::fwdpp_internal::adjust_mutation_counts
-    */
-    {
-      auto adjuster = [&n](typename iterator_type::value_type::mutation_list_type_iterator & __m) {
-	if(!__m->checked)
-	  {
-	    __m->n=n;
-	    __m->checked=true;
-	  }
-	else
-	  {
-	    __m->n += n;
-	  }
-      };
-      std::for_each(g->mutations.begin(),g->mutations.end(),
-		    std::cref(adjuster));
-      std::for_each(g->smutations.begin(),g->smutations.end(),
-		    std::cref(adjuster));
-    }
-
     template<typename gcont_t,
 	     typename mcont_t>
     inline void process_glist( const gcont_t & gametes,
@@ -57,10 +32,6 @@ namespace KTfwd
 	      for(const auto & m : g.smutations) mcounts[m]+=g.n;
 	    }
 	}
-      // for(auto itr = gametes->begin() ; itr != gametes->end() ; ++itr )
-      // 	{
-      // 	  if(itr->n) adjust_mutation_counts(itr,itr->n);
-      // 	}
     }
   }
 }
