f97152f6c9b39ec20eb6766a10bc100be14bc24e
Kevin R. Thornton <krthornt@uci.edu>
2016-01-08 13:56:17 -0800

    delete serialized type

--- fwdpp/sugar/metapop.hpp
+++ fwdpp/sugar/metapop.hpp
@@ -29,28 +29,5 @@ namespace KTfwd
 				 std::vector<mtype>,
 				 std::vector<uint_t>,
 				 std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>>;
-
-  /*!
-    \brief Single locus metapopulation simulation with serialization.  Can be copied, etc.
-    See @ref md_md_sugar for rationale, etc.
-    \ingroup sugar
-  */
-  template<typename mtype,
-	   typename mwriter_t,
-	   typename mreader_t,
-	   typename diploid_t = std::pair<typename metapop_glist_t<mtype>::iterator,
-					  typename metapop_glist_t<mtype>::iterator>,
-	   typename diploid_writer_t = diploidIOplaceholder,
-	   typename diploid_reader_t = diploidIOplaceholder>
-  using metapop_serialized = sugar::metapop_serialized<mtype,mwriter_t,mreader_t,
-						       metapop_mlist_t<mtype>,
-						       metapop_glist_t<mtype>,
-						       std::vector<diploid_t>,
-						       std::vector<std::vector<diploid_t> >,
-						       std::vector<mtype>,
-						       std::vector<uint_t>,
-						       std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>,
-						       diploid_writer_t,
-						       diploid_reader_t>;
 }
 #endif
