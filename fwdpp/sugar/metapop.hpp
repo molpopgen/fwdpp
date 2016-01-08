c3ff21e4acdb45e8d43913917b93f2ae73e273d4
Kevin R. Thornton <krthornt@uci.edu>
2016-01-08 13:58:04 -0800

    update type names and dox

--- fwdpp/sugar/metapop.hpp
+++ fwdpp/sugar/metapop.hpp
@@ -3,27 +3,24 @@
 
 #include <fwdpp/sugar/metapop/metapop.hpp>
 #include <fwdpp/fwd_functional.hpp>
-#include <list>
 #include <vector>
 #include <unordered_set>
 
 namespace KTfwd
 {
-  template<typename mtype> using metapop_mlist_t = std::list<mtype>;
-  template<typename mtype> using metapop_gamete_t = gamete_base<mtype,metapop_mlist_t<mtype>>;
-  template<typename mtype> using metapop_glist_t = std::list<metapop_gamete_t<mtype> >;
+  template<typename mtype> using metapop_mcont_t = std::list<mtype>;
+  template<typename mtype> using metapop_gamete_t = gamete_base<mtype,metapop_mcont_t<mtype>>;
+  template<typename mtype> using metapop_gcont_t = std::list<metapop_gamete_t<mtype> >;
 
   /*!
-    \brief Single locus metapopulation simulation without serialization.  Cannot be copied, etc.
-    See @ref md_md_sugar for rationale, etc.
+    \brief Single locus metapopulation simulation object
     \ingroup sugar
   */
   template<typename mtype,
-	   typename diploid_t = std::pair<typename metapop_glist_t<mtype>::iterator,
-					  typename metapop_glist_t<mtype>::iterator> >
+	   typename diploid_t = std::pair<std::size_t,std::size_t> >
   using metapop = sugar::metapop<mtype,
-				 metapop_mlist_t<mtype>,
-				 metapop_glist_t<mtype>,
+				 metapop_mcont_t<mtype>,
+				 metapop_gcont_t<mtype>,
 				 std::vector<diploid_t>,
 				 std::vector<std::vector<diploid_t> >,
 				 std::vector<mtype>,
