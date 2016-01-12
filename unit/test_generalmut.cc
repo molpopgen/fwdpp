d1b873fda3d1c17e2b3bbe074172828903a778c2
Kevin R. Thornton <krthornt@uci.edu>
2016-01-12 11:18:32 -0800

    update unit test

--- unit/test_generalmut.cc
+++ unit/test_generalmut.cc
@@ -15,10 +15,11 @@
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/generalmut.hpp>
+#include <fwdpp/sugar/serialization.hpp>
 
 BOOST_AUTO_TEST_CASE( construct_2 )
 {
-  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1,2);
+  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),2);
   BOOST_CHECK_EQUAL(p.s.size(),2);
   BOOST_CHECK_EQUAL(p.h.size(),2);
@@ -26,7 +27,7 @@ BOOST_AUTO_TEST_CASE( construct_2 )
 
 BOOST_AUTO_TEST_CASE( construct_2b )
 {
-  KTfwd::generalmut<2> p( {{0.5}},{{1,0}},0.001,1,2);
+  KTfwd::generalmut<2> p( {{0.5}},{{1,0}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),2);
   BOOST_CHECK_EQUAL(p.s.size(),2);
   BOOST_CHECK_EQUAL(p.h.size(),2);
@@ -34,7 +35,7 @@ BOOST_AUTO_TEST_CASE( construct_2b )
 
 BOOST_AUTO_TEST_CASE( construct_2c )
 {
-  KTfwd::generalmut<2> p( {{0.5}},{{1}},0.001,1,2);
+  KTfwd::generalmut<2> p( {{0.5}},{{1}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),2);
   BOOST_CHECK_EQUAL(p.s.size(),2);
   BOOST_CHECK_EQUAL(p.h.size(),2);
@@ -42,7 +43,7 @@ BOOST_AUTO_TEST_CASE( construct_2c )
 
 BOOST_AUTO_TEST_CASE( construct_4 )
 {
-  KTfwd::generalmut<4> p( {{0.5,-1,2.0,3.0}},{{1,0,-1,1}},0.001,1,2);
+  KTfwd::generalmut<4> p( {{0.5,-1,2.0,3.0}},{{1,0,-1,1}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),4);
   BOOST_CHECK_EQUAL(p.s.size(),4);
   BOOST_CHECK_EQUAL(p.h.size(),4);
@@ -50,7 +51,7 @@ BOOST_AUTO_TEST_CASE( construct_4 )
 
 BOOST_AUTO_TEST_CASE( construct_4b )
 {
-  KTfwd::generalmut<4> p( {{0.5}},{{1,0,-1,1}},0.001,1,2);
+  KTfwd::generalmut<4> p( {{0.5}},{{1,0,-1,1}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),4);
   BOOST_CHECK_EQUAL(p.s.size(),4);
   BOOST_CHECK_EQUAL(p.h.size(),4);
@@ -58,7 +59,7 @@ BOOST_AUTO_TEST_CASE( construct_4b )
 
 BOOST_AUTO_TEST_CASE( construct_4c )
 {
-  KTfwd::generalmut<4> p( {{0.5}},{{1}},0.001,1,2);
+  KTfwd::generalmut<4> p( {{0.5}},{{1}},0.001,1);
   BOOST_CHECK_EQUAL(std::tuple_size<decltype(p)::array_t>(),4);
   BOOST_CHECK_EQUAL(p.s.size(),4);
   BOOST_CHECK_EQUAL(p.h.size(),4);
@@ -66,7 +67,7 @@ BOOST_AUTO_TEST_CASE( construct_4c )
 
 BOOST_AUTO_TEST_CASE( serialize )
 {
-  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1,2);
+  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1);
 
   std::ostringstream o;
   KTfwd::mutation_writer w;
@@ -78,14 +79,13 @@ BOOST_AUTO_TEST_CASE( serialize )
 
   BOOST_CHECK_EQUAL(p.s.size(),p2.s.size());
   BOOST_CHECK_EQUAL(p.h.size(),p2.h.size());
-  BOOST_CHECK_EQUAL(p.n,p2.n);
   BOOST_CHECK_EQUAL(p.g,p2.g);
   BOOST_CHECK_EQUAL(p.pos,p2.pos);
 }
 
 BOOST_AUTO_TEST_CASE( serialize_gz )
 {
-  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1,2);
+  KTfwd::generalmut<2> p( {{0.5,-1}},{{1,0}},0.001,1);
 
   gzFile out = gzopen("test_generalmut_file.gz","w");
   KTfwd::mutation_writer w;
@@ -98,7 +98,6 @@ BOOST_AUTO_TEST_CASE( serialize_gz )
 
   BOOST_CHECK_EQUAL(p.s.size(),p2.s.size());
   BOOST_CHECK_EQUAL(p.h.size(),p2.h.size());
-  BOOST_CHECK_EQUAL(p.n,p2.n);
   BOOST_CHECK_EQUAL(p.g,p2.g);
   BOOST_CHECK_EQUAL(p.pos,p2.pos);
 
@@ -108,18 +107,7 @@ BOOST_AUTO_TEST_CASE( serialize_gz )
 BOOST_AUTO_TEST_CASE( serialize_pop1 )
 {
   using mtype = KTfwd::generalmut<2>;
-  using singlepop_serialized_t = KTfwd::singlepop_serialized<mtype,
-							     KTfwd::mutation_writer,
-							     KTfwd::mutation_reader<mtype>>;
-
-  singlepop_serialized_t pop1(100);
-  singlepop_serialized_t pop2(pop1);
-  
-}
-
-BOOST_AUTO_TEST_CASE( construct_extinct )
-{
-  auto x = KTfwd::generalmut<2>(KTfwd::tags::extinct());
-  BOOST_CHECK_EQUAL(x.n,0);
-  BOOST_CHECK_EQUAL(x.g,std::numeric_limits<unsigned>::max());
+  using singlepop_t = KTfwd::singlepop<mtype>;
+  singlepop_t pop1(100);
+  singlepop_t pop2(pop1);
 }
