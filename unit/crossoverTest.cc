651bac36f045eaaa9b0f3dea3be06fbd803ae7db
Kevin R. Thornton <krthornt@uci.edu>
2016-01-11 16:27:43 -0800

    updated unit test

--- unit/crossoverTest.cc
+++ unit/crossoverTest.cc
@@ -21,7 +21,7 @@
 #include <unistd.h>
 #include <iterator>
 #include <functional>
-#include <list>
+#include <vector>
 #include <iostream>
 
 using mut = KTfwd::mutation;
@@ -35,12 +35,11 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
 {
   //Neutral mutations at positions 0.1 and 0.9, resp.
   gtype g1(1),g2(1);
-  std::list<mut> mlist;
+  std::vector<mut> mvector(1,mut(0.1,0.));
 
-  auto mitr = mlist.insert(mlist.end(),mut(0.1,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
-  mitr = mlist.insert(mlist.end(),mut(0.9,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g2);
+  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
+  mvector.emplace_back(0.9,0.);
+  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g2);
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 1 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
@@ -53,55 +52,51 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
   //We need a "gamete pool"
   std::vector<gtype> gametes = { g1, g2 };
 
-  //get pointers/iterators to our two existing gametes
-  auto g1_itr = gametes.begin(),
-    g2_itr = g1_itr+1;
-
   //Make sure that our iterators are cool
-  BOOST_CHECK( std::find_if( g1_itr->mutations.begin(),
-			     g1_itr->mutations.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.1;
-			     } ) != g1_itr->mutations.end() );
-  BOOST_CHECK( std::find_if( g1_itr->mutations.begin(),
-			     g1_itr->mutations.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.0;
-			     } ) == g1_itr->mutations.end() );
-  BOOST_CHECK( std::find_if( g2_itr->mutations.begin(),
-			     g2_itr->mutations.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.9;
-			     } ) != g2_itr->mutations.end() );
-  BOOST_CHECK( std::find_if( g2_itr->mutations.begin(),
-			     g2_itr->mutations.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.1;
-			     } ) == g2_itr->mutations.end() );
+  BOOST_CHECK( std::find_if( g1.mutations.begin(),
+			     g1.mutations.end(),
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.1;
+			     } ) != g1.mutations.end() );
+  BOOST_CHECK( std::find_if( g1.mutations.begin(),
+			     g1.mutations.end(),
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.9;
+			     } ) == g1.mutations.end() );
+  BOOST_CHECK( std::find_if( g2.mutations.begin(),
+			     g2.mutations.end(),
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.9;
+			     } ) != g2.mutations.end() );
+  BOOST_CHECK( std::find_if( g2.mutations.begin(),
+			     g2.mutations.end(),
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.1;
+			     } ) == g2.mutations.end() );
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 1 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
 
   //These are our containers for the recombinants
 
-  //0.3.3 containers tha we need
   gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
-					    g1_itr, g2_itr,
+					    0,1,
+					    gametes,mvector,
 					    neutral,selected);
   /*
     Now, neutral should have both mutations
   */
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.9;
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.9;
 			     } ) != neutral.end() );
 
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.1;
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.1;
 			     } ) != neutral.end() );
 
 }
@@ -111,14 +106,13 @@ BOOST_AUTO_TEST_CASE( three_point_cross_1 )
   //g1: neutral muts at 0.1,0.5
   //g2: neutral muts at 0.9
   gtype g1(1),g2(1);
-  std::list<mut> mlist;
+  std::vector<mut> mvector(1,mut(0.1,0.));
 
-  auto mitr = mlist.insert(mlist.end(),mut(0.1,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
-  mitr = mlist.insert(mlist.end(),mut(0.5,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
-  mitr = mlist.insert(mlist.end(),mut(0.9,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g2);
+  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
+  mvector.emplace_back(0.5,0.);
+  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g1);
+  mvector.emplace_back(0.9,0.);
+  KTfwd::fwdpp_internal::add_new_mutation(2,mvector,g2);
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 2 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
@@ -128,16 +122,10 @@ BOOST_AUTO_TEST_CASE( three_point_cross_1 )
   //We need a "gamete pool"
   std::vector<gtype> gametes = { g1, g2 };
 
-  //get pointers/iterators to our two existing gametes
-  auto g1_itr = gametes.begin(),
-    g2_itr = g1_itr+1;
-
-  //gtype ng1(1),ng2(1);
-
   //Needed as of 0.3.3
   gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
-					    g1_itr, g2_itr,
+					    0,1,gametes,mvector,
 					    neutral,selected );
 
 
@@ -147,16 +135,16 @@ BOOST_AUTO_TEST_CASE( three_point_cross_1 )
   */
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.1;
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.1;
 			     } ) != neutral.end());
 
   for( auto d : {0.5,0.9} )
     {
       BOOST_CHECK( std::find_if( neutral.begin(),
 				 neutral.end(),
-				 [d]( std::list<mut>::iterator __m ) {
-				   return __m->pos == d;
+				 [d,&mvector]( std::size_t __m ) {
+				   return mvector[__m].pos == d;
 				 } ) == neutral.end() );
 
     }
@@ -170,14 +158,12 @@ BOOST_AUTO_TEST_CASE( three_point_cross_2 )
   //g1: neutral muts at 0.1,0.5
   //g2: neutral muts at 0.9
   gtype g1(1),g2(1);
-  std::list<mut> mlist;
-
-  auto mitr = mlist.insert(mlist.end(),mut(0.1,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
-  mitr = mlist.insert(mlist.end(),mut(0.5,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g1);
-  mitr = mlist.insert(mlist.end(),mut(0.9,0.,1));
-  KTfwd::fwdpp_internal::add_new_mutation(mitr,g2);
+  std::vector<mut> mvector(1,mut(0.1,0.));
+  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
+  mvector.emplace_back(0.5,0.);
+  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g1);
+  mvector.emplace_back(0.9,0.);
+  KTfwd::fwdpp_internal::add_new_mutation(2,mvector,g2);
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 2 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
@@ -191,16 +177,10 @@ BOOST_AUTO_TEST_CASE( three_point_cross_2 )
   //We need a "gamete pool"
   std::vector<gtype> gametes = { g1, g2 };
 
-  //get pointers/iterators to our two existing gametes
-  auto g1_itr = gametes.begin(),
-    g2_itr = g1_itr+1;
-
-  //gtype ng1(1),ng2(1);
-
   //Needed as of 0.3.3
   gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
-					    g1_itr, g2_itr,
+					    0,1,gametes,mvector,
 					    neutral,selected );
 
 
@@ -210,16 +190,16 @@ BOOST_AUTO_TEST_CASE( three_point_cross_2 )
   */
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     []( std::list<mut>::iterator __m ) {
-			       return __m->pos == 0.1;
+			     [&mvector]( std::size_t __m ) {
+			       return mvector[__m].pos == 0.1;
 			     } ) != neutral.end());
 
   for( auto d : {0.5,0.9} )
     {
       BOOST_CHECK( std::find_if( neutral.begin(),
 				 neutral.end(),
-				 [d]( std::list<mut>::iterator __m ) {
-				   return __m->pos == d;
+				 [d,&mvector]( std::size_t __m ) {
+				   return mvector[__m].pos == d;
 				 } ) == neutral.end() );
 
     }
