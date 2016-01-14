28b598cba0102041e6ec42e6097a58ad41221b27
Kevin R. Thornton <krthornt@uci.edu>
2016-01-14 10:05:14 -0800

    added API tests of fitness model assignment.

--- unit/siteDepFitness.cc
+++ unit/siteDepFitness.cc
@@ -2,20 +2,22 @@
   \file siteDepFitness.cc
   \brief Unit tests of KTfwd::site_dependent_fitness
   \ingroup unit
- */
+*/
 
 #define BOOST_TEST_MODULE siteDepFitness
 #define BOOST_TEST_DYN_LINK 
 
 #include <config.h>
-#include <fwdpp/diploid.hh>
-#include <boost/test/unit_test.hpp>
 #include <unistd.h>
 #include <iterator>
 #include <functional>
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <fwdpp/diploid.hh>
+#include <fwdpp/sugar/singlepop.hpp>
+#include <boost/test/unit_test.hpp>
+#include <custom_dip.hpp>
 
 using mut = KTfwd::mutation;
 using gtype = KTfwd::gamete;
@@ -30,7 +32,7 @@ using gvector = std::vector<gtype>;
 
   However, it does not.  Even with the bug, the remaining bit of the function
   gets the calculation right.  Yay!
- */
+*/
 BOOST_AUTO_TEST_CASE( simple_multiplicative1 )
 {
   gtype g1(1),g2(1);
@@ -58,7 +60,7 @@ BOOST_AUTO_TEST_CASE( simple_multiplicative1 )
 
 /*
   g2 has it, g1 does not
- */
+*/
 BOOST_AUTO_TEST_CASE( simple_multiplicative2 )
 {
   gtype g1(1),g2(1);
@@ -160,5 +162,135 @@ BOOST_AUTO_TEST_CASE( simple_additive_1 )
   gvector g{g1,g2};
 
   double w = KTfwd::additive_diploid()(g[0],g[1],mutations);
-  BOOST_CHECK_EQUAL(w,1.2);
+  BOOST_CHECK_EQUAL(w,1.2);  
+}
+
+/*
+  API checks on fitness policies.
+
+  Below, we test the ability to assign bound fitness models to variables
+  and then reassign them.
+
+  We make use of types defined by population objects from the "sugar" layer.
+*/
+
+BOOST_AUTO_TEST_CASE( reassign_test_1 )
+{
+  //Tests using a "standard" diploid type
+  using poptype = KTfwd::singlepop<mut>;
+  
+  {
+    //Test reassignment of the SAME fitness model type
+    //Multiplicative model first
+    
+    //assign a fitness model with default scaling = 1.
+    poptype::fitness_t dipfit = std::bind(KTfwd::multiplicative_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+    
+    //Now, reassign it with scaling = 2.
+    dipfit = std::bind(KTfwd::multiplicative_diploid(),
+		       std::placeholders::_1,
+		       std::placeholders::_2,
+		       std::placeholders::_3,
+		       2.);
+  }
+
+  {
+    //Test reassignment of the SAME fitness model type
+    //Now the additive model
+    
+    //assign a fitness model with default scaling = 1.
+    poptype::fitness_t dipfit = std::bind(KTfwd::additive_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+    //Now, reassign it with scaling = 2.
+    dipfit = std::bind(KTfwd::additive_diploid(),
+		       std::placeholders::_1,
+		       std::placeholders::_2,
+		       std::placeholders::_3,
+		       2.);
+  }
+
+  {
+    //Convert a multiplicative model to an additive model
+    //The application of this is a program using the library
+    //that wants to decide which model to use based on parameters
+    //passed in by a user.
+    
+    poptype::fitness_t dipfit = std::bind(KTfwd::multiplicative_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+
+    //Now, reassign it to addtive model with scaling = 2.
+    dipfit = std::bind(KTfwd::additive_diploid(),
+		       std::placeholders::_1,
+		       std::placeholders::_2,
+		       std::placeholders::_3,
+		       2.);
+  }
+}
+
+BOOST_AUTO_TEST_CASE( reassign_test_2 )
+{
+  //Tests using a custom diploid type
+  using poptype = KTfwd::singlepop<mut,diploid_t>;
+  //using poptype = KTfwd::singlepop<mut>;
+
+  {
+    //Test reassignment of the SAME fitness model type
+    //Multiplicative model first
+
+    //assign a fitness model with default scaling = 1.
+    poptype::fitness_t dipfit = std::bind(KTfwd::multiplicative_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+
+    //Now, reassign it with scaling = 2.
+    dipfit = std::bind(KTfwd::multiplicative_diploid(),
+    		       std::placeholders::_1,
+    		       std::placeholders::_2,
+    		       std::placeholders::_3,
+    		       2.);
+  }
+
+  {
+    //Test reassignment of the SAME fitness model type
+    //Now the additive model
+    
+    //assign a fitness model with default scaling = 1.
+    poptype::fitness_t dipfit = std::bind(KTfwd::additive_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+    //Now, reassign it with scaling = 2.
+    dipfit = std::bind(KTfwd::additive_diploid(),
+    		       std::placeholders::_1,
+    		       std::placeholders::_2,
+    		       std::placeholders::_3,
+    		       2.);
+  }
+
+  {
+    //Convert a multiplicative model to an additive model
+    //The application of this is a program using the library
+    //that wants to decide which model to use based on parameters
+    //passed in by a user.
+    
+    poptype::fitness_t dipfit = std::bind(KTfwd::multiplicative_diploid(),
+					  std::placeholders::_1,
+					  std::placeholders::_2,
+					  std::placeholders::_3);
+
+    //Now, reassign it to addtive model with scaling = 2.
+    dipfit = std::bind(KTfwd::additive_diploid(),
+    		       std::placeholders::_1,
+    		       std::placeholders::_2,
+    		       std::placeholders::_3,
+    		       2.);
+  }
 }
