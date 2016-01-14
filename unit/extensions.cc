2d809ba2306009fe3ba4ea4df7f43a20e750a1a8
Kevin R. Thornton <krthornt@uci.edu>
2016-01-13 18:23:37 -0800

    ok, discrete_mut_model appears to work

--- unit/extensions.cc
+++ unit/extensions.cc
@@ -1,12 +1,15 @@
 /*
   \file extensions.cc
-  API checks on fwdpp's extensions sub-library
+  API checks on fwdpp's extensions sub-library.
+
+  This test passes if all blocks compile.
 */
 
 #define BOOST_TEST_MODULE extensions_test
 #define BOOST_TEST_DYN_LINK 
 
 #include <config.h>
+#include <iostream>
 #include <type_traits>
 #include <boost/test/unit_test.hpp>
 #include <fwdpp/diploid.hh>
@@ -83,3 +86,38 @@ BOOST_AUTO_TEST_CASE( discrete_mut_model_test_3 )
   static_assert( std::is_same<decltype(x),std::size_t>::value,
 		 "extensions::discrete_mut_model::make_muts must return a std::size_t" );
 }
+
+BOOST_AUTO_TEST_CASE( discrete_mut_model_test_4 )
+{
+  poptype pop(1000);
+
+  //attempt
+  extensions::discrete_mut_model dm({0,1},
+				    {1,2},
+				    {1,0.5},
+				    {},
+				    {},
+				    {},
+				    {}
+				    );
+  auto rb = fwdpp_internal::make_mut_queue(pop.mcounts);
+  KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2> rng(0u);
+  auto mmodel = std::bind(&extensions::discrete_mut_model::make_mut<KTfwd::recycling_bin_t<decltype(pop.mutations)>::type,decltype(pop.mut_lookup),decltype(pop.mutations)>,
+			  &dm,rng.get(),0.001,0.,0u,std::placeholders::_1,std::placeholders::_2,std::ref(pop.mut_lookup));
+  
+  auto wbar = KTfwd::sample_diploid(rng.get(),
+				    pop.gametes,  //non-const reference to gametes
+				    pop.diploids, //non-const reference to diploids
+				    pop.mutations, //non-const reference to mutations
+				    pop.mcounts,
+				    1000,     //current pop size, remains constant
+				    0.001,    //mutation rate per gamete
+				    mmodel,
+				    std::bind(KTfwd::poisson_xover(),rng.get(),0.001,0.,2.,
+					      std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),
+				    std::bind(KTfwd::multiplicative_diploid(),std::placeholders::_1,std::placeholders::_2,
+					      std::placeholders::_3,2.),
+				    pop.neutral,
+				    pop.selected);
+}
+
