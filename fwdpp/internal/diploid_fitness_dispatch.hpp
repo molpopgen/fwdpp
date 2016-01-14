43c8593a97bc284e32ec6ca09654607ec809d15a
Kevin R. Thornton <krthornt@uci.edu>
2016-01-13 17:49:04 -0800

    comment out un-needed dispatch fxn

--- fwdpp/internal/diploid_fitness_dispatch.hpp
+++ fwdpp/internal/diploid_fitness_dispatch.hpp
@@ -29,16 +29,16 @@ namespace KTfwd {
     }
 
     //! Custom diploids
-    template<typename fitness_policy_type,
-	     typename diploid_itr_t,
-	     typename gcont_t,
-	     typename mcont_t>
-    inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d,
-					     const gcont_t & gametes,const mcont_t & mutations,
-					     std::true_type,
-					     typename std::enable_if< traits::is_custom_diploid_t<typename diploid_itr_t::value_type>::type >::type()) { 
-      return fp(d,gametes,mutations);
-    }
+    // template<typename fitness_policy_type,
+    // 	     typename diploid_itr_t,
+    // 	     typename gcont_t,
+    // 	     typename mcont_t>
+    // inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d,
+    // 					     const gcont_t & gametes,const mcont_t & mutations,
+    // 					     std::true_type,
+    // 					     typename std::enable_if< traits::is_custom_diploid_t<typename diploid_itr_t::value_type>::type >::type()) { 
+    //   return fp(d,gametes,mutations);
+    // }
     
   }
 }
