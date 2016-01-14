28525b539d7a20e281144e1d471937fc6f9f3e3c
Kevin R. Thornton <krthornt@uci.edu>
2016-01-14 10:14:13 -0800

    make custom diploid type re-usable in unit tests

--- unit/sugar2_custom.cc
+++ unit/sugar2_custom.cc
@@ -13,48 +13,11 @@
 #include <fwdpp/sugar/metapop.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/serialization.hpp>
+#include <custom_dip.hpp>
 
 using mutation_t = KTfwd::popgenmut;
 using mwriter = KTfwd::mutation_writer;
 using mreader = KTfwd::mutation_reader<mutation_t>;
-
-//Custom diploid type.
-struct diploid_t : public KTfwd::tags::custom_diploid_t
-{
-  using first_type = std::size_t;
-  using second_type = std::size_t;
-  first_type first;
-  second_type second;
-  unsigned i;
-  diploid_t() : first(first_type()),second(second_type()),i(std::numeric_limits<unsigned>::max()) {}
-  diploid_t(first_type g1, first_type g2) : first(g1),second(g2),i(std::numeric_limits<unsigned>::max()) {}
-  bool operator==(const diploid_t & rhs) const
-  {
-    return this->first == rhs.first &&
-    this->second == rhs.second &&
-    this->i == rhs.i;
-  }
-};
-
-struct diploid_writer
-{
-  using result_type = void;
-  template<typename itr,typename streamtype>
-  inline result_type operator()( itr i, streamtype & o ) const
-  {
-    o.write( reinterpret_cast<const char *>(&i.i),sizeof(unsigned) );
-  }
-};
-
-struct diploid_reader
-{
-  using result_type = void;
-  template<typename itr,typename streamtype>
-  inline result_type operator()( itr i, streamtype & in ) const
-  {
-    in.read( reinterpret_cast<char *>(&i.i),sizeof(unsigned) );
-  }
-};
 
 size_t migpop(const size_t & source_pop, gsl_rng * r, const double & mig_prob)
 {
