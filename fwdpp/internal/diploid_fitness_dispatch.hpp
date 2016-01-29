bbe7d982fbfbce9528c2016619523c3572aa10cb
Kevin R. Thornton <krthornt@uci.edu>
2016-01-29 14:47:21 -0800

    cleanup typenames and unused code

--- fwdpp/internal/diploid_fitness_dispatch.hpp
+++ fwdpp/internal/diploid_fitness_dispatch.hpp
@@ -8,38 +8,25 @@ namespace KTfwd {
   namespace fwdpp_internal{
     //! Standard diploids
     template<typename fitness_policy_type,
-	     typename diploid_itr_t,
+	     typename diploid_t,
 	     typename gcont_t,
 	     typename mcont_t>
-    inline double diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d,
+    inline double diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_t & d,
 					    const gcont_t & gametes,const mcont_t & mutations,
 					    std::false_type ) {
       return fp(gametes[d.first],gametes[d.second],mutations);
     }
-    
-    //! Custom diploids
-    template<typename fitness_policy_type,
-	     typename diploid_itr_t,
-	     typename gcont_t,
-	     typename mcont_t>
-    inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d,
-					     const gcont_t & gametes,const mcont_t & mutations,
-					     std::true_type ) { 
-      return fp(d,gametes,mutations);
-    }
 
     //! Custom diploids
-    // template<typename fitness_policy_type,
-    // 	     typename diploid_itr_t,
-    // 	     typename gcont_t,
-    // 	     typename mcont_t>
-    // inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_itr_t & d,
-    // 					     const gcont_t & gametes,const mcont_t & mutations,
-    // 					     std::true_type,
-    // 					     typename std::enable_if< traits::is_custom_diploid_t<typename diploid_itr_t::value_type>::type >::type()) { 
-    //   return fp(d,gametes,mutations);
-    // }
-    
+    template<typename fitness_policy_type,
+	     typename diploid_t,
+	     typename gcont_t,
+	     typename mcont_t>
+    inline double  diploid_fitness_dispatch( const fitness_policy_type & fp, const diploid_t & d,
+					     const gcont_t & gametes,const mcont_t & mutations,
+					     std::true_type ) {
+      return fp(d,gametes,mutations);
+    }
   }
 }
 
