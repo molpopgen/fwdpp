f2ac190e095d7e1233b7eec595593dc095b31200
Kevin R. Thornton <krthornt@uci.edu>
2016-02-08 11:43:13 -0800

    add update_mutations_n

--- fwdpp/util.hpp
+++ fwdpp/util.hpp
@@ -98,5 +98,38 @@ namespace KTfwd
 	if(!mcounts[i]) lookup.erase(mutations[i].pos);
       }
   }
+
+    /*!
+    Label all fixed neutral variant and all extinct variants for recycling. Copy fixations and fixation times
+    for neutral mutations into containers.
+
+    \note: lookup must be compatible with lookup->erase(lookup->find(double))
+  */
+  template<typename mutation_list_type,
+	   typename fixation_container_t,
+	   typename fixation_time_container_t,
+	   typename mutation_lookup_table>
+  void update_mutations_n( mutation_list_type & mutations,
+			 fixation_container_t & fixations,
+			 fixation_time_container_t & fixation_times,
+			 mutation_lookup_table & lookup,
+			 std::vector<uint_t> & mcounts,
+			 const unsigned & generation,const unsigned & twoN )
+  {
+    static_assert( typename traits::is_mutation_t<typename mutation_list_type::value_type>::type(),
+		   "mutation_type must be derived from KTfwd::mutation_base" );
+    for(unsigned i=0;i<mcounts.size();++i)
+      {
+	assert(mcounts[i] <= twoN);
+	if(mutations[i].neutral&&mcounts[i]==twoN)
+	  {
+	    fixations.push_back(mutations[i]);
+	    fixation_times.push_back(generation);
+	    mcounts[i]=0; //set count to zero to mark mutation as "recyclable"
+	    lookup.erase(mutations[i].pos);
+	  }
+	if(!mcounts[i]) lookup.erase(mutations[i].pos);
+      }
+  }
 }
 #endif /* _UTIL_HPP_ */
