e8cda5d39c136dd6f671a5c1c774e7e8a21a5bb5
Kevin R. Thornton <krthornt@uci.edu>
2016-02-12 11:54:57 -0800

    correctness testing for single demes

--- unit/sugar_sampling.cc
+++ unit/sugar_sampling.cc
@@ -12,6 +12,7 @@
 #include <fwdpp/sugar/GSLrng_t.hpp>
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/metapop.hpp>
+#include <fwdpp/sugar/multiloc.hpp>
 #include <fwdpp/sugar/sampling.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 
@@ -19,7 +20,8 @@ using mutation_t = KTfwd::popgenmut;
 
 using singlepop_t = KTfwd::singlepop<mutation_t>;
 using metapop_t = KTfwd::metapop<mutation_t>;
-
+using multiloc_t = KTfwd::multiloc<mutation_t>;
+  
 KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2> rng(0u);
 
 //This is an API check
@@ -162,3 +164,62 @@ BOOST_AUTO_TEST_CASE( metapop8 )
   BOOST_REQUIRE_THROW(auto s = KTfwd::sample(pop,2,std::vector<unsigned>({1000}),true),
 		      std::out_of_range);
 }
+
+//Correctness tests
+
+BOOST_AUTO_TEST_CASE( singlepop_1 )
+{
+  singlepop_t pop(1000);
+  pop.mutations.emplace_back( 0.1,0,0,0 );
+  pop.mcounts.emplace_back(1);
+  pop.gametes.emplace_back( 1, std::vector<std::size_t>{0}, std::vector<std::size_t>{} );
+  pop.gametes[0].n--;
+  BOOST_REQUIRE_EQUAL( KTfwd::check_sum(pop.gametes,2000), true );
+  BOOST_REQUIRE_EQUAL( KTfwd::popdata_sane(pop.diploids,pop.gametes,pop.mcounts), true );
+  pop.diploids[1].first=1;
+
+  auto x = KTfwd::sample(pop,{0,1},true);
+
+  BOOST_REQUIRE_EQUAL(x.size(),1);
+  BOOST_REQUIRE_EQUAL(std::count(x[0].second.begin(),x[0].second.end(),'1'),1);
+  BOOST_REQUIRE_EQUAL(x[0].second[2],'1');
+  //Now, make that diploid homozygous for the mutation
+  //(Note: the pop data are now inconsistent...)
+  pop.diploids[1].second=1;
+
+  x = KTfwd::sample(pop,{0,1},true);
+  BOOST_REQUIRE_EQUAL(x.size(),1);
+  BOOST_REQUIRE_EQUAL(std::count(x[0].second.begin(),x[0].second.end(),'1'),2);
+  BOOST_REQUIRE_EQUAL(x[0].second[2],'1');
+  BOOST_REQUIRE_EQUAL(x[0].second[3],'1');
+
+  //Now, we are only sampling individual 1, and thus
+  //the variant at 0.1 will be fixed in the sample,
+  //and the sample should be empty
+  x = KTfwd::sample(pop,{1},true);
+  BOOST_REQUIRE_EQUAL(x.empty(),true);
+
+  //Now, allow fixed variants in the sample
+  x = KTfwd::sample(pop,{1},false);
+  BOOST_REQUIRE_EQUAL(x.empty(),false);
+
+  //now, add a fixation with position < 0.1
+  pop.fixations.emplace_back(-0.1,0,0,0);
+
+  //Now, we sample, but we don't want the fixations...
+  x = KTfwd::sample(pop,{0,1},true);
+  BOOST_REQUIRE_EQUAL(x.size(),1);
+  BOOST_REQUIRE_EQUAL(std::count(x[0].second.begin(),x[0].second.end(),'1'),2);
+  BOOST_REQUIRE_EQUAL(x[0].second[2],'1');
+  BOOST_REQUIRE_EQUAL(x[0].second[3],'1');
+
+  //Now, we want the fixation
+  x = KTfwd::sample(pop,{0,1},false);
+  BOOST_REQUIRE_EQUAL(x.size(),2);
+  BOOST_REQUIRE_EQUAL(x[0].first,-0.1);
+  BOOST_REQUIRE_EQUAL(x[1].first,0.1);
+  BOOST_REQUIRE_EQUAL( std::is_sorted(x.begin(),x.end(),[](const KTfwd::sample_site_t & i,const KTfwd::sample_site_t & j) {
+	return i.first<j.first;
+      }), true);
+}
+
