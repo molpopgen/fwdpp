5dc0dff1fa5d303f9eff63b3ab9d22108dc9002e
Kevin R. Thornton <krthornt@uci.edu>
2016-03-15 16:41:20 -0700

    more idiomatic design w/overloaded call operators leads to less code replication

--- fwdpp/internal/gamete_cleaner.hpp
+++ fwdpp/internal/gamete_cleaner.hpp
@@ -49,77 +49,83 @@ namespace KTfwd {
       return std::find_if(beg,end,[](const typename gcont_t_iterator::value_type & g) noexcept { return g.n; });
     }
 
+    struct find_fixation
     //! Find the first mutation in a gamete that is also a fixation
-    template<typename mut_index_cont,typename mcounts_t>
-    inline typename mut_index_cont::const_iterator find_fixation(const mut_index_cont & mc,
-								 const mcounts_t & mcounts,
-								 const uint_t twoN) noexcept
     {
-      return std::find_if(mc.cbegin(),mc.cend(),[&mcounts,&twoN](const std::size_t & i) noexcept
-			  {
-			    return mcounts[i]==twoN;
-			  });
-    }
+      template<typename mut_index_cont,typename mcounts_t>
+      inline typename mut_index_cont::const_iterator operator()(const mut_index_cont & mc,
+								const mcounts_t & mcounts,
+								const uint_t twoN) const noexcept
+      {
+	return std::find_if(mc.cbegin(),mc.cend(),[&mcounts,&twoN](const std::size_t & i) noexcept
+			    {
+			      return mcounts[i]==twoN;
+			    });
+      }
+      
+      template<typename mut_index_cont,typename mcont_t,
+	       typename mcounts_t,typename mutation_removal_policy>
+      inline typename mut_index_cont::const_iterator operator()(const mut_index_cont & mc,
+								const mcont_t & mutations,
+								const mcounts_t & mcounts,
+								const uint_t twoN,
+								mutation_removal_policy && mp) const noexcept
+      {
+	return std::find_if(mc.cbegin(),mc.cend(),[&mutations,&mcounts,&twoN,&mp](const std::size_t & i) noexcept
+			    {
+			      return mcounts[i]==twoN && mp(mutations[i]);
+			    });
+      }
+    };
 
+    struct gamete_cleaner_erase_remove_idiom_wrapper
     //! Wrapper for erase/remove idiom.
-    template<typename mut_index_cont,typename mcounts_t>
-    inline void gamete_cleaner_erase_remove_idiom_wrapper( mut_index_cont & mc,
-							   const mcounts_t & mcounts,
-							   const typename mut_index_cont::value_type & first_fixation,
-							   const uint_t twoN ) noexcept
     {
-      /*
-	The first call to std::find relies on a de-referencing of the return value of 
-	find_fixation, passed to this function as first_fixation.  Because mc is sorted according to mutation position, 
-	first_fixation is the fixation with the smallest position.  Using std::find like this avoids some calls to the lambda
-	expression, which experiences cache-misses because mcounts is NOT sorted according to mutation position.
-      */
-      mc.erase(std::remove_if(std::find(mc.begin(),mc.end(),first_fixation),
-			      mc.end(),[&mcounts,&twoN](const typename mut_index_cont::value_type & i) noexcept
-			      {
-				return mcounts[i]==twoN;
-			      }),mc.end());
-    }
+      template<typename mut_index_cont,typename mcounts_t>
+      inline void operator()(mut_index_cont & mc,
+			     const mcounts_t & mcounts,
+			     const typename mut_index_cont::value_type & first_fixation,
+			     const uint_t twoN) const noexcept
+      //! Overload for no custom policy
+      {
+	/*
+	  The first call to std::find relies on a de-referencing of the return value of 
+	  find_fixation, passed to this function as first_fixation.  Because mc is sorted according to mutation position, 
+	  first_fixation is the fixation with the smallest position.  Using std::find like this avoids some calls to the lambda
+	  expression, which experiences cache-misses because mcounts is NOT sorted according to mutation position.
+	*/
+	mc.erase(std::remove_if(std::find(mc.begin(),mc.end(),first_fixation),
+				mc.end(),[&mcounts,&twoN](const typename mut_index_cont::value_type & i) noexcept
+				{
+				  return mcounts[i]==twoN;
+				}),mc.end());
+      }
 
-    //! Find the first mutation in a gamete that is also a fixation AND satisfies the policy
-    template<typename mut_index_cont,typename mcont_t,
-	     typename mcounts_t,typename mutation_removal_policy>
-    inline typename mut_index_cont::const_iterator find_fixation_pol(const mut_index_cont & mc,
-								     const mcont_t & mutations,
-								     const mcounts_t & mcounts,
-								     const uint_t twoN,
-								     mutation_removal_policy && mp) noexcept
-    {
-      return std::find_if(mc.cbegin(),mc.cend(),[&mutations,&mcounts,&twoN,&mp](const std::size_t & i) noexcept
-			  {
-			    return mcounts[i]==twoN && mp(mutations[i]);
-			  });
-    }
-
-    //! Wrapper for erase/remove idiom with a custom policy
-    template<typename mut_index_cont,
-	     typename mcont_t,
-	     typename mcounts_t,
-	     typename mutation_removal_policy>
-    inline void gamete_cleaner_erase_remove_idiom_wrapper_pol( mut_index_cont & mc,
-							       const mcont_t & mutations,
-							       const mcounts_t & mcounts,
-							       const typename mut_index_cont::value_type & first_fixation,
-							       const uint_t twoN,
-							       mutation_removal_policy && mp) noexcept
-    {
-      /*
-	The first call to std::find relies on a de-referencing of the return value of 
-	find_fixation, passed to this function as first_fixation.  Because mc is sorted according to mutation position, 
-	first_fixation is the fixation with the smallest position.  Using std::find like this avoids some calls to the lambda
-	expression, which experiences cache-misses because mcounts is NOT sorted according to mutation position.
-      */
-      mc.erase(std::remove_if(std::find(mc.begin(),mc.end(),first_fixation),
-			      mc.end(),[&mcounts,&mutations,&twoN,&mp](const typename mut_index_cont::value_type & i) noexcept
-			      {
-				return mcounts[i]==twoN && mp(mutations[i]);
-			      }),mc.end());
-    }
+      template<typename mut_index_cont,
+	       typename mcont_t,
+	       typename mcounts_t,
+	       typename mutation_removal_policy>
+      inline void operator()( mut_index_cont & mc,
+			      const mcont_t & mutations,
+			      const mcounts_t & mcounts,
+			      const typename mut_index_cont::value_type & first_fixation,
+			      const uint_t twoN,
+			      mutation_removal_policy && mp) const noexcept
+      //! Overload for custom policy.
+      {
+	/*
+	  The first call to std::find relies on a de-referencing of the return value of 
+	  find_fixation, passed to this function as first_fixation.  Because mc is sorted according to mutation position, 
+	  first_fixation is the fixation with the smallest position.  Using std::find like this avoids some calls to the lambda
+	  expression, which experiences cache-misses because mcounts is NOT sorted according to mutation position.
+	*/
+	mc.erase(std::remove_if(std::find(mc.begin(),mc.end(),first_fixation),
+				mc.end(),[&mcounts,&mutations,&twoN,&mp](const typename mut_index_cont::value_type & i) noexcept
+				{
+				  return mcounts[i]==twoN && mp(mutations[i]);
+				}),mc.end());
+      }
+    };
 
     /*! \brief Handles removal of indexes to mutations from gametes after sampling
       Intended use is when std::is_same< mutation_removal_policy, KTfwd::remove_nothing >::type is true.
@@ -133,27 +139,21 @@ namespace KTfwd {
       return;
     }
 
-    /*
-      Now, we have the two overloads of gamete_cleaner.
-
-      Future versions should consider merging into one policy-based function
-      called by two different wrappers.  For now, they are kept separate.
+    template<typename gcont_t,
+	     typename fixation_finder,
+	     typename idiom_wrapper>
+    inline void gamete_cleaner_details( gcont_t & gametes,
+					const fixation_finder & ff,
+					const idiom_wrapper & iw ) noexcept
+    /*!
+      The two overloads of gamete_cleaner dispatch the above policies into this function.
     */
-    
-    /*! \brief Handles removal of indexes to mutations from gametes after sampling
-      Intended use is when std::is_same< mutation_removal_policy, KTfwd::true_type >::type is true.
-      Called by KTfwd::sample_diploid
-    */
-    template<typename gcont_t,typename mcont_t,typename mutation_removal_policy>
-    inline typename std::enable_if< std::is_same<mutation_removal_policy,std::true_type>::value >::type
-    gamete_cleaner(gcont_t & gametes, const mcont_t &, const std::vector<uint_t> & mcounts,
-		   const uint_t twoN, const mutation_removal_policy &)
     {
       auto extant_gamete = next_extant_gamete(gametes.begin(),gametes.end());
       if(extant_gamete==gametes.end()) return;
-      const auto fixation_n = find_fixation(extant_gamete->mutations,mcounts,twoN);
+      const auto fixation_n = ff(extant_gamete->mutations);
       bool neutral_fixations_exist = (fixation_n!=extant_gamete->mutations.cend());
-      const auto fixation_s = find_fixation(extant_gamete->smutations,mcounts,twoN);
+      const auto fixation_s = ff(extant_gamete->smutations);
       bool selected_fixations_exist = (fixation_s!=extant_gamete->smutations.cend());
       if(!neutral_fixations_exist && !selected_fixations_exist) return;
 
@@ -163,17 +163,38 @@ namespace KTfwd {
       const auto fixation_s_value = (fixation_s==extant_gamete->smutations.cend()) ? typename decltype(fixation_s)::value_type() : *fixation_s;
       while(extant_gamete < gend)
 	{
-
 	  if(neutral_fixations_exist)
 	    {
-	      gamete_cleaner_erase_remove_idiom_wrapper(extant_gamete->mutations,mcounts,fixation_n_value,twoN);
+	      iw(extant_gamete->mutations,fixation_n_value);
 	    }
 	  if(selected_fixations_exist)
 	    {
-	      gamete_cleaner_erase_remove_idiom_wrapper(extant_gamete->smutations,mcounts,fixation_s_value,twoN);
+	      iw(extant_gamete->smutations,fixation_s_value);
 	    }
 	  extant_gamete = next_extant_gamete(extant_gamete+1,gend);
 	}
+    }
+
+    
+    /*
+      Now, we have the two overloads of gamete_cleaner.
+      The std::cref(...) below are required, o/w copies will get bound, 
+      which is bad for performance.
+    */
+
+    /*! \brief Handles removal of indexes to mutations from gametes after sampling
+      Intended use is when std::is_same< mutation_removal_policy, KTfwd::true_type >::type is true.
+      Called by KTfwd::sample_diploid
+    */
+    template<typename gcont_t,typename mcont_t,typename mutation_removal_policy>
+    inline typename std::enable_if< std::is_same<mutation_removal_policy,std::true_type>::value >::type
+    gamete_cleaner(gcont_t & gametes, const mcont_t &, const std::vector<uint_t> & mcounts,
+		   const uint_t twoN, const mutation_removal_policy &)
+    {
+      gamete_cleaner_details(gametes,
+			     std::bind(find_fixation(),std::placeholders::_1,std::cref(mcounts),twoN),
+			     std::bind(gamete_cleaner_erase_remove_idiom_wrapper(),
+				       std::placeholders::_1,std::cref(mcounts),std::placeholders::_2,twoN));
     }
 
     /*! \brief Handles removal of indexes to mutations from gametes after sampling
@@ -188,34 +209,10 @@ namespace KTfwd {
     gamete_cleaner(gcont_t & gametes, const mcont_t & mutations,const std::vector<uint_t> & mcounts,
 		   const uint_t twoN, const mutation_removal_policy & mp)
     {
-      auto extant_gamete = next_extant_gamete(gametes.begin(),gametes.end());
-      if(extant_gamete==gametes.end()) return;
-      const auto fixation_n = find_fixation_pol(extant_gamete->mutations,mutations,mcounts,twoN,std::forward<decltype(mp)>(mp));
-      bool neutral_fixations_exist = (fixation_n!=extant_gamete->mutations.cend());
-      const auto fixation_s = find_fixation_pol(extant_gamete->smutations,mutations,mcounts,twoN,std::forward<decltype(mp)>(mp));
-      bool selected_fixations_exist = (fixation_s!=extant_gamete->smutations.cend());
-      if(!neutral_fixations_exist && !selected_fixations_exist) return;
-
-      const auto gend = gametes.end();
-      //Assign values to avoid tons of de-referencing later
-      const auto fixation_n_value = (fixation_n==extant_gamete->mutations.cend()) ? typename decltype(fixation_n)::value_type() : *fixation_n;
-      const auto fixation_s_value = (fixation_s==extant_gamete->smutations.cend()) ? typename decltype(fixation_s)::value_type() : *fixation_s;
-      while(extant_gamete < gend)
-	{
-	  if(neutral_fixations_exist)
-	    {
-	      gamete_cleaner_erase_remove_idiom_wrapper_pol(extant_gamete->mutations,mutations,
-							    mcounts,fixation_n_value,
-							    twoN,std::forward<decltype(mp)>(mp));
-	    }
-	  if(selected_fixations_exist)
-	    {
-	      gamete_cleaner_erase_remove_idiom_wrapper_pol(extant_gamete->smutations,mutations,
-							    mcounts,fixation_s_value,
-							    twoN,std::forward<decltype(mp)>(mp));
-	    }
-	  extant_gamete = next_extant_gamete(extant_gamete+1,gend);
-	}
+      gamete_cleaner_details(gametes,
+			     std::bind(find_fixation(),std::placeholders::_1,std::cref(mutations),std::cref(mcounts),twoN,std::forward<decltype(mp)>(mp)),
+			     std::bind(gamete_cleaner_erase_remove_idiom_wrapper(),
+				       std::placeholders::_1,std::cref(mutations),std::cref(mcounts),std::placeholders::_2,twoN,std::forward<decltype(mp)>(mp)));
     }
   }
 }
