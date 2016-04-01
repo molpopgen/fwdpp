5519b9d754d1594a735e4b92d8ee55d15abeb9d3
Kevin R. Thornton <krthornt@uci.edu>
2016-04-01 11:36:25 -0700

    added test for metapop construction from singlepop with custom diploid type.

--- unit/sugar_metapop_custom_diploid.cc
+++ unit/sugar_metapop_custom_diploid.cc
@@ -10,6 +10,7 @@
 #include <boost/test/unit_test.hpp>
 #include <fwdpp/diploid.hh>
 #include <fwdpp/sugar/GSLrng_t.hpp>
+#include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/metapop.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/serialization.hpp>
@@ -28,7 +29,35 @@ size_t migpop(const size_t & source_pop, const gsl_rng * r, const double & mig_p
   return source_pop;
 }
 
+using spoptype = KTfwd::singlepop<mutation_t,diploid_t>;
 using poptype = KTfwd::metapop<mutation_t,diploid_t>;
+
+/*
+  These next two derived classes mimic what software 
+  packages like fwdpy do, which is to extend sugart types
+  with data that they need.
+ */
+struct spop_derived : public spoptype
+{
+  unsigned generation;
+  spop_derived(unsigned N) : spoptype(N),generation(0)
+  {
+  }
+};
+
+struct mpop_derived : public poptype
+{
+  unsigned generation;
+  mpop_derived(std::initializer_list<unsigned> & Ns) : poptype(Ns),generation(0)
+  {
+  }
+  mpop_derived(const spop_derived & s) : poptype(s),generation(s.generation)
+  {
+  }
+  mpop_derived(spop_derived && s) : poptype(std::move(s)),generation(s.generation)
+  {
+  }
+};
 
 void simulate(poptype & pop)
 {
@@ -97,4 +126,30 @@ BOOST_AUTO_TEST_CASE( metapop_sugar_custom_test4 )
   simulate(pop);
   poptype pop2=std::move(pop);
   BOOST_CHECK_EQUAL(pop==pop2,false);
+}
+
+//Test construction from singlepops
+BOOST_AUTO_TEST_CASE(copy_construct_spop_derived)
+{
+  spop_derived pop(1000);
+  BOOST_REQUIRE_EQUAL(pop.N,1000);
+  mpop_derived mpop(pop);
+  BOOST_REQUIRE_EQUAL(mpop.Ns.size(),1);
+  BOOST_REQUIRE_EQUAL(mpop.Ns[0],1000);
+  BOOST_REQUIRE_EQUAL(mpop.diploids.size(),1);
+  BOOST_REQUIRE_EQUAL(mpop.diploids[0].size(),1000);
+  BOOST_REQUIRE_EQUAL(pop.mutations==mpop.mutations,true);
+  BOOST_REQUIRE_EQUAL(pop.gametes==mpop.gametes,true);
+  BOOST_REQUIRE_EQUAL(pop.diploids==mpop.diploids[0],true);
+}
+
+BOOST_AUTO_TEST_CASE(move_construct_spop_derived)
+{
+  spop_derived pop(1000);
+  BOOST_REQUIRE_EQUAL(pop.N,1000);
+  mpop_derived mpop(std::move(pop));
+  BOOST_REQUIRE_EQUAL(mpop.Ns.size(),1);
+  BOOST_REQUIRE_EQUAL(mpop.Ns[0],1000);
+  BOOST_REQUIRE_EQUAL(mpop.diploids.size(),1);
+  BOOST_REQUIRE_EQUAL(mpop.diploids[0].size(),1000);
 }
