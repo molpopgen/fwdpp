0422b797914acc649b172777db648f59176c694c
Kevin R. Thornton <krthornt@uci.edu>
2016-04-08 14:20:29 -0700

    add_mutation implemented for mult-locus sims.  unit test updated

--- fwdpp/sugar/util.hpp
+++ fwdpp/sugar/util.hpp
@@ -17,9 +17,43 @@ namespace KTfwd
 {
   namespace fwdpp_internal
   {
+    template<typename mcont_t,
+	     typename mcounts_t>
+    std::size_t get_mut_index(mcont_t & mutations,
+			      mcounts_t & mcounts,
+			      typename mcont_t::value_type & new_mutation)
+    /*!
+      \brief Helper function for implementing KTfwd::add_mutation
+
+      This function puts the new mutation into the mutations container and 
+      updates other objects as needed.
+
+      \ingroup sugar
+     */
+    {
+      //find an extinct mutation, if one exists
+      auto extinct_mut = std::find(std::begin(mcounts),std::end(mcounts),0);
+      std::size_t mindex=mcounts.size();  //this will be the correct value if we use the else block below
+      if( extinct_mut != std::end(mcounts) ) //then we can recycle
+	{
+	  auto dist=std::distance(std::begin(mcounts),extinct_mut);
+	  mutations[dist] = std::move(new_mutation); //move new mutation into place
+	  mindex=std::size_t(dist); //update our value
+	}
+      else
+	{
+	  //cannot recycle, so add it to end
+	  mutations.emplace_back(std::move(new_mutation));
+	  mcounts.push_back(0); //Add a place for this variant
+	}
+      return mindex;
+    }
+    
     template<typename poptype,
 	     typename dipvector_t>
     /*!
+      \brief Overload for single-deme sims and metapop sims.
+
       This function def'n looks funny b/c we use 
       diploids to refer both to the diploids in a single-deme sim
       or the diploids in a particular deme of a meta-pop sim.
@@ -34,24 +68,10 @@ namespace KTfwd
 			       const std::vector<std::size_t> & indlist,
 			       const std::vector<short> & clist)
     {
-      //find an extinct mutation, if one exists
-      auto extinct_mut = std::find(std::begin(p.mcounts),std::end(p.mcounts),0);
       auto gam_recycling_bin = fwdpp_internal::make_gamete_queue(p.gametes);
 
       //Add new mutation to mutation container and track its location
-      std::size_t mindex=p.mcounts.size();  //this will be the correct value if we use the else block below
-      if( extinct_mut != std::end(p.mcounts) ) //then we can recycle
-	{
-	  auto dist=std::distance(std::begin(p.mcounts),extinct_mut);
-	  p.mutations[dist] = std::move(new_mutation); //move new mutation into place
-	  mindex=std::size_t(dist); //update our value
-	}
-      else
-	{
-	  //cannot recycle, so add it to end
-	  p.mutations.emplace_back(std::move(new_mutation));
-	  p.mcounts.push_back(0); //Add a place for this variant
-	}
+      auto mindex = get_mut_index(p.mutations,p.mcounts,new_mutation);
 
       //Function object for calls to upper bound
       auto inserter = [&p](const double & __value,const std::size_t __mut) noexcept {
@@ -112,6 +132,87 @@ namespace KTfwd
 	    }
 	}
     }
+
+    template<typename poptype,
+	     typename dipvector_t>
+    /*!
+      \brief Overload for adding mutations into single-deme multi-locus populations
+
+      \note Do not call directly.  Use KTfwd::add_mutation instead.
+
+      \ingroup sugar
+    */
+    void add_mutation_details( poptype & p,
+			       dipvector_t & diploids,
+			       const std::size_t locus,
+			       typename poptype::mcont_t::value_type & new_mutation,
+			       const std::vector<std::size_t> & indlist,
+			       const std::vector<short> & clist)
+    {
+      auto gam_recycling_bin = fwdpp_internal::make_gamete_queue(p.gametes);
+
+      //Add new mutation to mutation container and track its location
+      auto mindex = get_mut_index(p.mutations,p.mcounts,new_mutation);
+
+      //Function object for calls to upper bound
+      auto inserter = [&p](const double & __value,const std::size_t __mut) noexcept {
+	assert(__mut<p.mutations.size());
+	return __value < p.mutations[__mut].pos;
+      };
+      
+      //update the diploids
+      bool neutral = p.mutations[mindex].neutral;
+      auto pos = p.mutations[mindex].pos;
+      for( std::size_t i = 0 ; i < indlist.size() ; ++i )
+	{
+	  if( clist[i] == 0 || clist[i] == 2 )
+	    {
+	      auto n = p.gametes[diploids[indlist[i]][locus].first].mutations;
+	      auto s = p.gametes[diploids[indlist[i]][locus].first].smutations;
+	      if(neutral)
+		{
+		  n.insert( std::upper_bound(n.begin(),
+					     n.end(),pos,
+					     inserter),
+			    mindex );
+		}
+	      else
+		{
+		  s.insert( std::upper_bound(s.begin(),
+					     s.end(),pos,
+					     inserter),
+			    mindex );
+		}
+	      diploids[indlist[i]][locus].first = fwdpp_internal::recycle_gamete(p.gametes,
+									  gam_recycling_bin,
+									  n,s);
+	      p.mcounts[mindex]++;
+	    }
+	  if(clist[i]>0)
+	    {
+	      auto n = p.gametes[diploids[indlist[i]][locus].second].mutations;
+	      auto s = p.gametes[diploids[indlist[i]][locus].second].smutations;
+	      if(p.mutations[mindex].neutral)
+		{
+		  n.insert( std::upper_bound(n.begin(),
+					     n.end(),pos,
+					     inserter),
+			    mindex );
+		}
+	      else
+		{
+		  s.insert( std::upper_bound(s.begin(),
+					     s.end(),pos,
+					     inserter),
+			    mindex );
+		}
+	      diploids[indlist[i]][locus].second = fwdpp_internal::recycle_gamete(p.gametes,
+									   gam_recycling_bin,
+									   n,s);
+	      p.mcounts[mindex]++;
+	    }
+	}
+    }
     
     template<typename poptype,
 	     class... Args>
@@ -121,16 +222,45 @@ namespace KTfwd
 				const std::vector<short> & clist,
 				KTfwd::sugar::METAPOP_TAG, //distpatch tag
 				Args&&... args)
+    /*! 
+      Dispatch of KTfwd::add_mutation for meta-populations
+
+      \ingroup sugar
+    */
     {
       //Before we go deep into creating objects, let's do some checks
       if(deme >= p.diploids.size()) throw std::out_of_range("deme index out of range");
       for(const auto & i : indlist)
 	{
-	  if(i>=p.diploids[deme].size()) throw std::out_of_range("indlist contains elements > p.diploids.size()");
+	  if(i>=p.diploids[deme].size()) throw std::out_of_range("indlist contains elements > p.diploids[deme].size()");
 	}
       //create a new mutation
       typename poptype::mcont_t::value_type new_mutant(std::forward<Args>(args)...);
       fwdpp_internal::add_mutation_details(p,p.diploids[deme],new_mutant,indlist,clist);
+    }
+
+    template<typename poptype,
+	     class... Args>
+    void add_mutation_dispatch( poptype & p,
+				const std::size_t locus,
+				const std::vector<std::size_t> & indlist,
+				const std::vector<short> & clist,
+				KTfwd::sugar::MULTILOCPOP_TAG, //distpatch tag
+				Args&&... args)
+    /*! 
+      Dispatch of KTfwd::add_mutation for single-deme, multi-locus simulations
+      \ingroup sugar
+    */
+    {
+      //Before we go deep into creating objects, let's do some checks
+      if(locus >= p.diploids[0].size()) throw std::out_of_range("locus index out of range");
+      for(const auto & i : indlist)
+	{
+	  if(i>=p.diploids.size()) throw std::out_of_range("indlist contains elements > p.diploids.size()");
+	}
+      //create a new mutation
+      typename poptype::mcont_t::value_type new_mutant(std::forward<Args>(args)...);
+      fwdpp_internal::add_mutation_details(p,p.diploids,locus,new_mutant,indlist,clist);
     }
   }
   
@@ -193,7 +323,7 @@ namespace KTfwd
 		    Args&&... args)
   /*!
     \brief Add a mutation into a deme from a population at a given frequency at in a specific
-    deme or locus
+    deme or locus.
 
     \param p A population object. Meta- or multi-locus.
     \parm index Index of the deme or locus in which to add mutation.
@@ -226,6 +356,7 @@ namespace KTfwd
       }
     if(indlist.size()!=clist.size()) throw std::runtime_error("indlist and clist must be same length");
 
+    //Dispatch to the correct function.
     fwdpp_internal::add_mutation_dispatch(p,index,indlist,clist,typename poptype::popmodel_t(),args...);
   }
 }
--- unit/test_sugar_add_mutation.cc
+++ unit/test_sugar_add_mutation.cc
@@ -4,7 +4,7 @@
   \brief test KTfwd::add_mutation
 */
 
-#define BOOST_TEST_MODULE sugar_singlepop
+#define BOOST_TEST_MODULE test_sugar_add_mutation
 #define BOOST_TEST_DYN_LINK 
 
 #include <config.h>
@@ -13,6 +13,7 @@
 #include <fwdpp/sugar/GSLrng_t.hpp>
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/metapop.hpp>
+#include <fwdpp/sugar/multiloc.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/serialization.hpp>
 #include <fwdpp/sugar/util.hpp>
@@ -22,6 +23,7 @@ using mwriter = KTfwd::mutation_writer;
 using mreader = KTfwd::mutation_reader<mutation_t>;
 using singlepop_t = KTfwd::singlepop<mutation_t>;
 using metapop_t = KTfwd::metapop<mutation_t>;
+using multiloc_t = KTfwd::multiloc<mutation_t>;
 
 BOOST_AUTO_TEST_CASE( test_add_mutation )
 {
@@ -136,3 +138,36 @@ BOOST_AUTO_TEST_CASE( test_add_mutation_metapop )
     }
 }
 
+BOOST_AUTO_TEST_CASE( test_add_mutation_multiloc )
+{
+  multiloc_t pop(1000,10); //10-locus system
+  const std::size_t LOCUS=7; //we'll add mutations into the 8-th locus
+  KTfwd::add_mutation(pop,
+		      //locus index...
+		      LOCUS,
+		      //individuals where we want to place the mutation
+		      {0,1,3,5,7,9},
+		      /*
+			gametes in each individual: 0 = .first, 1 = .second, 2 = .first and .second
+			Thus, there should be 1+1+1+2+2+1=8 copies of the mutation in the population
+		      */
+		      {0,1,0,2,2,0},
+		      //params for new mutation
+		      0.1,-0.1,1,0);
+
+  for( auto i : {0,3,9} ) //should have mutation on first gamete only
+    {
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].first].smutations.size(),1 );
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].second].smutations.size(),0 );
+    }
+  for( auto i : {1} ) //should have mutation on second gamete only
+    {
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].first].smutations.size(),0 );
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].second].smutations.size(),1 );
+    }
+  for(auto i : {5,7}) // should have mutations on both gametes
+    {
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].first].smutations.size(),1 );
+      BOOST_REQUIRE_EQUAL( pop.gametes[pop.diploids[i][LOCUS].second].smutations.size(),1 );
+    }
+}
