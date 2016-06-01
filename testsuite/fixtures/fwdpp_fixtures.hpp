8ce36a0e94609aa2069b44a2f8517d15446d3513
Kevin R. Thornton <krthornt@uci.edu>
2016-06-01 14:05:05 -0700

    starting to refactor the test suite: unit tests more unit-y and integration tests will be compiled and run separately.  This should improved both coverage and ability to do TDD

---
+++ testsuite/fixtures/fwdpp_fixtures.hpp
@@ -0,0 +1,47 @@
+/*!
+  \file fwdpp_fixtures.hpp
+  \brief Minimal fixtures for testing main library with lowest-level (e.g. non-sugar) types
+  \ingroup unit
+*/
+#ifndef FWDPP_TESTSUITE_FWDPP_FIXTURES_HPP
+#define FWDPP_TESTSUITE_FWDPP_FIXTURES_HPP
+
+#include <vector>
+#include <utility>
+#include <unordered_set>
+#include <fwdpp/fwd_functional.hpp>
+#include <fwdpp/forward_types.hpp>
+
+using mtype = KTfwd::mutation;
+using mcont_t = std::vector<mtype>;
+using gcont_t = std::vector<KTfwd::gamete>;
+using dipvector_t = std::vector<std::pair<std::size_t,std::size_t> >;
+using lookup_table_t = std::unordered_set<double,std::hash<double>,KTfwd::equal_eps>;
+using mcounts_t = std::vector<KTfwd::uint_t>;
+
+struct standard_empty_single_deme_fixture
+/*!
+  Basic stuff needed for a simulation of a single deme using the low level bit of fwddpp
+  \ingroup unit
+ */
+{
+  mcont_t mutations,fixations;
+  gcont_t gametes;
+  dipvector_t diploids;
+  lookup_table_t mut_lookup;
+  mcounts_t mcounts,fixation_times;
+  KTfwd::gamete::mutation_container neutral,selected;
+  standard_empty_single_deme_fixture() : mutations(mcont_t()),
+					 fixations(mcont_t()),
+					 gametes(gcont_t()),
+					 diploids(dipvector_t()),
+					 mut_lookup(lookup_table_t()),
+					 mcounts(mcounts_t()),
+					 fixation_times(mcounts_t()),
+					 neutral(KTfwd::gamete::mutation_container()),
+					 selected(KTfwd::gamete::mutation_container())
+  {
+  }
+};
+
+#endif
--- testsuite/unit/crossoverTest.cc
+++ testsuite/unit/crossoverTest.cc
@@ -8,21 +8,17 @@
   They are implemented using boost's unit testing framework, and will not be compiled unless
   the configure script finds this library on your system.
  */
+
 /*! \file crossoverTest.cc
   \ingroup unit
   \brief Tests of KTfwd::fwdpp_internal::recombine_gametes
 */
-#define BOOST_TEST_MODULE crossoverTest
-#define BOOST_TEST_DYN_LINK 
 
 #include <config.h>
-#include <fwdpp/diploid.hh>
+#include "../fixtures/fwdpp_fixtures.hpp"
+#include <fwdpp/internal/recombination_common.hpp>
+#include <fwdpp/debug.hpp>
 #include <boost/test/unit_test.hpp>
-#include <unistd.h>
-#include <iterator>
-#include <functional>
-#include <vector>
-#include <iostream>
 
 using mut = KTfwd::mutation;
 using gtype = KTfwd::gamete;
@@ -31,16 +27,17 @@ using gtype = KTfwd::gamete;
   These tests model what goes on inside of the recombination routines.
 */
 
-BOOST_AUTO_TEST_CASE( simple_test_1 )
+BOOST_AUTO_TEST_SUITE( test_crossing_over )
+
+BOOST_FIXTURE_TEST_CASE( simple_test_1,standard_empty_single_deme_fixture )
 {
   //Neutral mutations at positions 0.1 and 0.9, resp.
   gtype g1(1),g2(1);
-  std::vector<mut> mvector(1,mut(0.1,0.));
+  mutations.emplace_back(0.1,0.);
 
-  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
-  mvector.emplace_back(0.9,0.);
-  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g2);
-
+  g1.mutations.push_back(0);
+  mutations.emplace_back(0.9,0.);
+  g2.mutations.push_back(1);
   BOOST_CHECK_EQUAL( g1.mutations.size(), 1 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
 
@@ -50,28 +47,29 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
   std::vector<double> rec_positions = {0.5, std::numeric_limits<double>::max()};
 
   //We need a "gamete pool"
-  std::vector<gtype> gametes = { g1, g2 };
+  gametes = { g1, g2 };
 
+  auto & MUT = mutations; //HACK so that lambda compiles...
   //Make sure that our iterators are cool
   BOOST_CHECK( std::find_if( g1.mutations.begin(),
 			     g1.mutations.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.1;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.1;
 			     } ) != g1.mutations.end() );
   BOOST_CHECK( std::find_if( g1.mutations.begin(),
 			     g1.mutations.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.9;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.9;
 			     } ) == g1.mutations.end() );
   BOOST_CHECK( std::find_if( g2.mutations.begin(),
 			     g2.mutations.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.9;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.9;
 			     } ) != g2.mutations.end() );
   BOOST_CHECK( std::find_if( g2.mutations.begin(),
 			     g2.mutations.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.1;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.1;
 			     } ) == g2.mutations.end() );
 
   BOOST_CHECK_EQUAL( g1.mutations.size(), 1 );
@@ -82,50 +80,49 @@ BOOST_AUTO_TEST_CASE( simple_test_1 )
   gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
 					    0,1,
-					    gametes,mvector,
+					    gametes,mutations,
 					    neutral,selected);
   /*
     Now, neutral should have both mutations
   */
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.9;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.9;
 			     } ) != neutral.end() );
 
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.1;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.1;
 			     } ) != neutral.end() );
 
 }
 
-BOOST_AUTO_TEST_CASE( three_point_cross_1 )
+BOOST_FIXTURE_TEST_CASE( three_point_cross_1,standard_empty_single_deme_fixture )
 {
   //g1: neutral muts at 0.1,0.5
   //g2: neutral muts at 0.9
   gtype g1(1),g2(1);
-  std::vector<mut> mvector(1,mut(0.1,0.));
+  mutations.emplace_back(0.1,0.);
 
-  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
-  mvector.emplace_back(0.5,0.);
-  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g1);
-  mvector.emplace_back(0.9,0.);
-  KTfwd::fwdpp_internal::add_new_mutation(2,mvector,g2);
-
+  g1.mutations.push_back(0);
+  mutations.emplace_back(0.5,0.);
+  g1.mutations.push_back(1);
+  mutations.emplace_back(0.9,0.);
+  g2.mutations.push_back(2);
   BOOST_CHECK_EQUAL( g1.mutations.size(), 2 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
 
   std::vector<double> rec_positions = {0.25, 0.75, std::numeric_limits<double>::max()};
 
   //We need a "gamete pool"
-  std::vector<gtype> gametes = { g1, g2 };
+  gametes = { g1, g2 };
 
   //Needed as of 0.3.3
   gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
-					    0,1,gametes,mvector,
+					    0,1,gametes,mutations,
 					    neutral,selected );
 
 
@@ -133,18 +130,19 @@ BOOST_AUTO_TEST_CASE( three_point_cross_1 )
     This was a double x-over.
     Neutral must therefore only contain 0.1
   */
+  auto & MUT = mutations; //HACK so that lambda compiles...
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.1;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.1;
 			     } ) != neutral.end());
 
   for( auto d : {0.5,0.9} )
     {
       BOOST_CHECK( std::find_if( neutral.begin(),
 				 neutral.end(),
-				 [d,&mvector]( std::size_t __m ) {
-				   return mvector[__m].pos == d;
+				 [d,&MUT]( std::size_t __m ) {
+				   return MUT[__m].pos == d;
 				 } ) == neutral.end() );
 
     }
@@ -153,18 +151,17 @@ BOOST_AUTO_TEST_CASE( three_point_cross_1 )
 /*
   A unit test to ensure that issue #26 can't happen again
 */
-BOOST_AUTO_TEST_CASE( three_point_cross_2 )
+BOOST_FIXTURE_TEST_CASE( three_point_cross_2,standard_empty_single_deme_fixture )
 {
   //g1: neutral muts at 0.1,0.5
   //g2: neutral muts at 0.9
   gtype g1(1),g2(1);
-  std::vector<mut> mvector(1,mut(0.1,0.));
-  KTfwd::fwdpp_internal::add_new_mutation(0,mvector,g1);
-  mvector.emplace_back(0.5,0.);
-  KTfwd::fwdpp_internal::add_new_mutation(1,mvector,g1);
-  mvector.emplace_back(0.9,0.);
-  KTfwd::fwdpp_internal::add_new_mutation(2,mvector,g2);
-
+  mutations.push_back(mut(0.1,0.));
+  g1.mutations.push_back(0);
+  mutations.emplace_back(0.5,0.);
+  g1.mutations.push_back(1);
+  mutations.emplace_back(0.9,0.);
+  g2.mutations.push_back(2);
   BOOST_CHECK_EQUAL( g1.mutations.size(), 2 );
   BOOST_CHECK_EQUAL( g2.mutations.size(), 1 );
 
@@ -175,32 +172,33 @@ BOOST_AUTO_TEST_CASE( three_point_cross_2 )
   std::vector<double> rec_positions = {0.25, 0.5, std::numeric_limits<double>::max()};
 
   //We need a "gamete pool"
-  std::vector<gtype> gametes = { g1, g2 };
+  gametes = { g1, g2 };
 
-  //Needed as of 0.3.3
-  gtype::mutation_container neutral,selected;
   KTfwd::fwdpp_internal::recombine_gametes( rec_positions,
-					    0,1,gametes,mvector,
+					    0,1,gametes,mutations,
 					    neutral,selected );
 
 
+  auto & MUT = mutations; //HACK so that lambda compiles...
   /*
     This was a double x-over.
     Neutral must therefore only contain 0.1
   */
   BOOST_CHECK( std::find_if( neutral.begin(),
 			     neutral.end(),
-			     [&mvector]( std::size_t __m ) {
-			       return mvector[__m].pos == 0.1;
+			     [&MUT]( std::size_t __m ) {
+			       return MUT[__m].pos == 0.1;
 			     } ) != neutral.end());
 
   for( auto d : {0.5,0.9} )
     {
       BOOST_CHECK( std::find_if( neutral.begin(),
 				 neutral.end(),
-				 [d,&mvector]( std::size_t __m ) {
-				   return mvector[__m].pos == d;
+				 [d,&MUT]( std::size_t __m ) {
+				   return MUT[__m].pos == d;
 				 } ) == neutral.end() );
 
     }
 }
+
+BOOST_AUTO_TEST_SUITE_END()
