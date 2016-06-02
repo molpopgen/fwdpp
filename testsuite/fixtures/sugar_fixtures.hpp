ac509f2225f0be4041ebfb6a0c84e8f044a3d2b6
Kevin R. Thornton <krthornt@uci.edu>
2016-06-01 18:09:43 -0700

    add new fixture

--- testsuite/fixtures/sugar_fixtures.hpp
+++ testsuite/fixtures/sugar_fixtures.hpp
@@ -3,11 +3,20 @@
 
 #include <fwdpp/sugar/popgenmut.hpp>
 #include <fwdpp/sugar/singlepop.hpp>
+#include <fwdpp/sugar/metapop.hpp>
 
 struct singlepop_popgenmut_fixture
 {
   KTfwd::singlepop<KTfwd::popgenmut> pop;
   singlepop_popgenmut_fixture() : pop(KTfwd::singlepop<KTfwd::popgenmut>(1000))
+  {
+  }
+};
+
+struct metapop_popgenmut_fixture
+{
+  KTfwd::metapop<KTfwd::popgenmut> pop;
+  metapop_popgenmut_fixture() : pop(KTfwd::metapop<KTfwd::popgenmut>({1000,1000}))
   {
   }
 };
