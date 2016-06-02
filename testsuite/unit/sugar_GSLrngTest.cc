04ea3664dec71e22664b9276b3bf36d005f84b3d
Kevin R. Thornton <krthornt@uci.edu>
2016-06-02 14:34:59 -0700

    add name to test suite

--- testsuite/unit/sugar_GSLrngTest.cc
+++ testsuite/unit/sugar_GSLrngTest.cc
@@ -4,6 +4,8 @@
 #include <gsl/gsl_randist.h>
 #include <sstream>
 
+BOOST_AUTO_TEST_SUITE( test_GSLrng )
+
 BOOST_AUTO_TEST_CASE( copy_construct_test )
 {
   KTfwd::GSLrng_t<KTfwd::GSL_RNG_MT19937> rng(101);
@@ -26,6 +28,7 @@ BOOST_AUTO_TEST_CASE( is_moveable )
   BOOST_CHECK(rng.get()==nullptr);
 }
 
+BOOST_AUTO_TEST_SUITE_END()
 // BOOST_AUTO_TEST_CASE( serialize )
 // {
 //   KTfwd::GSLrng_t<KTfwd::GSL_RNG_MT19937> rng(101);
