401ce108510f698ff1219f6aedbe3af8a80a8d65
Kevin R. Thornton <krthornt@uci.edu>
2016-06-07 13:14:52 -0700

    fix unit test check that was failing b/c of previous fix to initialization of multi-locus types

--- unit/test_sugar_add_mutation.cc
+++ unit/test_sugar_add_mutation.cc
@@ -167,7 +167,7 @@ BOOST_AUTO_TEST_CASE( test_add_mutation_multiloc )
 		      {0,1,0,2,2,0},
 		      //params for new mutation
 		      0.1,-0.1,1,0);
-  BOOST_REQUIRE_EQUAL(KTfwd::check_sum(pop.gametes,2000),true);
+  BOOST_REQUIRE_EQUAL(KTfwd::check_sum(pop.gametes,10*2000),true);
   BOOST_REQUIRE_EQUAL(pop.gametes.size(),2); 
   BOOST_REQUIRE_EQUAL(pop.mutations.size(),1);
   BOOST_REQUIRE_EQUAL(pop.mcounts.size(),1);
