f7f0e449e4fde1a6afcc0b77057829a9adbebe8f
Kevin R. Thornton <krthornt@uci.edu>
2016-10-06 11:58:44 -0700

    added overload for infinite_sites::operator()

--- fwdpp/sugar/infsites.hpp
+++ fwdpp/sugar/infsites.hpp
@@ -128,6 +128,45 @@ namespace KTfwd
     }
 
     /*!
+      \brief Overload for different position distributions for neutral and non-neutral variants
+
+      \param r A const gsl_rng *
+      \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
+      \param generation Generation when this mutation is happening
+      \param neutral_mutation_rate Either the rate at which neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param selected_mutation_rate Either the rate at which non-neutral variants arise (per gamete per generation), or something directly proportional to it
+      \param nposmaker A policy that returns the position of new neutral mutation
+      \param sposmaker A policy that returns the position of new non-neutral mutation
+      \param smaker A policy generating the selection coefficient/effect size associated with non-neutral variants
+      \param hmaker A policy generating the dominance associated with non-neutral variants
+
+      \note A mutation will be "selected" with probability selected_mutation_rate/(selected_mutation_rate + neutral_mutation_rate)
+    */
+        template<typename queue_t,
+	     typename mcont_t,
+	     typename lookup_table_t,
+	     typename nposition_t,
+	     typename sposition_t,
+	     typename sdist_t,
+	     typename hdist_t>
+    inline typename std::enable_if<std::is_same<typename mcont_t::value_type,popgenmut>::value,
+				   std::size_t>::type
+    operator()(queue_t & recycling_bin,
+	       mcont_t & mutations,
+	       const gsl_rng * r, lookup_table_t & lookup,
+	       const uint_t * generation,
+	       const double & neutral_mutation_rate,
+	       const double & selected_mutation_rate,
+	       const nposition_t & nposmaker,
+	       const sposition_t & sposmaker,
+	       const sdist_t & smaker,
+	       const hdist_t & hmaker) const
+    {
+		return this->operator()(recycling_bin,mutations,r,lookup,*generation,
+				neutral_mutation_rate,selected_mutation_rate,nposmaker,sposmaker,
+				smaker,hmaker);
+}
+    /*!
       \param r A const gsl_rng *
       \param lookup A lookup table of mutation positions, see @ref md_md_policies for 
       \param generation Generation when this mutation is happening
