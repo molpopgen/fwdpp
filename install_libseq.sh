6d12042748e576ba3794a5608b9e1d1d27b3ed2c
molpopgen <krthornt@uci.edu>
2016-10-28 11:12:13 -0700

    TravisCI now uses miniconda.  Examples and unit tests are now noinstall_PROGRAMS

--- install_libseq.sh
+++ install_libseq.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-##Install libseq 1.8.5
-wget http://github.com/molpopgen/libsequence/archive/1.8.5.tar.gz
-tar xzf 1.8.5.tar.gz
-cd libsequence-1.8.5
-./configure --prefix=$HOME && make install
+##Install libseq 1.9.0
+wget http://github.com/molpopgen/libsequence/archive/1.9.0.tar.gz
+tar xzf 1.9.0.tar.gz
+cd libsequence-1.9.0
+./configure --prefix=$HOME/miniconda && make install
