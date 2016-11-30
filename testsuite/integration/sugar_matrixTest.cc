aba61bceae6b33955bccbd70c90fe4799817af35
molpopgen <krthornt@uci.edu>
2016-11-29 16:50:10 -0800

    add test for singlepop/genotype matrix

--- testsuite/integration/sugar_matrixTest.cc
+++ testsuite/integration/sugar_matrixTest.cc
@@ -24,3 +24,20 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix)
 	BOOST_REQUIRE_EQUAL(keys.second.size(),m.selected_positions.size());
 	BOOST_REQUIRE_EQUAL(keys.second.size(),m.selected_popfreq.size());
 }
+
+BOOST_AUTO_TEST_CASE(singlepop_genotype_matrix)
+{
+    using spoptype = singlepop_popgenmut_fixture::poptype;
+    spoptype pop(1000);
+    simulate_singlepop(pop, 1000);
+	auto keys = mutation_keys(pop,{0,1,2,3},true,true);
+	auto m = genotype_matrix(pop,{0,1,2,3},keys.first,keys.second);
+
+	BOOST_REQUIRE_EQUAL(m.nrow,4);
+	BOOST_REQUIRE_EQUAL(m.neutral.size(),m.nrow*keys.first.size());
+	BOOST_REQUIRE_EQUAL(m.selected.size(),m.nrow*keys.second.size());
+	BOOST_REQUIRE_EQUAL(keys.first.size(),m.neutral_positions.size());
+	BOOST_REQUIRE_EQUAL(keys.first.size(),m.neutral_popfreq.size());
+	BOOST_REQUIRE_EQUAL(keys.second.size(),m.selected_positions.size());
+	BOOST_REQUIRE_EQUAL(keys.second.size(),m.selected_popfreq.size());
+}
