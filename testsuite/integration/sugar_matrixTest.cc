b8913ee9e61c12e0f9783fadc06541d8724cf229
molpopgen <krthornt@uci.edu>
2016-11-29 16:27:26 -0800

    new test suite file added for matrix stuff

---
+++ testsuite/integration/sugar_matrixTest.cc
@@ -0,0 +1,18 @@
+/* \brief Integration and unit tests of data matrix generation
+ * \ingroup unit
+ */
+
+#include <config.h>
+#include <boost/test/unit_test.hpp>
+#include "../fixtures/sugar_fixtures.hpp"
+#include "../util/quick_evolve_sugar.hpp"
+#include <fwdpp/sugar/matrix.hpp>
+
+BOOST_AUTO_TEST_CASE(singlepop_hapmatrix)
+{
+    using spoptype = singlepop_popgenmut_fixture::poptype;
+    spoptype pop(1000);
+    simulate_singlepop(pop, 1000);
+	auto keys = mutation_keys(pop,{0,1,2,3},true,true);
+	auto m = haplotype_matrix(pop,{0,1,2,3},keys.first,keys.second);
+}
