51d687b1ef73a9d0d31b64311861f402b0d72969
molpopgen <krthornt@uci.edu>
2016-12-06 13:01:35 -0800

    merge integration testing of haplotype and genotype matrices

--- testsuite/integration/sugar_matrixTest.cc
+++ testsuite/integration/sugar_matrixTest.cc
@@ -10,24 +10,7 @@
 #include <fwdpp/sugar/sampling.hpp>
 #include <gsl/gsl_matrix_short.h>
 
-BOOST_AUTO_TEST_CASE(singlepop_hapmatrix)
-{
-    using spoptype = singlepop_popgenmut_fixture::poptype;
-    spoptype pop(1000);
-    simulate_singlepop(pop, 10000);
-    auto keys = mutation_keys(pop, { 0, 1, 2, 3 }, true, true);
-    auto m = haplotype_matrix(pop, { 0, 1, 2, 3 }, keys.first, keys.second);
-
-    BOOST_REQUIRE_EQUAL(m.nrow, 8);
-    BOOST_REQUIRE_EQUAL(m.neutral.size(), m.nrow * keys.first.size());
-    BOOST_REQUIRE_EQUAL(m.selected.size(), m.nrow * keys.second.size());
-    BOOST_REQUIRE_EQUAL(keys.first.size(), m.neutral_positions.size());
-    BOOST_REQUIRE_EQUAL(keys.first.size(), m.neutral_popfreq.size());
-    BOOST_REQUIRE_EQUAL(keys.second.size(), m.selected_positions.size());
-    BOOST_REQUIRE_EQUAL(keys.second.size(), m.selected_popfreq.size());
-}
-
-BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
+BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_exhaustive)
 {
     using spoptype = singlepop_popgenmut_fixture::poptype;
     spoptype pop(1000);
@@ -37,9 +20,9 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
     // Sample a LOT of individuals
     for (std::size_t i = 100; i < 750; i += 5)
         indlist.push_back(i);
-	for(unsigned ntests=0;ntests<1000;++ntests)
+    std::vector<unsigned> indlist2(indlist.begin(), indlist.end());
+    for (unsigned ntests = 0; ntests < 1000; ++ntests)
         {
-            std::vector<unsigned> indlist2(indlist.begin(), indlist.end());
             auto keys = mutation_keys(pop, indlist, true, true);
             // keys come out unsorted, so we have to sort for this test:
             std::sort(keys.first.begin(), keys.first.end(),
@@ -72,8 +55,8 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
                         return a.second == 2 * indlist.size();
                     }),
                 keys.second.end());
+            // Create data matrices and do basic sanity checks
             auto m = haplotype_matrix(pop, indlist, keys.first, keys.second);
-            // Same basic checks as previous tests
             BOOST_REQUIRE_EQUAL(m.nrow, 2 * indlist.size());
             BOOST_REQUIRE_EQUAL(m.neutral.size(), m.nrow * keys.first.size());
             BOOST_REQUIRE_EQUAL(m.selected.size(),
@@ -84,25 +67,29 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
                                 m.selected_positions.size());
             BOOST_REQUIRE_EQUAL(keys.second.size(), m.selected_popfreq.size());
 
+            auto gm = genotype_matrix(pop, indlist, keys.first, keys.second);
+            BOOST_REQUIRE_EQUAL(gm.nrow, indlist.size());
+            BOOST_REQUIRE_EQUAL(gm.neutral.size(),
+                                gm.nrow * keys.first.size());
+            BOOST_REQUIRE_EQUAL(gm.selected.size(),
+                                gm.nrow * keys.second.size());
+            BOOST_REQUIRE_EQUAL(keys.first.size(),
+                                gm.neutral_positions.size());
+            BOOST_REQUIRE_EQUAL(keys.first.size(), gm.neutral_popfreq.size());
+            BOOST_REQUIRE_EQUAL(keys.second.size(),
+                                gm.selected_positions.size());
+            BOOST_REQUIRE_EQUAL(keys.second.size(),
+                                gm.selected_popfreq.size());
+
             // Now, compare to an independent calculation of the genotypes
             // for same individuals
             auto s = KTfwd::sample_separate(pop, indlist2, true);
             // Check same total # of mutations
             BOOST_REQUIRE_EQUAL(s.first.size(), m.neutral_positions.size());
             BOOST_REQUIRE_EQUAL(s.second.size(), m.selected_positions.size());
-            // Check that no variants are fixed
-            for (auto &&si : s.first)
-                {
-                    BOOST_REQUIRE(
-                        std::count(si.second.begin(), si.second.end(), '1')
-                        < 2 * indlist.size());
-                }
-            for (auto &&si : s.second)
-                {
-                    BOOST_REQUIRE(
-                        std::count(si.second.begin(), si.second.end(), '1')
-                        < 2 * indlist.size());
-                }
+            BOOST_REQUIRE_EQUAL(s.first.size(), gm.neutral_positions.size());
+            BOOST_REQUIRE_EQUAL(s.second.size(), gm.selected_positions.size());
+
             std::vector<double> pos;
             for (auto &&i : s.first)
                 pos.push_back(i.first);
@@ -110,6 +97,7 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
             for (std::size_t i = 0; i < pos.size(); ++i)
                 {
                     BOOST_REQUIRE_EQUAL(pos[i], m.neutral_positions[i]);
+                    BOOST_REQUIRE_EQUAL(pos[i], gm.neutral_positions[i]);
                 }
             pos.clear();
             for (auto &&i : s.second)
@@ -117,76 +105,128 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_compare_to_sample)
             for (std::size_t i = 0; i < pos.size(); ++i)
                 {
                     BOOST_REQUIRE_EQUAL(pos[i], m.selected_positions[i]);
+                    BOOST_REQUIRE_EQUAL(pos[i], gm.selected_positions[i]);
                 }
-            // Use GSL matrix stuff to make sure that matrix == s
-            auto v = gsl_matrix_short_const_view_array(
-                m.neutral.data(), m.nrow, m.neutral_positions.size());
-            BOOST_REQUIRE_EQUAL(v.matrix.size1, 2 * indlist.size());
-            // Make sure column sums check out
-            for (std::size_t c = 0; c < v.matrix.size2; ++c)
+            auto sums = KTfwd::col_sums(m);
+            auto gm_sums = KTfwd::col_sums(gm);
+            BOOST_REQUIRE_EQUAL(sums.first.size(), m.neutral_positions.size());
+            BOOST_REQUIRE_EQUAL(sums.second.size(),
+                                m.selected_positions.size());
+            // Check that haplotype and genotype matrices
+            // have same row sums in same order
+            for (std::size_t i = 0; i < sums.first.size(); ++i)
                 {
-                    auto col_view
-                        = gsl_matrix_short_const_column(&v.matrix, c);
-                    unsigned sum = 0, sum2 = 0;
-                    for (std::size_t i = 0; i < v.matrix.size1; ++i)
+                    BOOST_REQUIRE_EQUAL(sums.first[i], gm_sums.first[i]);
+                }
+            for (std::size_t i = 0; i < sums.second.size(); ++i)
+                {
+                    BOOST_REQUIRE_EQUAL(sums.second[i], gm_sums.second[i]);
+                }
+            // Make sure column sums check out for neutral sites.
+            // Only compare to haplotype matrix. If we've made it this far,
+            // we know m == gm in terms of column sums.
+            for (std::size_t c = 0; c < sums.first.size(); ++c)
+                {
+                    unsigned sum2 = 0;
+                    for (std::size_t i = 0; i < m.nrow; ++i)
                         {
-                            sum += gsl_vector_short_get(&col_view.vector, i);
                             sum2 += (s.first[c].second[i] == '1') ? 1 : 0;
                         }
-                    BOOST_REQUIRE_EQUAL(sum, sum2);
+                    BOOST_REQUIRE_EQUAL(sums.first[c], sum2);
+                }
+            // Make sure column sums check out for selected sites
+            for (std::size_t c = 0; c < sums.second.size(); ++c)
+                {
+                    unsigned sum2 = 0;
+                    for (std::size_t i = 0; i < m.nrow; ++i)
+                        {
+                            sum2 += (s.second[c].second[i] == '1') ? 1 : 0;
+                        }
+                    BOOST_REQUIRE_EQUAL(sums.second[c], sum2);
                 }
             // Now, row sums.
-            for (std::size_t r = 0; r < v.matrix.size1; ++r)
+            sums = KTfwd::row_sums(m);
+            gm_sums = KTfwd::row_sums(gm);
+            // Compare row sums from haplotype and genotype matrix.
+            for (std::size_t hi = 0, gi = 0; hi < sums.first.size();
+                 hi += 2, gi += 1)
                 {
-                    auto row_view = gsl_matrix_short_const_row(&v.matrix, r);
-                    unsigned sum = 0, sum2 = 0;
-                    for (std::size_t i = 0; i < v.matrix.size2; ++i)
+                    // Neutral sites
+                    BOOST_REQUIRE_EQUAL(sums.first[hi] + sums.first[hi + 1],
+                                        gm_sums.first[gi]);
+                }
+            for (std::size_t hi = 0, gi = 0; hi < sums.second.size();
+                 hi += 2, gi += 1)
+                {
+                    // Selected sites
+                    BOOST_REQUIRE_EQUAL(sums.second[hi] + sums.second[hi + 1],
+                                        gm_sums.second[gi]);
+                }
+            // Check neutral sites in haplotype matrix to the sample
+            for (std::size_t r = 0; r < sums.first.size(); ++r)
+                {
+                    unsigned sum2 = 0;
+                    for (std::size_t i = 0; i < m.neutral_positions.size();
+                         ++i)
                         {
-                            sum += gsl_vector_short_get(&row_view.vector, i);
                             sum2 += (s.first[i].second[r] == '1') ? 1 : 0;
                         }
-                    BOOST_REQUIRE_EQUAL(sum, sum2);
+                    BOOST_REQUIRE_EQUAL(sums.first[r], sum2);
                 }
-            // Repeat for selected sites
-            if (!m.selected_positions.empty())
+            // Check selected sites
+            for (std::size_t r = 0; r < sums.second.size(); ++r)
                 {
-                    v = gsl_matrix_short_const_view_array(
-                        m.selected.data(), m.nrow,
-                        m.selected_positions.size());
-                    BOOST_REQUIRE_EQUAL(v.matrix.size1, 2 * indlist.size());
-                    // Make sure column sums check out
-                    for (std::size_t c = 0; c < v.matrix.size2; ++c)
+                    unsigned sum2 = 0;
+                    for (std::size_t i = 0; i < m.selected_positions.size();
+                         ++i)
                         {
-                            auto col_view
-                                = gsl_matrix_short_const_column(&v.matrix, c);
-                            unsigned sum = 0, sum2 = 0;
-                            for (std::size_t i = 0; i < v.matrix.size1; ++i)
-                                {
-                                    sum += gsl_vector_short_get(
-                                        &col_view.vector, i);
-                                    sum2 += (s.second[c].second[i] == '1') ? 1
-                                                                           : 0;
-                                }
-                            BOOST_REQUIRE_EQUAL(sum, sum2);
+                            sum2 += (s.second[i].second[r] == '1') ? 1 : 0;
                         }
-                    // Now, row sums.
-                    for (std::size_t r = 0; r < v.matrix.size1; ++r)
-                        {
-                            auto row_view
-                                = gsl_matrix_short_const_row(&v.matrix, r);
-                            unsigned sum = 0, sum2 = 0;
-                            for (std::size_t i = 0; i < v.matrix.size2; ++i)
-                                {
-                                    sum += gsl_vector_short_get(
-                                        &row_view.vector, i);
-                                    sum2 += (s.second[i].second[r] == '1') ? 1
-                                                                           : 0;
-                                }
-                            BOOST_REQUIRE_EQUAL(sum, sum2);
-                        }
+                    BOOST_REQUIRE_EQUAL(sums.second[r], sum2);
                 }
+            /*
+if (!m.selected_positions.empty())
+{
+v = gsl_matrix_short_const_view_array(
+m.selected.data(), m.nrow,
+m.selected_positions.size());
+BOOST_REQUIRE_EQUAL(v.matrix.size1, 2 * indlist.size());
+// Make sure column sums check out
+for (std::size_t c = 0; c < v.matrix.size2; ++c)
+{
+auto col_view
+    = gsl_matrix_short_const_column(&v.matrix, c);
+unsigned sum = 0, sum2 = 0;
+for (std::size_t i = 0; i < v.matrix.size1; ++i)
+    {
+        sum += gsl_vector_short_get(
+            &col_view.vector, i);
+        sum2 += (s.second[c].second[i] == '1') ? 1
+                                               : 0;
+    }
+BOOST_REQUIRE_EQUAL(sum, sum2);
+}
+// Now, row sums.
+for (std::size_t r = 0; r < v.matrix.size1; ++r)
+{
+auto row_view
+    = gsl_matrix_short_const_row(&v.matrix, r);
+unsigned sum = 0, sum2 = 0;
+for (std::size_t i = 0; i < v.matrix.size2; ++i)
+    {
+        sum += gsl_vector_short_get(
+            &row_view.vector, i);
+        sum2 += (s.second[i].second[r] == '1') ? 1
+                                               : 0;
+    }
+BOOST_REQUIRE_EQUAL(sum, sum2);
+}
+}
+*/
+            // Look at properties of the GENOTYPE matrix
+            m = genotype_matrix(pop, indlist, keys.first, keys.second);
             // Evolve pop 100 more generations
-            generation=simulate_singlepop2(pop, rng, generation, 100);
+            generation = simulate_singlepop2(pop, rng, generation, 100);
         }
 }
 
@@ -200,7 +240,7 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_GSL_behavior)
 // and then turn error handler back on.
 {
     using spoptype = singlepop_popgenmut_fixture::poptype;
-    spoptype pop(1000);
+    spoptype pop(1000); // a monomorphic population
     std::vector<std::size_t> indlist;
     // Sample a LOT of individuals
     for (std::size_t i = 100; i < 750; i += 5)
@@ -208,28 +248,10 @@ BOOST_AUTO_TEST_CASE(singlepop_hapmatrix_GSL_behavior)
     auto keys = mutation_keys(pop, indlist, true, true);
     auto m = haplotype_matrix(pop, indlist, keys.first, keys.second);
     BOOST_REQUIRE(keys.first.empty());
-    // Use GSL matrix stuff to make sure that matrix == s
     auto error_handler = gsl_set_error_handler_off();
     auto v = gsl_matrix_short_const_view_array(m.neutral.data(), m.nrow,
                                                m.neutral_positions.size());
     BOOST_REQUIRE_EQUAL(v.matrix.size1, 0);
     BOOST_REQUIRE_EQUAL(v.matrix.size2, 0);
     gsl_set_error_handler(error_handler);
-}
-
-BOOST_AUTO_TEST_CASE(singlepop_genotype_matrix)
-{
-    using spoptype = singlepop_popgenmut_fixture::poptype;
-    spoptype pop(1000);
-    simulate_singlepop(pop, 10000);
-    auto keys = mutation_keys(pop, { 0, 1, 2, 3 }, true, true);
-    auto m = genotype_matrix(pop, { 0, 1, 2, 3 }, keys.first, keys.second);
-
-    BOOST_REQUIRE_EQUAL(m.nrow, 4);
-    BOOST_REQUIRE_EQUAL(m.neutral.size(), m.nrow * keys.first.size());
-    BOOST_REQUIRE_EQUAL(m.selected.size(), m.nrow * keys.second.size());
-    BOOST_REQUIRE_EQUAL(keys.first.size(), m.neutral_positions.size());
-    BOOST_REQUIRE_EQUAL(keys.first.size(), m.neutral_popfreq.size());
-    BOOST_REQUIRE_EQUAL(keys.second.size(), m.selected_positions.size());
-    BOOST_REQUIRE_EQUAL(keys.second.size(), m.selected_popfreq.size());
 }
