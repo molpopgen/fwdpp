81f36b1fe49c1f5faf20ce97ab45f2c6b6142f87
molpopgen <krthornt@uci.edu>
2017-01-08 11:15:10 -0800

    changes to make fixtures more fully-featured and hopefully reduce code elsewhere.

--- testsuite/fixtures/sugar_fixtures.hpp
+++ testsuite/fixtures/sugar_fixtures.hpp
@@ -5,53 +5,156 @@
 #include <fwdpp/sugar/singlepop.hpp>
 #include <fwdpp/sugar/metapop.hpp>
 #include <fwdpp/sugar/multiloc.hpp>
-
+#include <fwdpp/sugar/infsites.hpp>
+#include <fwdpp/sugar/GSLrng_t.hpp>
+#include <fwdpp/recombination.hpp>
+#include <fwdpp/fitness_models.hpp>
+#include <fwdpp/sugar/serialization.hpp>
 #include <testsuite/util/custom_dip.hpp>
+#include <functional>
 
 struct singlepop_popgenmut_fixture
 {
-  using poptype= KTfwd::singlepop<KTfwd::popgenmut>;
-  poptype pop;
-  singlepop_popgenmut_fixture() : pop(poptype(1000))
-  {
-  }
+    using poptype = KTfwd::singlepop<KTfwd::popgenmut>;
+    using rng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2>;
+    using mwriter = KTfwd::mutation_writer;
+    using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+    poptype pop;
+    rng_t rng;
+    singlepop_popgenmut_fixture(const unsigned seed = 0)
+        : pop(poptype(1000)), rng(rng_t(seed))
+    {
+    }
 };
 
 struct singlepop_popgenmut_custom_fixture
 {
-  using poptype = KTfwd::singlepop<KTfwd::popgenmut,custom_diploid_testing_t>;
-  poptype pop;
-  singlepop_popgenmut_custom_fixture() : pop(poptype(1000))
-  {
-  }
+    using poptype
+        = KTfwd::singlepop<KTfwd::popgenmut, custom_diploid_testing_t>;
+    using rng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2>;
+    using mwriter = KTfwd::mutation_writer;
+    using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+    poptype pop;
+    rng_t rng;
+    singlepop_popgenmut_custom_fixture(const unsigned seed = 0)
+        : pop(poptype(1000)), rng(rng_t(seed))
+    {
+    }
 };
 
 struct metapop_popgenmut_fixture
 {
-  using poptype = KTfwd::metapop<KTfwd::popgenmut>;
-  poptype pop;
-  metapop_popgenmut_fixture() : pop(poptype{1000,1000})
-  {
-  }
+    using poptype = KTfwd::metapop<KTfwd::popgenmut>;
+    using rng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2>;
+    using mwriter = KTfwd::mutation_writer;
+    using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+    poptype pop;
+    rng_t rng;
+    metapop_popgenmut_fixture(const unsigned seed = 0)
+        : pop(poptype{ 1000, 1000 }), rng(rng_t(seed))
+    {
+    }
 };
 
 struct metapop_popgenmut_custom_fixture
 {
-  using poptype = KTfwd::metapop<KTfwd::popgenmut,custom_diploid_testing_t>;
-  poptype pop;
-  metapop_popgenmut_custom_fixture() : pop(poptype{1000,1000})
-  {
-  }
+    using poptype = KTfwd::metapop<KTfwd::popgenmut, custom_diploid_testing_t>;
+    using rng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2>;
+    using mwriter = KTfwd::mutation_writer;
+    using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+    poptype pop;
+    rng_t rng;
+    metapop_popgenmut_custom_fixture(const unsigned seed = 0)
+        : pop(poptype{ 1000, 1000 }), rng(rng_t(seed))
+    {
+    }
 };
 
 struct multiloc_popgenmut_fixture
 {
-  using poptype = KTfwd::multiloc<KTfwd::popgenmut>;
-  poptype pop;
-  multiloc_popgenmut_fixture() : pop(poptype(1000,4))
-				/*! N=1000, 4 loci */
-  {
-  }
+    using poptype = KTfwd::multiloc<KTfwd::popgenmut>;
+    using rng_t = KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2>;
+    using mutmodel = std::function<std::size_t(std::queue<std::size_t> &,
+                                               poptype::mcont_t &)>;
+    using recmodel = std::function<std::vector<double>(
+        const poptype::gamete_t &, const poptype::gamete_t &,
+        const poptype::mcont_t &)>;
+    using mwriter = KTfwd::mutation_writer;
+    using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+    // Fitness function
+    struct multilocus_additive
+    {
+        using result_type = double;
+        inline double
+        operator()(const poptype::dipvector_t::value_type &diploid,
+                   const poptype::gcont_t &gametes,
+                   const poptype::mcont_t &mutations) const
+        {
+            using dip_t = poptype::dipvector_t::value_type::value_type;
+            return std::max(
+                0., 1. + std::accumulate(
+                             diploid.begin(), diploid.end(), 0.,
+                             [&gametes, &mutations](const double d,
+                                                    const dip_t &dip) {
+                                 return d + KTfwd::additive_diploid()(
+                                                gametes[dip.first],
+                                                gametes[dip.second], mutations)
+                                        - 1.;
+                             }));
+        }
+    };
+    poptype pop;
+    rng_t rng;
+    std::vector<mutmodel> mutmodels;
+    std::vector<recmodel> recmodels;
+    multiloc_popgenmut_fixture(KTfwd::uint_t *generation,
+                               const unsigned seed = 0)
+        /*! N=1000, 4 loci */
+        : pop(poptype(1000, 4)),
+          rng(rng_t(seed)),
+          mutmodels(
+              { // Locus 0: positions Uniform [0,1)
+                std::bind(KTfwd::infsites(), std::placeholders::_1,
+                          std::placeholders::_2, this->rng.get(),
+                          std::ref(pop.mut_lookup), generation, 0.0025, 0.0025,
+                          [this]() { return gsl_rng_uniform(rng.get()); },
+                          []() { return -0.005; }, []() { return 0.25; }),
+                // Locus 1: positions Uniform [1,2)
+                std::bind(KTfwd::infsites(), std::placeholders::_1,
+                          std::placeholders::_2, this->rng.get(),
+                          std::ref(pop.mut_lookup), generation, 0.0025, 0.0025,
+                          [this]() { return gsl_ran_flat(rng.get(), 1., 2.); },
+                          []() { return -0.005; }, []() { return 0.25; }),
+                // Locus 2: positions Uniform [2,3)
+                std::bind(KTfwd::infsites(), std::placeholders::_1,
+                          std::placeholders::_2, this->rng.get(),
+                          std::ref(pop.mut_lookup), generation, 0.0025, 0.0025,
+                          [this]() {
+                              return gsl_ran_flat(this->rng.get(), 2., 3.);
+                          },
+                          []() { return -0.005; }, []() { return 0.25; }),
+                // Locus 3: positions Uniform [3,4)
+                std::bind(KTfwd::infsites(), std::placeholders::_1,
+                          std::placeholders::_2, this->rng.get(),
+                          std::ref(pop.mut_lookup), generation, 0.005, 0.005,
+                          [this]() { return gsl_ran_flat(rng.get(), 3., 4.); },
+                          []() { return 0.; }, []() { return 0.; }) }),
+          recmodels({ std::bind(KTfwd::poisson_xover(), rng.get(), 0.005, 0.,
+                                1., std::placeholders::_1,
+                                std::placeholders::_2, std::placeholders::_3),
+                      std::bind(KTfwd::poisson_xover(), rng.get(), 0.005, 1.,
+                                2., std::placeholders::_1,
+                                std::placeholders::_2, std::placeholders::_3),
+                      std::bind(KTfwd::poisson_xover(), rng.get(), 0.005, 2.,
+                                3., std::placeholders::_1,
+                                std::placeholders::_2, std::placeholders::_3),
+                      std::bind(KTfwd::poisson_xover(), rng.get(), 0.005, 3.,
+                                4., std::placeholders::_1,
+                                std::placeholders::_2,
+                                std::placeholders::_3) })
+
+    {
+    }
 };
 
 #endif
--- testsuite/integration/sugar_multilocusTest.cc
+++ testsuite/integration/sugar_multilocusTest.cc
@@ -16,135 +16,91 @@
 #include <fwdpp/sugar/multiloc.hpp>
 #include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/serialization.hpp>
+#include "../fixtures/sugar_fixtures.hpp"
+#include "../util/quick_evolve_sugar.hpp"
 
-using mwriter = KTfwd::mutation_writer;
-using mreader = KTfwd::mutation_reader<KTfwd::popgenmut>;
+using poptype = multiloc_popgenmut_fixture::poptype;
 
-using poptype = KTfwd::multiloc<KTfwd::popgenmut>;
-
-//Fitness function
-struct multilocus_additive
-{
-  using result_type = double;
-  inline double operator()(const poptype::dipvector_t::value_type & diploid,
-			   const poptype::gcont_t & gametes,
-			   const poptype::mcont_t & mutations) const
-  {
-    using dip_t = poptype::dipvector_t::value_type::value_type;
-    return std::max(0.,1.+ std::accumulate( diploid.begin(),diploid.end(),0.,[&gametes,&mutations](const double d,const dip_t & dip)
-				{
-				  return d + KTfwd::additive_diploid()(gametes[dip.first],gametes[dip.second],mutations) - 1.;
-				} ));
-  }
-};
-
-void simulate( poptype & pop )
+void
+simulate(multiloc_popgenmut_fixture &f, unsigned &generation)
 {
 
-  KTfwd::GSLrng_t<KTfwd::GSL_RNG_TAUS2> rng(0u);
-
-  unsigned generation = 0;
-
-  std::vector<std::function<std::size_t(std::queue<std::size_t> &,poptype::mcont_t &)> > mutmodels {
-    //Locus 0: positions Uniform [0,1)
-    std::bind(KTfwd::infsites(),std::placeholders::_1,std::placeholders::_2,rng.get(),std::ref(pop.mut_lookup),&generation,
-	      0.005,0.,[&rng](){return gsl_rng_uniform(rng.get());},[](){return 0.;},[](){return 0.;}) ,
-      //Locus 1: positions Uniform [1,2)
-      std::bind(KTfwd::infsites(),std::placeholders::_1,std::placeholders::_2,rng.get(),std::ref(pop.mut_lookup),&generation,
-      		0.005,0.,[&rng](){return gsl_ran_flat(rng.get(),1.,2.);},[](){return 0.;},[](){return 0.;}),
-      //Locus 2: positions Uniform [2,3)
-      std::bind(KTfwd::infsites(),std::placeholders::_1,std::placeholders::_2,rng.get(),std::ref(pop.mut_lookup),&generation,
-      		0.005,0.,[&rng](){return gsl_ran_flat(rng.get(),2.,3.);},[](){return 0.;},[](){return 0.;}),
-      //Locus 3: positions Uniform [3,4)
-      std::bind(KTfwd::infsites(),std::placeholders::_1,std::placeholders::_2,rng.get(),std::ref(pop.mut_lookup),&generation,
-      		0.005,0.,[&rng](){return gsl_ran_flat(rng.get(),3.,4.);},[](){return 0.;},[](){return 0.;})
-      };
-
-  //Within-locus recombination models for 4 loci
-  std::vector<std::function<std::vector<double>(const poptype::gamete_t &,
-						const poptype::gamete_t &,
-						const poptype::mcont_t &)> > recmodels
-  {
-    std::bind(KTfwd::poisson_xover(),rng.get(),0.005,0.,1.,
-	      std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),
-      std::bind(KTfwd::poisson_xover(),rng.get(),0.005,1.,2.,
-		std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),
-      std::bind(KTfwd::poisson_xover(),rng.get(),0.005,2.,3.,
-		std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),
-      std::bind(KTfwd::poisson_xover(),rng.get(),0.005,3.,4.,
-		std::placeholders::_1,std::placeholders::_2,std::placeholders::_3)
-      };
-
-  //Equal mutation and rec. rates per locus
-  std::vector<double> mu(4,0.005),rbw(3,0.005);
-  BOOST_REQUIRE_EQUAL(mu.size(),4);
-  for( ; generation < 10 ; ++generation )
-    {
-      double wbar = KTfwd::sample_diploid( rng.get(),
-					   pop.gametes,
-					   pop.diploids,
-					   pop.mutations,
-					   pop.mcounts,
-					   1000,
-					   &mu[0],
-					   mutmodels,
-					   recmodels,
-					   &rbw[0],
-					   [](const gsl_rng * __r, const double __d){ return gsl_ran_binomial(__r,__d,1); },
-					   std::bind(multilocus_additive(),std::placeholders::_1,std::placeholders::_2,std::placeholders::_3),
-					   pop.neutral,pop.selected);
-      assert( check_sum(pop.gametes,8000) );
-      KTfwd::update_mutations(pop.mutations,pop.fixations,pop.fixation_times,pop.mut_lookup,pop.mcounts,generation,2000);
-    }
+    // Equal mutation and rec. rates per locus
+    std::vector<double> mu(4, 0.005), rbw(3, 0.005);
+    BOOST_REQUIRE_EQUAL(mu.size(), 4);
+    for (; generation < 10; ++generation)
+        {
+            double wbar = KTfwd::sample_diploid(
+                f.rng.get(), f.pop.gametes, f.pop.diploids, f.pop.mutations,
+                f.pop.mcounts, 1000, &mu[0], f.mutmodels, f.recmodels, &rbw[0],
+                [](const gsl_rng *__r, const double __d) {
+                    return gsl_ran_binomial(__r, __d, 1);
+                },
+                std::bind(multiloc_popgenmut_fixture::multilocus_additive(),
+                          std::placeholders::_1, std::placeholders::_2,
+                          std::placeholders::_3),
+                f.pop.neutral, f.pop.selected);
+            assert(check_sum(f.pop.gametes, 8000));
+            KTfwd::update_mutations(f.pop.mutations, f.pop.fixations,
+                                    f.pop.fixation_times, f.pop.mut_lookup,
+                                    f.pop.mcounts, generation, 2000);
+        }
 }
 
-BOOST_AUTO_TEST_CASE( multiloc_sugar_test1 )
+BOOST_AUTO_TEST_CASE(multiloc_sugar_test1)
 {
-  poptype pop(1000,4);
-  simulate(pop);
-  poptype pop2(pop);
-  BOOST_CHECK_EQUAL(pop==pop2,true);
+    unsigned generation = 0;
+    multiloc_popgenmut_fixture f(&generation);
+    simulate(f, generation);
+    poptype pop2(f.pop);
+    BOOST_CHECK_EQUAL(f.pop == pop2, true);
 }
 
-BOOST_AUTO_TEST_CASE( multiloc_sugar_test2 )
+BOOST_AUTO_TEST_CASE(multiloc_sugar_test2)
 {
-  poptype pop(1000,4);
-  simulate(pop);
-  poptype pop2(0,0);
-  KTfwd::serialize s;
-  std::stringstream buffer;
-  s(buffer,pop,mwriter());
-  KTfwd::deserialize()(pop2,buffer,mreader());
-  BOOST_CHECK_EQUAL(pop==pop2,true);
+    unsigned generation = 0;
+    multiloc_popgenmut_fixture f(&generation);
+    simulate(f, generation);
+    poptype pop2(0, 0);
+    KTfwd::serialize s;
+    std::stringstream buffer;
+    s(buffer, f.pop, multiloc_popgenmut_fixture::mwriter());
+    KTfwd::deserialize()(pop2, buffer, multiloc_popgenmut_fixture::mreader());
+    BOOST_CHECK_EQUAL(f.pop == pop2, true);
 }
 
-BOOST_AUTO_TEST_CASE( multiloc_sugar_test2_gz )
+BOOST_AUTO_TEST_CASE(multiloc_sugar_test2_gz)
 {
-  poptype pop(1000,4);
-  simulate(pop);
-  poptype pop2(0,0);
-  gzFile gzf = gzopen("sugar_multilocus_out.gz","wb");
-  KTfwd::gzserialize()(gzf,pop,mwriter());
-  gzclose(gzf);
-  gzf=gzopen("sugar_multilocus_out.gz","rb");
-  KTfwd::gzdeserialize()(pop2,gzf,std::bind(mreader(),std::placeholders::_1));
-  gzclose(gzf);
-  unlink("sugar_multilocus_out.gz");
-  BOOST_CHECK_EQUAL(pop==pop2,true);
+    unsigned generation = 0;
+    multiloc_popgenmut_fixture f(&generation);
+    simulate(f, generation);
+    poptype pop2(0, 0);
+    gzFile gzf = gzopen("sugar_multilocus_out.gz", "wb");
+    KTfwd::gzserialize()(gzf, f.pop, multiloc_popgenmut_fixture::mwriter());
+    gzclose(gzf);
+    gzf = gzopen("sugar_multilocus_out.gz", "rb");
+    KTfwd::gzdeserialize()(pop2, gzf,
+                           std::bind(multiloc_popgenmut_fixture::mreader(),
+                                     std::placeholders::_1));
+    gzclose(gzf);
+    unlink("sugar_multilocus_out.gz");
+    BOOST_CHECK_EQUAL(f.pop == pop2, true);
 }
 
-BOOST_AUTO_TEST_CASE( multiloc_sugar_test3 )
+BOOST_AUTO_TEST_CASE(multiloc_sugar_test3)
 {
-  poptype pop(1000,4);
-  simulate(pop);
-  poptype pop2(std::move(pop));
-  BOOST_CHECK_EQUAL(pop==pop2,false);
+    unsigned generation = 0;
+    multiloc_popgenmut_fixture f(&generation);
+    simulate(f, generation);
+    poptype pop2(std::move(f.pop));
+    BOOST_CHECK_EQUAL(f.pop == pop2, false);
 }
 
-BOOST_AUTO_TEST_CASE( multiloc_sugar_test4 )
+BOOST_AUTO_TEST_CASE(multiloc_sugar_test4)
 {
-  poptype pop(1000,4);
-  simulate(pop);
-  poptype pop2=std::move(pop);
-  BOOST_CHECK_EQUAL(pop==pop2,false);
+    unsigned generation = 0;
+    multiloc_popgenmut_fixture f(&generation);
+    simulate(f, generation);
+    poptype pop2 = std::move(f.pop);
+    BOOST_CHECK_EQUAL(f.pop == pop2, false);
 }
