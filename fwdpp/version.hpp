86bf84d68d91eaa4967c8f6d6147f92c66dd4f32
molpopgen <krthornt@uci.edu>
2017-01-23 15:15:07 -0800

    auto-generate header containing version info

---
+++ fwdpp/version.hpp
@@ -0,0 +1,14 @@
+/*!
+ * \file version.hpp
+ * \brief fwdpp version information
+ */
+#ifndef FWDPP_VERSION_HPP_
+#define FWDPP_VERSION_HPP_
+
+#define FWDPP_VERSION_MAJOR 0
+#define FWDPP_VERSION_MINOR 5
+#define FWDPP_VERSION_REVISION 4
+#define FWDPP_VERSION_STRING "0.5.4"
+
+#endif
+
