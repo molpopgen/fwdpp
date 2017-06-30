b4c64d657453bc8cad52016e42e102ce33470f68
molpopgen <krthornt@uci.edu>
2017-06-30 13:12:24 -0700

    remove unit test that GSLrng_t is copy-constructible.  #50

--- testsuite/unit/sugar_GSLrngTest.cc
+++ testsuite/unit/sugar_GSLrngTest.cc
@@ -6,19 +6,6 @@
 
 BOOST_AUTO_TEST_SUITE(test_GSLrng)
 
-BOOST_AUTO_TEST_CASE(copy_construct_test)
-{
-    KTfwd::GSLrng_t<KTfwd::GSL_RNG_MT19937> rng(101);
-
-    auto rng2(rng);
-
-    // This tests that rng have same state
-    BOOST_REQUIRE_EQUAL(gsl_rng_get(rng.get()), gsl_rng_get(rng2.get()));
-
-    // If something went wrong with copy, destruction will fail here,
-    // and we'll get a failure of the test
-}
-
 BOOST_AUTO_TEST_CASE(is_moveable)
 {
     KTfwd::GSLrng_t<KTfwd::GSL_RNG_MT19937> rng(101);
