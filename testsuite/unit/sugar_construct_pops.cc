abbd93455723a8f99d30fe8da094ee35622ddbf7
molpopgen <krthornt@uci.edu>
2017-09-17 16:55:04 -0700

    create a fixture so that tests of exceptions can be done separately

--- testsuite/unit/sugar_construct_pops.cc
+++ testsuite/unit/sugar_construct_pops.cc
@@ -7,9 +7,7 @@
 #include <fwdpp/sugar/popgenmut.hpp>
 #include <testsuite/fixtures/sugar_fixtures.hpp>
 
-BOOST_AUTO_TEST_SUITE(test_construct_pops)
-
-BOOST_AUTO_TEST_CASE(test_singlepop)
+struct singlepop_objects
 {
     using poptype = singlepop_popgenmut_fixture::poptype;
 
@@ -19,31 +17,58 @@ BOOST_AUTO_TEST_CASE(test_singlepop)
 
     using mutation_container
         = poptype::gcont_t::value_type::mutation_container;
-    // Add some mutations
-    for (unsigned i = 0; i < 3; ++i)
-        {
-            // position = i, effect size = i
-            // meaning muts 1 and 2 not neutral
-            mutations.emplace_back(i, i, 1, 0);
-        }
 
-	BOOST_REQUIRE_EQUAL(mutations[0].neutral, true);
-	BOOST_REQUIRE_EQUAL(mutations[1].neutral, false);
-	BOOST_REQUIRE_EQUAL(mutations[2].neutral, false);
+    singlepop_objects() : diploids{}, mutations{}, gametes{}
+    {
+        // Add some mutations
+        for (unsigned i = 0; i < 3; ++i)
+            {
+                // position = i, effect size = i
+                // meaning muts 1 and 2 not neutral
+                mutations.emplace_back(i, i, 1, 0);
+            }
 
-    // Add two gametes
-    gametes.emplace_back(1, mutation_container{ 0 }, mutation_container{ 1 });
-    gametes.emplace_back(1, mutation_container{}, mutation_container{ 2 });
+        // Add two gametes
+        gametes.emplace_back(1, mutation_container{ 0 },
+                             mutation_container{ 1 });
+        gametes.emplace_back(1, mutation_container{}, mutation_container{ 2 });
 
-    // Add a diploid
-    diploids.emplace_back(0, 1);
+        // Add a diploid
+        diploids.emplace_back(0, 1);
+    }
+};
 
-    poptype p(diploids, gametes, mutations);
+BOOST_AUTO_TEST_SUITE(test_construct_pops)
+
+BOOST_FIXTURE_TEST_CASE(test_singlepop, singlepop_objects)
+{
+    BOOST_REQUIRE_EQUAL(mutations[0].neutral, true);
+    BOOST_REQUIRE_EQUAL(mutations[1].neutral, false);
+    BOOST_REQUIRE_EQUAL(mutations[2].neutral, false);
+
     BOOST_REQUIRE_NO_THROW(poptype p(diploids, gametes, mutations););
+}
 
-	// Now, make incorrect data to trigger exceptions
-	diploids[0].second=0;
-	BOOST_REQUIRE_THROW(poptype p(diploids,gametes,mutations), std::runtime_error);
+BOOST_FIXTURE_TEST_CASE(test_singlepop_bad_gamete_counts, singlepop_objects)
+{
+    // Now, make incorrect data to trigger exceptions
+    diploids[0].second = 0;
+    BOOST_REQUIRE_THROW(poptype p(diploids, gametes, mutations),
+                        std::runtime_error);
+
+    // restore
+    diploids[0].second = 1;
+    // make the gamete data incorrect
+    gametes[0].n++;
+    BOOST_REQUIRE_THROW(poptype p(diploids, gametes, mutations),
+                        std::runtime_error);
+}
+
+BOOST_FIXTURE_TEST_CASE(test_singlepop_incorrect_mutation_storage, singlepop_objects)
+{
+    std::swap(gametes[0].mutations,gametes[0].smutations);
+    BOOST_REQUIRE_THROW(poptype p(diploids, gametes, mutations),
+                        std::logic_error);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
