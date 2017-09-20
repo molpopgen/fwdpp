4355a5035a61635fd4f2bf7ee7e24236fb097411
Kevin R. Thornton <krthornt@uci.edu>
2017-09-20 16:35:34 -0700

    Test different GCC versions on Travis (#60)

---
+++ travis_scripts/build_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ "$USECONDA" == "1" ];
+then
+    export LD_LIBRARY_PATH=$HOME/miniconda/lib
+    export CPPFLAGS="-I$HOME/miniconda/include $CPPFLAGS"
+    export LDFLAGS="-L$HOME/miniconda/lib $LDFLAGS"
+    LDFLAGS="-L$HOME/miniconda/lib -Wl,-rpath,$HOME/miniconda/lib" ./configure --prefix=$HOME && make -j 3 &&  make install
+    else
+    ./configure --prefix=$HOME && make -j 3 && make install
+fi
+
+fwdppConfig --version
+make check
---
+++ travis_scripts/installation_script.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+## This is a complex script...
+
+echo "Variables = "$USECONDA $TRAVIS_OS_NAME
+
+if [ "$USECONDA" == "1" ];
+then
+    if [ "$TRAVIS_OS_NAME" == "linux" ];
+    then 
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    elif [ "$TRAVIS_OS_NAME" == "osx" ];
+        then
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    fi
+    echo "doo"
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    Useful for debugging any issues with conda
+    conda info -a
+    conda install gcc zlib boost
+    conda install -c asmeurer gsl
+    conda install -c bioconda libsequence
+else
+    sudo apt-get update -qq
+    sudo apt-get -f install libboost-test-dev libgsl*-dev
+fi
