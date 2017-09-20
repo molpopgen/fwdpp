6fb187e0ce7f6e8c53bb5539ced70054b815a17d
molpopgen <krthornt@uci.edu>
2017-09-20 14:27:11 -0700

    attempt to farm off Travis duties to shell scripts

---
+++ travis_scripts/installation_script.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+## This is a complex script...
+
+if [ "$USECONDA" == 1 ]
+then
+    if [ "$TRAVIS_OS_NAME" == "linux" ]
+    then 
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    elif [ "$TRAVIS_OS_NAME" == "osx" ]
+        then
+            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    fi
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    hash -r
+    conda config --set always_yes yes --set changeps1 no
+    conda update -q conda
+    Useful for debugging any issues with conda
+    conda info -a
+    conda install gcc zlib boost
+    conda install -c asmeurer gsl
+    conda install -c bioconda libsequence
+fi
