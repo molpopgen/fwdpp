5510b87b34cb13b3030757f16e883ca5851f5930
molpopgen <krthornt@uci.edu>
2017-09-20 14:42:53 -0700

    script formatting

--- travis_scripts/installation_script.sh
+++ travis_scripts/installation_script.sh
@@ -4,12 +4,12 @@
 
 echo $USECONDA $TRAVIS_OS_NAME
 
-if [ "$USECONDA" == "1" ]
+if [ "$USECONDA" == "1" ];
 then
-    if [ "$TRAVIS_OS_NAME" == "linux" ]
+    if [ "$TRAVIS_OS_NAME" == "linux" ];
     then 
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    elif [ "$TRAVIS_OS_NAME" == "osx" ]
+    elif [ "$TRAVIS_OS_NAME" == "osx" ];
         then
             wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
     fi
