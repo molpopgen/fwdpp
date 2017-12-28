4324b3bc2ff8468bcb7912c4090a2820892f053b
molpopgen <krthornt@uci.edu>
2017-12-28 12:04:17 -0800

    add dispatch for mutation models requiring diploid info and require gamete-dependent models take const reference to gamete as argument

--- fwdpp/internal/mutation_internal.hpp
+++ fwdpp/internal/mutation_internal.hpp
@@ -7,11 +7,12 @@ namespace KTfwd
 {
     namespace fwdpp_internal
     {
-        template <typename mmodel, typename gamete_type, typename mcont_type,
-                  typename queue_t>
+        template <typename mmodel, typename diploid_t, typename gamete_type,
+                  typename mcont_type, typename queue_t>
         inline typename std::result_of<mmodel(queue_t &, mcont_type &)>::type
-        mmodel_dispatcher(const mmodel &m, gamete_type &,
-                          mcont_type &mutations, queue_t &recycling_bin)
+        mmodel_dispatcher(const mmodel &m, const diploid_t &,
+                          const gamete_type &, mcont_type &mutations,
+                          queue_t &recycling_bin)
         /*!
           Run-time dispatcher for mutation model
         */
@@ -19,17 +20,32 @@ namespace KTfwd
             return m(recycling_bin, mutations);
         }
 
-        template <typename mmodel, typename gamete_type, typename mcont_type,
-                  typename queue_t>
-        inline typename std::result_of<mmodel(queue_t &, gamete_type &,
+        template <typename mmodel, typename diploid_t, typename gamete_type,
+                  typename mcont_type, typename queue_t>
+        inline typename std::result_of<mmodel(queue_t &, const gamete_type &,
                                               mcont_type &)>::type
-        mmodel_dispatcher(const mmodel &m, gamete_type &g,
-                          mcont_type &mutations, queue_t &recycling_bin)
+        mmodel_dispatcher(const mmodel &m, const diploid_t &,
+                          const gamete_type &g, mcont_type &mutations,
+                          queue_t &recycling_bin)
         /*!
           Run-time dispatcher for mutation model
         */
         {
             return m(recycling_bin, g, mutations);
+        }
+        template <typename mmodel, typename diploid_t, typename gamete_type,
+                  typename mcont_type, typename queue_t>
+        inline typename std::result_of<mmodel(queue_t &, const diploid_t &,
+                                              const gamete_type &,
+                                              mcont_type &)>::type
+        mmodel_dispatcher(const mmodel &m, const diploid_t &dip,
+                          const gamete_type &g, mcont_type &mutations,
+                          queue_t &recycling_bin)
+        /*!
+          Run-time dispatcher for mutation model
+        */
+        {
+            return m(recycling_bin, dip, g, mutations);
         }
     }
 }
