a649a60779b49af2406c75e071c2bea4d3263e20
molpopgen <krthornt@uci.edu>
2017-12-29 19:18:10 -0800

    move diploid serialization types to their own header

---
+++ fwdpp/io/diploid.hpp
@@ -0,0 +1,41 @@
+#ifndef FWDPP_OP_DIPLOID_HPP__
+#define FWDPP_OP_DIPLOID_HPP__
+
+#include "scalar_serialization.hpp"
+
+namespace fwdpp
+{
+    namespace io
+    {
+        template <typename T> struct serialize_diploid
+        {
+            io::scalar_writer writer;
+            serialize_diploid() : writer{} {}
+            template <typename ostreamtype>
+            inline void
+            operator()(const T &dip, ostreamtype &buffer) const
+            {
+                writer(buffer, &dip.first);
+                writer(buffer, &dip.second);
+            }
+        };
+
+        template <typename T> struct deserialize_diploid
+        {
+            io::scalar_reader reader;
+            deserialize_diploid() : reader{} {}
+            template <typename istreamtype>
+            inline void
+            operator()(T &dip, istreamtype &buffer) const
+            {
+                typename T::first_type c;
+                reader(buffer, &c);
+                dip.first = c;
+                reader(buffer, &c);
+                dip.second = c;
+            }
+        };
+    }
+}
+
+#endif
