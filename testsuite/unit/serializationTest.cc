acba87fe848ba23b78e62746b24d1fbda13d7d7a
molpopgen <krthornt@uci.edu>
2018-01-02 14:47:57 -0800

    add unit test of scalar serialization

--- testsuite/unit/serializationTest.cc
+++ testsuite/unit/serializationTest.cc
@@ -3,21 +3,38 @@
   \brief Tests of low-level serialization functionality.
 */
 
-/*
- * TODO: check how to test for error when writing to closed gzFile
- */
-
 #include <config.h>
-#include <fstream>
 #include <sstream>
-#include <unistd.h>
+#include <array>
+#include <limits>
 #include "../fixtures/fwdpp_fixtures.hpp"
 #include <fwdpp/io/scalar_serialization.hpp>
 #include <boost/test/unit_test.hpp>
 
 using gtype = fwdpp::gamete;
 
+struct scalar_fixture
+{
+    std::array<double, 2> ad2, ad2_2;
+
+    scalar_fixture()
+        : ad2{ { -1, 1 } },
+          ad2_2{ { std::numeric_limits<double>::quiet_NaN(),
+                   std::numeric_limits<double>::quiet_NaN() } }
+    {
+    }
+};
+
 BOOST_AUTO_TEST_SUITE(test_scalar_writer)
 
+BOOST_FIXTURE_TEST_CASE(serialize_std_array, scalar_fixture)
+{
+    fwdpp::io::scalar_writer writer;
+    fwdpp::io::scalar_reader reader;
+    std::stringstream buffer;
+    writer(buffer,&ad2[0],2);
+    reader(buffer,&ad2_2[0],2);
+    BOOST_REQUIRE(ad2 == ad2_2);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
