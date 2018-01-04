4478ee0be130fa9f6c1a31dc2cb011db0ef18434
molpopgen <krthornt@uci.edu>
2018-01-03 17:54:01 -0800

    candidate function to return an infinitely-many sites scheme generating fwdpp::popgenmut

--- fwdpp/sugar/popgenmut.hpp
+++ fwdpp/sugar/popgenmut.hpp
@@ -66,6 +66,31 @@ namespace fwdpp
                        == std::tie(rhs.g, rhs.s, rhs.h)
                    && is_equal(rhs);
         }
+
+        template <typename mcont_t, typename lookup_table_t, typename posmaker,
+                  typename smaker, typename hmaker>
+        inline static std::function<std::size_t(
+            fwdpp::traits::recycling_bin_t<mcont_t> &, mcont_t &)>
+        infsites(const gsl_rng *r, double psel, const mcont_t &,
+                 lookup_table_t &lookup, uint_t &generation, posmaker p,
+                 smaker s, hmaker h)
+        {
+            auto mutation_model = [&r, &generation, &lookup, psel, p, s, h](
+                fwdpp::traits::recycling_bin_t<mcont_t> &recycling_bin,
+                mcont_t &mutations) {
+                auto pos = p();
+                while (lookup.find(pos) != lookup.end())
+                    {
+                        pos = p();
+                    }
+                lookup.insert(pos);
+                bool selected = gsl_rng_uniform(r) < psel;
+                return fwdpp_internal::recycle_mutation_helper(
+                    recycling_bin, mutations, pos, (selected) ? s() : 0.,
+                    (selected) ? h() : 0., generation);
+            };
+            return mutation_model;
+        }
     };
 
     namespace io
