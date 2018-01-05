948be223b913590f83615235abe32c1b47d9056b
molpopgen <krthornt@uci.edu>
2018-01-04 18:22:29 -0800

    minor cleanup of lamba used in rec_update_itr and remove ref to double as param

--- fwdpp/internal/rec_gamete_updater.hpp
+++ fwdpp/internal/rec_gamete_updater.hpp
@@ -11,15 +11,14 @@ namespace fwdpp
         template <typename itr_type, typename mcont_t>
         inline itr_type
         rec_update_itr(itr_type __first, itr_type __last,
-                       const mcont_t &mutations, const double &val)
+                       const mcont_t &mutations, const double val)
         {
             if (__first == __last)
                 return __first;
             return std::upper_bound(
-                __first, __last, std::cref(val),
+                __first, __last, val,
                 [&mutations](const double __val,
-                             const std::size_t __mut) noexcept {
-                    assert(__mut < mutations.size());
+                             const typename itr_type::value_type __mut) {
                     return __val < mutations[__mut].pos;
                 });
         }
@@ -29,7 +28,7 @@ namespace fwdpp
         itr_type
         rec_gam_updater(itr_type __first, itr_type __last,
                         const mcont_t &mutations, mutation_index_cont_t &muts,
-                        const double &val)
+                        const double val)
         {
             // O(log_2) comparisons of double plus at most __last - __first
             // copies
