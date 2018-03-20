9d85128ad58f490f3b4aa79c3f0c15608c34ebdd
Kevin R. Thornton <krthornt@uci.edu>
2018-03-20 16:48:22 -0700

    Remove custom deleters (#103)
    * refactor internals of GSLrng_t.  Issue #98
    
    * make smart pointer private and change default -> delete, which is what we really mean
    
    * Closes #98

--- fwdpp/sugar/GSLrng_t.hpp
+++ fwdpp/sugar/GSLrng_t.hpp
@@ -37,12 +37,14 @@ namespace fwdpp
       private:
         sugar::gsl_rng_ptr_t setup(GSL_RNG_MT19937)
         {
-            return sugar::gsl_rng_ptr_t(gsl_rng_alloc(gsl_rng_mt19937));
+            return sugar::gsl_rng_ptr_t(gsl_rng_alloc(gsl_rng_mt19937),
+                                        [](gsl_rng *r) { gsl_rng_free(r); });
         }
 
         sugar::gsl_rng_ptr_t setup(GSL_RNG_TAUS2)
         {
-            return sugar::gsl_rng_ptr_t(gsl_rng_alloc(gsl_rng_taus2));
+            return sugar::gsl_rng_ptr_t(gsl_rng_alloc(gsl_rng_taus2),
+                                        [](gsl_rng *r) { gsl_rng_free(r); });
         }
 
         sugar::gsl_rng_ptr_t
@@ -51,9 +53,9 @@ namespace fwdpp
             return sugar::gsl_rng_ptr_t(gsl_rng_clone(r));
         }
 
-      public:
         //! Smart pointer wrapping the gsl_rng *
         sugar::gsl_rng_ptr_t r;
+      public:
 
         //! Typedef for RNG type, if needed
         using rngtype = T;
@@ -64,7 +66,7 @@ namespace fwdpp
             gsl_rng_set(r.get(), seed);
         }
 
-        GSLrng_t(const GSLrng_t &rng) = default; 
+        GSLrng_t(const GSLrng_t &rng) = delete;
 
         GSLrng_t(GSLrng_t &&) = default;
 
