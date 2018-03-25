2d623f968d990b2082ec744c89929b44cf3c1951
molpopgen <krthornt@uci.edu>
2018-03-25 15:03:32 -0700

    delete declarations and dispatch tags for meta-populations

--- fwdpp/sugar/poptypes/tags.hpp
+++ fwdpp/sugar/poptypes/tags.hpp
@@ -11,7 +11,6 @@ namespace fwdpp
         enum class FWDPP_SUGAR_POPTYPE
         {
             SINGLE,
-            META,
             MULTILOC
         };
         //! Dispatch tag template for population types supported by sugar layer
@@ -21,8 +20,6 @@ namespace fwdpp
         };
         //! Single-population simulations \ingroup sugar
         using SINGLEPOP_TAG = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::SINGLE>;
-        //! Meta-population simulations \ingroup sugar
-        using METAPOP_TAG = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::META>;
         //! Single-population, multi-locus simulations \ingroup sugar
         using MULTILOCPOP_TAG
             = FWDPP_SUGAR_POPTAG<FWDPP_SUGAR_POPTYPE::MULTILOC>;
