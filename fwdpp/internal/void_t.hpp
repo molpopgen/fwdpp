5c7d33ab41f8ef308788a6dc0b28b013c5a4d8ea
molpopgen <krthornt@uci.edu>
2018-03-26 13:28:57 -0700

    get rid of macro that void_t does better

--- fwdpp/internal/void_t.hpp
+++ fwdpp/internal/void_t.hpp
@@ -15,25 +15,7 @@ namespace fwdpp
             {
                 typedef void type;
             };
-
-/*
-This macro is used to generate
-types able to determine if a class type
-has a particular member type name
-*/
-#define HAS_TYPE(NAME)                                                        \
-    template <typename, typename = void> struct has_##NAME : std::false_type  \
-    {                                                                         \
-    };                                                                        \
-    template <typename T>                                                     \
-    struct has_##NAME<T, typename void_t<typename T::NAME>::type>             \
-        : std::true_type                                                      \
-    {                                                                         \
-    };
-
-            HAS_TYPE(gamete_tag);
-            HAS_TYPE(mutation_type);
-        }
-    }
+		}
+	}
 }
 #endif
