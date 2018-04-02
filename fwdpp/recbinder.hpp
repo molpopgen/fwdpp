eee7b8dedee1f4f3babffc9c6f4ed68708a79ea1
molpopgen <krthornt@uci.edu>
2018-04-02 15:58:57 -0700

    new file added to help binding recombination models

---
+++ fwdpp/recbinder.hpp
@@ -0,0 +1,21 @@
+#ifndef FWDPP_RECBINDER_HPP__
+#define FWDPP_RECBINDER_HPP__
+
+#include <vector>
+#include <functional>
+
+namespace fwdpp
+{
+    template <typename T>
+    inline std::function<std::vector<double>(void)>
+    recbinder(T&& recmodel, const gsl_rng* r)
+    /*! Convenience utility for binding simple recombination models.
+     *  \version 0.6.0
+     *  First added to library
+     */
+    {
+        return [recmodel, r]() { return recmodel(r); };
+    }
+}
+
+#endif
