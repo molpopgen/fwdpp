028144615b0b676c7de2ba7994aaa9f7a39ad46d
molpopgen <krthornt@uci.edu>
2018-04-03 08:21:27 -0700

    add GSL includes

--- fwdpp/sugar/popgenmut.hpp
+++ fwdpp/sugar/popgenmut.hpp
@@ -1,6 +1,8 @@
 #ifndef __FWDPP_SUGAR_MUTATION_POPGENMUT_HPP__
 #define __FWDPP_SUGAR_MUTATION_POPGENMUT_HPP__
 
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
 #include <fwdpp/forward_types.hpp>
 #include <fwdpp/tags/tags.hpp>
 #include <fwdpp/io/scalar_serialization.hpp>
