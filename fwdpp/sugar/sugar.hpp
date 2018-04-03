dfa1f553d8ea7fe1abe1aa30ef74b0854297c35f
molpopgen <krthornt@uci.edu>
2018-04-03 09:28:48 -0700

    remove sugar/infsites.hpp

--- fwdpp/sugar/sugar.hpp
+++ fwdpp/sugar/sugar.hpp
@@ -6,7 +6,6 @@
 #define __FWDPP_SUGAR_SUGAR_HPP__
 
 #include <fwdpp/sugar/GSLrng_t.hpp>
-#include <fwdpp/sugar/infsites.hpp>
 #include <fwdpp/sugar/multiloc.hpp>
 #include <fwdpp/sugar/popgenmut.hpp>
 #include <fwdpp/sugar/generalmut.hpp>
