4b2433da17791f05157edf0795aaa9b0b239a77b
molpopgen <krthornt@uci.edu>
2018-04-12 16:51:57 -0700

    remove ref to non-existent example

--- fwdpp/sample_diploid.hpp
+++ fwdpp/sample_diploid.hpp
@@ -34,7 +34,6 @@ namespace fwdpp
       sampling.
       \return The mean fitness of the parental generation
       \example diploid_ind.cc
-      \example pfix.cc
       \example diploid_fixed_sh_ind_lambda.cc
     */
     template <typename gamete_type, typename gamete_cont_type_allocator,
