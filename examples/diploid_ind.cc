a70955a1cc9b36502063f356fa8f6db9d47696e7
molpopgen <krthornt@uci.edu>
2018-04-17 12:12:49 -0700

    tentative: it seems that simpler setup is faster

--- examples/diploid_ind.cc
+++ examples/diploid_ind.cc
@@ -22,36 +22,24 @@
 using mtype = fwdpp::popgenmut;
 #define SINGLEPOP_SIM
 #include <common_ind.hpp>
-
+#include <numeric>
 void
 compact(singlepop_t &pop)
 {
-    using pair_type
-        = std::pair<singlepop_t::mcont_t::value_type, fwdpp::uint_t>;
-    using iter_type = std::vector<pair_type>::iterator;
-    std::vector<pair_type> data_copy;
-    data_copy.reserve(pop.mutations.size());
-    std::vector<std::size_t> indexes;
-    unsigned nzeroes = 0;
-    for (std::size_t i = 0; i < pop.mcounts.size(); ++i)
-        {
-            if (!pop.mcounts[i])
-                ++nzeroes;
-            data_copy.emplace_back(std::move(pop.mutations[i]),
-                                   pop.mcounts[i]);
-            indexes.push_back(i);
-        }
+    std::vector<fwdpp::uint_t> indexes(pop.mutations.size());
+    std::iota(std::begin(indexes), std::end(indexes), 0);
 
     auto new_indexes_end = std::stable_partition(
         std::begin(indexes), std::end(indexes),
-        [&data_copy](std::size_t i) { return data_copy[i].second > 0; });
+        [&pop](const fwdpp::uint_t i) { return pop.mcounts[i]; });
+
+    std::sort(std::begin(indexes), new_indexes_end,
+              [&pop](const fwdpp::uint_t i, const fwdpp::uint_t j) {
+                  return pop.mutations[i].pos < pop.mutations[j].pos;
+              });
+    std::vector<fwdpp::uint_t> reindex(indexes.size());
     std::size_t new_indexes_size
         = std::distance(std::begin(indexes), new_indexes_end);
-    std::sort(std::begin(indexes), new_indexes_end,
-              [&data_copy](const std::size_t i, const std::size_t j) {
-                  return data_copy[i].first.pos < data_copy[j].first.pos;
-              });
-	std::vector<fwdpp::uint_t> reindex(indexes.size());
     for (std::size_t i = 0; i < new_indexes_size; ++i)
         {
             reindex[indexes[i]] = i;
@@ -70,27 +58,32 @@ compact(singlepop_t &pop)
                         }
                 }
         }
-    pop.mutations.clear();
-    pop.mcounts.clear();
+    decltype(pop.mutations) reordered_muts;
+    decltype(pop.mcounts) reordered_mcounts;
+    reordered_muts.reserve(pop.mutations.size());
+    reordered_mcounts.reserve(pop.mutations.size());
     for (auto i : indexes)
         {
-            pop.mutations.emplace_back(std::move(data_copy[i].first));
-            pop.mcounts.push_back(data_copy[i].second);
-            if (pop.mcounts.back() > 0)
+            reordered_muts.emplace_back(std::move(pop.mutations[i]));
+            reordered_mcounts.push_back(pop.mcounts[i]);
+            if (reordered_mcounts.back() > 0)
                 {
-                    auto x
-                        = pop.mut_lookup.equal_range(pop.mutations.back().pos);
+                    auto x = pop.mut_lookup.equal_range(
+                        reordered_muts.back().pos);
                     while (x.first != x.second)
                         {
                             if (x.first->second == i)
                                 {
-                                    x.first->second = pop.mutations.size() - 1;
+                                    x.first->second
+                                        = reordered_muts.size() - 1;
                                     break;
                                 }
                             ++x.first;
                         }
                 }
         }
+    pop.mutations.swap(reordered_muts);
+    pop.mcounts.swap(reordered_mcounts);
 }
 
 int
