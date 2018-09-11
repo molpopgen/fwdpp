6144efc7df3ababc2afd366885c35120d7776534
molpopgen <krthornt@uci.edu>
2018-09-11 13:27:15 -0700

    add new header for debug details

---
+++ fwdpp/internal/debug_details.hpp
@@ -0,0 +1,14 @@
+#ifndef FWDPP_INTERNAL_DEBUG_DETAILS_HPP
+#define FWDPP_INTERNAL_DEBUG_DETAILS_HPP
+
+namespace fwdpp
+{
+    namespace debug
+    {
+        namespace detail
+        {
+        }
+    }
+}
+
+#endif
