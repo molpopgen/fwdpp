7e213e294701344d99163560135fc665a43c0bdb
molpopgen <krthornt@uci.edu>
2018-10-10 10:23:05 -0700

    Add new example program.  wfts = Wright-Fisher, tree sequence

---
+++ examples/wfts.cc
@@ -0,0 +1,12 @@
+/*! \include wfts.cet
+ */
+
+#include <fwdpp/ts/table_collection.hpp>
+#include <fwdpp/ts/table_simplifier.hpp>
+#include <fwdpp/sugar/slocuspop.hpp>
+
+int main(int argc, char ** argv)
+{
+    fwdpp::ts::table_collection tables(1.0);
+    fwdpp::ts::table_simplifier(1.0);
+}
