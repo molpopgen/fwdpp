346d37c1da3d561a88132c76cb65ec0aabe80651
molpopgen <krthornt@uci.edu>
2018-10-10 10:52:16 -0700

    get CLI in place for wfts using boost program_options

--- examples/wfts.cc
+++ examples/wfts.cc
@@ -1,12 +1,41 @@
 /*! \include wfts.cet
  */
 
+#include <cstdio>
+#include <iostream>
+#include <cassert>
 #include <fwdpp/ts/table_collection.hpp>
 #include <fwdpp/ts/table_simplifier.hpp>
 #include <fwdpp/sugar/slocuspop.hpp>
+#include <boost/program_options.hpp>
 
-int main(int argc, char ** argv)
+namespace po = boost::program_options;
+
+int
+main(int argc, char** argv)
 {
+    fwdpp::uint_t N, gcint;
+    double theta,rho,mean,shape,mu;
+    po::options_description options("Usage");
+    options.add_options()("help", "Display help")
+        ("N", po::value<unsigned>(&N), "Diploid population size")
+        ("gc", po::value<unsigned>(&gcint),"Simplification interval")
+        ("theta", po::value<double>(&theta), "4Nu")
+        ("rho", po::value<double>(&rho), "4Nr")
+        ("mu", po::value<double>(&mu), "mutation rate to selected variants")
+        ("mean", po::value<double>(&mean),
+        "Mean of Gamma distribution of selection coefficients")
+        ("shape", po::value<double>(&shape),
+        "Shape of Gamma distribution of selection coefficients");
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, options), vm);
+    po::notify(vm);
+
+    if (vm.count("help"))
+        {
+            std::cout << options << '\n';
+            std::exit(1);
+        }
     fwdpp::ts::table_collection tables(1.0);
     fwdpp::ts::table_simplifier(1.0);
 }
