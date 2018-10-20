c781ab7804a71880bde7cfc5ba0839ba76e8ca16
molpopgen <krthornt@uci.edu>
2018-10-19 21:01:00 -0700

    Add explicit sample node id -> sample index map.  This makes the sample traversal much more clear.

--- fwdpp/ts/marginal_tree.hpp
+++ fwdpp/ts/marginal_tree.hpp
@@ -15,20 +15,26 @@ namespace fwdpp
         {
             std::vector<std::int32_t> parents, leaf_counts,
                 preserved_leaf_counts, left_sib, right_sib, left_child,
-                right_child, left_sample, right_sample, next_sample, is_sample;
+                right_child, left_sample, right_sample, next_sample,
+                sample_index_map;
             double left, right;
             marginal_tree(std::int32_t nnodes,
                           const std::vector<std::int32_t>& samples)
                 : parents(nnodes, TS_NULL_NODE), leaf_counts(nnodes, 0),
-                  preserved_leaf_counts(nnodes, 0), left_sib(nnodes, TS_NULL_NODE),
-                  right_sib(nnodes, TS_NULL_NODE), left_child(nnodes, TS_NULL_NODE),
-                  right_child(nnodes, TS_NULL_NODE), left_sample(nnodes, TS_NULL_NODE),
-                  right_sample(nnodes, TS_NULL_NODE), next_sample(nnodes, TS_NULL_NODE),
-                  is_sample(nnodes, 0),
+                  preserved_leaf_counts(nnodes, 0),
+                  left_sib(nnodes, TS_NULL_NODE),
+                  right_sib(nnodes, TS_NULL_NODE),
+                  left_child(nnodes, TS_NULL_NODE),
+                  right_child(nnodes, TS_NULL_NODE),
+                  left_sample(nnodes, TS_NULL_NODE),
+                  right_sample(nnodes, TS_NULL_NODE),
+                  next_sample(nnodes, TS_NULL_NODE),
+                  sample_index_map(nnodes, TS_NULL_NODE),
                   left{ std::numeric_limits<double>::quiet_NaN() }, right{
                       std::numeric_limits<double>::quiet_NaN()
                   }
             {
+                int i = 0;
                 for (auto s : samples)
                     {
                         if (static_cast<std::size_t>(s) >= leaf_counts.size())
@@ -37,19 +43,23 @@ namespace fwdpp
                                     "sample index out of range");
                             }
                         leaf_counts[s] = 1;
-                        is_sample[s] = 1;
-                        left_sample[s] = right_sample[s] = s;
+                        sample_index_map[s] = i;
+                        left_sample[s] = right_sample[s] = sample_index_map[s];
                     }
             }
             marginal_tree(std::int32_t nnodes,
                           const std::vector<std::int32_t>& samples,
                           const std::vector<std::int32_t>& preserved_nodes)
                 : parents(nnodes, TS_NULL_NODE), leaf_counts(nnodes, 0),
-                  preserved_leaf_counts(nnodes, 0), left_sib(nnodes, TS_NULL_NODE),
-                  right_sib(nnodes, TS_NULL_NODE), left_child(nnodes, TS_NULL_NODE),
-                  right_child(nnodes, TS_NULL_NODE), left_sample(nnodes, TS_NULL_NODE),
-                  right_sample(nnodes, TS_NULL_NODE), next_sample(nnodes, TS_NULL_NODE),
-                  is_sample(nnodes, 0),
+                  preserved_leaf_counts(nnodes, 0),
+                  left_sib(nnodes, TS_NULL_NODE),
+                  right_sib(nnodes, TS_NULL_NODE),
+                  left_child(nnodes, TS_NULL_NODE),
+                  right_child(nnodes, TS_NULL_NODE),
+                  left_sample(nnodes, TS_NULL_NODE),
+                  right_sample(nnodes, TS_NULL_NODE),
+                  next_sample(nnodes, TS_NULL_NODE),
+                  sample_index_map(nnodes, TS_NULL_NODE),
                   left{ std::numeric_limits<double>::quiet_NaN() }, right{
                       std::numeric_limits<double>::quiet_NaN()
                   }
@@ -62,7 +72,6 @@ namespace fwdpp
                                     "sample index out of range");
                             }
                         leaf_counts[s] = 1;
-                        is_sample[s] = 1;
                         left_sample[s] = right_sample[s] = s;
                     }
                 for (auto s : preserved_nodes)
@@ -73,16 +82,19 @@ namespace fwdpp
                                     "sample index out of range");
                             }
                         preserved_leaf_counts[s] = 1;
-                        is_sample[s] = 1;
                         left_sample[s] = right_sample[s] = s;
                     }
             }
             marginal_tree(std::int32_t nnodes)
-                : parents(nnodes, TS_NULL_NODE), leaf_counts{}, preserved_leaf_counts{},
-                  left_sib(nnodes, TS_NULL_NODE), right_sib(nnodes, TS_NULL_NODE),
-                  left_child(nnodes, TS_NULL_NODE), right_child(nnodes, TS_NULL_NODE),
-                  left_sample(nnodes, TS_NULL_NODE), right_sample(nnodes, TS_NULL_NODE),
-                  next_sample(nnodes, TS_NULL_NODE), is_sample(nnodes, 0),
+                : parents(nnodes, TS_NULL_NODE), leaf_counts{},
+                  preserved_leaf_counts{}, left_sib(nnodes, TS_NULL_NODE),
+                  right_sib(nnodes, TS_NULL_NODE),
+                  left_child(nnodes, TS_NULL_NODE),
+                  right_child(nnodes, TS_NULL_NODE),
+                  left_sample(nnodes, TS_NULL_NODE),
+                  right_sample(nnodes, TS_NULL_NODE),
+                  next_sample(nnodes, TS_NULL_NODE),
+                  sample_index_map(nnodes, TS_NULL_NODE),
                   left{ std::numeric_limits<double>::quiet_NaN() }, right{
                       std::numeric_limits<double>::quiet_NaN()
                   }
