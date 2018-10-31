8577598c44c8b484633ab1fed689bc23bbf62669
molpopgen <krthornt@uci.edu>
2018-10-31 12:43:08 -0700

    Document fwdpp::ts::remove_fixations_from_gametes

--- fwdpp/ts/remove_fixations_from_gametes.hpp
+++ fwdpp/ts/remove_fixations_from_gametes.hpp
@@ -17,6 +17,34 @@ namespace fwdpp
             const mutation_count_container &mcounts,
             const mutation_count_container &mcounts_from_preserved_nodes,
             const fwdpp::uint_t twoN, const bool preserve_selected_fixations)
+        /*! \brief Removed fixed variants from gametes
+         *
+         * This function should be called immediately after simplification and mutation counting.
+         *
+         * \param gametes A gamete container
+         * \param mutations A mutation container
+         * \param mcounts A container stating how many times each element in \a mutations is present in the
+         * currently-alive population
+         * \param mcounts_from_preserved_nodes A container recording the counts of each mutation in ancient samples
+         * \param twoN Twice the current population size
+         * \param preserve_selected_fixations If true, do not mark selected fixations for recycling.
+         *
+         * A mutation is removed from a gamete if one of the following conditions holds:
+         * 1. The sum of \a mcounts and \a mcounts_from_preserved_nodes is zero.
+         * 2. \a mcounts == \a twoN, mcounts_from_preserved_nodes is zero, 
+         * and \a preserve_selected_fixations is false or the mutation is neutral.
+         *
+         * When \a preserve_selected_fixations is true, selected fixations are retained
+         * in the population.  We do this because simulations of phenotypes (as opposed
+         * to relative fitness) require tracking the contribution of fixation to 
+         * trait values.
+         *
+         * \note When simulating a trait and never simulating neutral mutations, the most 
+         * efficient thing is to skip calling this function entirely.
+         *
+         * \todo Improve treatment of fixations by allowing for variants fixed in alive AND 
+         * ancient samples to be detected.
+         */
         {
             bool fixations_exist = false;
             for (std::size_t i = 0; !fixations_exist && i < mcounts.size();
