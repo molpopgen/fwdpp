84eb44d04f3c53e4a33d72184a28c6b2d133555a
molpopgen <krthornt@uci.edu>
2018-11-11 14:43:44 -0800

    update examples/simplify tables to new mutation flagging api

--- examples/simplify_tables.hpp
+++ examples/simplify_tables.hpp
@@ -49,9 +49,8 @@ simplify_tables(poptype &pop,
         pop.gametes, pop.mutations, pop.mcounts, mcounts_from_preserved_nodes,
         2 * pop.diploids.size(), false);
 
-    fwdpp::ts::flag_mutations_for_recycling(
-        pop.mutations, pop.mcounts, mcounts_from_preserved_nodes,
-        pop.mut_lookup, 2 * pop.diploids.size(), false);
+    fwdpp::ts::flag_mutations_for_recycling(pop, mcounts_from_preserved_nodes,
+                                            2 * pop.diploids.size(), false);
     confirm_mutation_counts(pop, tables);
     return idmap;
 }
