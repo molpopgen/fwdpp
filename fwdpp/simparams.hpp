e34b7081456227fb477f0ef3d43a570544c2b898
molpopgen <krthornt@uci.edu>
2018-12-05 13:23:18 -0800

    update build system to latest automake

--- fwdpp/simparams.hpp
+++ fwdpp/simparams.hpp
@@ -9,12 +9,14 @@
 namespace fwdpp
 {
     template <typename genetic_value, typename mutation_function,
-              typename recombination_function>
+              typename recombination_function,
+              typename interlocus_recombination_function>
     struct genetic_parameters
     {
         const genetic_value gvalue;
         const mutation_function generate_mutations;
         const recombination_function generate_breakpoints;
+        const interlocus_recombination_function interlocus_recombination;
         std::vector<uint_t> neutral;
         std::vector<uint_t> selected;
 
@@ -26,7 +28,7 @@ namespace fwdpp
                   generate_mutations_param) },
               generate_breakpoints{ std::forward<rec>(
                   generate_breakpoints_param) },
-              neutral{}, selected{}
+              interlocus_recombination{}, neutral{}, selected{}
         {
         }
     };
@@ -34,16 +36,33 @@ namespace fwdpp
     template <typename genetic_value, typename mutation_function,
               typename recombination_function>
     inline genetic_parameters<genetic_value, mutation_function,
-                              recombination_function>
+                              recombination_function, std::nullptr_t>
     make_genetic_parameters(
         genetic_value&& gvalue_param,
         mutation_function&& generate_mutations_param,
         recombination_function&& generate_breakpoints_param)
     {
         return genetic_parameters<genetic_value, mutation_function,
-                                  recombination_function>(
+                                  recombination_function, std::nullptr_t>(
             std::move(gvalue_param), std::move(generate_mutations_param),
             std::move(generate_breakpoints_param));
+    }
+
+    template <typename genetic_value, typename mutation_function,
+              typename recombination_function,
+              typename interlocus_recombination>
+    inline genetic_parameters<genetic_value, mutation_function,
+                              recombination_function, interlocus_recombination>
+    make_genetic_parameters(
+        genetic_value&& gvalue_param,
+        mutation_function&& generate_mutations_param,
+        recombination_function&& generate_breakpoints_param,
+        interlocus_recombination&& interlocus_rec)
+    {
+        return genetic_parameters<genetic_value, mutation_function,
+                                  recombination_function, std::nullptr_t>(
+            std::move(gvalue_param), std::move(generate_mutations_param),
+            std::move(generate_breakpoints_param), std::move(interlocus_rec));
     }
 
 } // namespace fwdpp
