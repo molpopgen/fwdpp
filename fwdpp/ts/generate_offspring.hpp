f445c55848c4221a34c33c5ec712a18b1d311039
molpopgen <krthornt@uci.edu>
2018-12-06 13:03:39 -0800

    Fix several errors in the interface revealed by testing.

--- fwdpp/ts/generate_offspring.hpp
+++ fwdpp/ts/generate_offspring.hpp
@@ -2,6 +2,7 @@
 #define FWDPP_TS_GENERATE_OFFSPRING_HPP
 
 #include <tuple>
+#include <vector>
 #include <utility>
 #include <type_traits>
 #include <gsl/gsl_rng.h>
@@ -19,13 +20,13 @@ namespace fwdpp
     {
         struct mut_rec_intermediates
         {
+            int swapped;
             std::vector<double> breakpoints;
             std::vector<uint_t> mutation_keys;
             template <typename B, typename M>
-            mut_rec_intermediates(B&& b, M&& m)
-                : breakpoints{ std::forward<B>(b) }, mutation_keys{
-                      std::forward<M>(m)
-                  }
+            mut_rec_intermediates(int s, B&& b, M&& m)
+                : swapped{ s }, breakpoints{ std::forward<B>(b) },
+                  mutation_keys{ std::forward<M>(m) }
             {
             }
         };
@@ -38,7 +39,7 @@ namespace fwdpp
         {
         };
 
-		//TODO: much of the stuff below can go into an internal namespace
+        //TODO: much of the stuff below can go into an internal namespace
         template <typename key_vector, typename mcont_t>
         inline wrapped_range<typename key_vector::iterator>
         process_new_mutations(const key_vector& new_mutation_keys,
@@ -49,48 +50,44 @@ namespace fwdpp
         }
 
         template <typename key_vector, typename mcont_t>
-        inline typename key_vector::iterator
-        process_new_mutations(const key_vector& new_mutation_keys,
-                              const mcont_t& mutations, selected_variants_only)
+        inline wrapped_range<typename key_vector::iterator>
+        process_new_mutations(key_vector& new_mutation_keys,
+                              mcont_t& mutations, selected_variants_only)
         {
             auto itr = std::stable_partition(
-                new_mutation_keys.begin(), new_mutation_keys.end(),
+                begin(new_mutation_keys), end(new_mutation_keys),
                 [&mutations](typename key_vector::value_type k) {
                     return mutations[k].neutral == false;
                 });
             return make_wrapped_range(begin(new_mutation_keys), itr);
         }
 
-        template <typename genetic_param_holder, typename poptype,
-                  typename recmodel, typename mutmodel, typename recycling_bin,
-                  typename mutation_key_container>
+        template <typename poptype, typename recmodel, typename mutmodel,
+                  typename recycling_bin>
         inline mut_rec_intermediates
         generate_mutations_and_breakpoints(
-            std::size_t parent, std::size_t parental_gamete,
+            std::size_t parent, std::size_t parental_gamete, int swapped,
             const recmodel& generate_breakpoints,
             const mutmodel& generate_mutations,
-            recycling_bin& mutation_recycling_bin,
-            recycling_bin& gamete_recycling_bin,
-            mutation_key_container& neutral, mutation_key_container& selected,
-            poptype& pop)
+            recycling_bin& mutation_recycling_bin, poptype& pop)
         {
             auto breakpoints = generate_breakpoints();
             auto new_mutation_keys = fwdpp_internal::mmodel_dispatcher(
                 generate_mutations, pop.diploids[parent],
                 pop.gametes[parental_gamete], pop.mutations,
                 mutation_recycling_bin);
-            return mut_rec_intermediates(std::move(breakpoints),
+            return mut_rec_intermediates(swapped, std::move(breakpoints),
                                          std::move(new_mutation_keys));
         }
 
         struct parental_data
         {
             std::size_t index, gamete1, gamete2;
+            int swapped;
         };
 
-        template <typename genetic_param_holder, typename poptype,
-                  typename recmodel, typename mutmodel, typename recycling_bin,
-                  typename mutation_key_container,
+        template <typename poptype, typename recmodel, typename mutmodel,
+                  typename recycling_bin, typename mutation_key_container,
                   typename mutation_handling_policy>
         inline std::pair<std::size_t, mut_rec_intermediates>
         generate_offspring_gamete(
@@ -103,9 +100,9 @@ namespace fwdpp
             poptype& pop)
         {
             auto gamete_data = generate_mutations_and_breakpoints(
-                parent.index, parent.gamete1, generate_breakpoints,
-                generate_mutations, mutation_recycling_bin,
-                gamete_recycling_bin, neutral, selected, pop);
+                parent.index, parent.gamete1, parent.swapped,
+                generate_breakpoints, generate_mutations,
+                mutation_recycling_bin, pop);
             auto range = process_new_mutations(gamete_data.mutation_keys,
                                                pop.mutations, mutation_policy);
             std::size_t offspring_gamete = mutate_recombine(
@@ -115,18 +112,6 @@ namespace fwdpp
             return std::make_pair(offspring_gamete, std::move(gamete_data));
         }
 
-        // NOTE The entire problem that we have here is due
-        // to the idea that we may or may not want to put neutral mutations
-        // into the gametes.  This function could be much simpler
-        // if we just returned breakpoints and new mutation keys.
-        //
-        // We almost certainly DO want to reuse fwdpp::mutate_recombine,
-        // which constrains us.  However, a lot of the hand-wringing is having
-        // to do with not wanting to copy the vector of new mutation keys. But,
-        // fwdpp::mutate_recombine uses an iterator range to update the new gamete
-        // in one place, and a range-based for loop in another.  We should consider
-        // a simplification where we change the API of that function to take a
-        // pair of iterators. We could to likewise with breakpoints, too.
         template <typename genetic_param_holder,
                   typename mutation_handling_policy, typename poptype>
         std::pair<mut_rec_intermediates, mut_rec_intermediates>
@@ -159,20 +144,20 @@ namespace fwdpp
                     std::swap(p2g1, p2g2);
                 }
             auto offspring_first_gamete_data = generate_offspring_gamete(
-                parental_data{ parents.first, p1g1, p1g2 },
+                parental_data{ parents.first, p1g1, p1g2, swap1 },
                 genetics.generate_breakpoints, genetics.generate_mutations,
                 mutation_policy, genetics.mutation_recycling_bin,
                 genetics.gamete_recycling_bin, genetics.neutral,
                 genetics.selected, pop);
             auto offspring_second_gamete_data = generate_offspring_gamete(
-                parental_data{ parents.second, p2g1, p2g2 },
+                parental_data{ parents.second, p2g1, p2g2, swap2 },
                 genetics.generate_breakpoints, genetics.generate_mutations,
                 mutation_policy, genetics.mutation_recycling_bin,
                 genetics.gamete_recycling_bin, genetics.neutral,
                 genetics.selected, pop);
-			// Update the offspring's gametes.
-			offspring.first=offspring_first_gamete_data.first;
-			offspring.second=offspring_second_gamete_data.second;
+            // Update the offspring's gametes.
+            offspring.first = offspring_first_gamete_data.first;
+            offspring.second = offspring_second_gamete_data.first;
             return std::make_pair(
                 std::move(offspring_first_gamete_data.second),
                 std::move(offspring_second_gamete_data.second));
