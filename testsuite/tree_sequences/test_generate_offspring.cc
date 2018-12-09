6c2f40da5dc515c93ad7a7089045cb99a61b2d50
molpopgen <krthornt@uci.edu>
2018-12-09 08:17:41 -0800

    add empty test file with dummy fixture

---
+++ testsuite/tree_sequences/test_generate_offspring.cc
@@ -0,0 +1,14 @@
+#include <config.h>
+#include <cmath>
+#include <fwdpp/ts/generate_offspring.hpp>
+#include <fwdpp/popgenmut.hpp>
+#include <fwdpp/mlocuspop.hpp>
+#include <boost/test/unit_test.hpp>
+
+struct multilocus_fixture
+{
+};
+
+BOOST_FIXTURE_TEST_CASE(test_multilocus, multilocus_fixture)
+{
+}
