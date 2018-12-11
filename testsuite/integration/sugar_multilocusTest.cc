8813dfcd8c5ea30fb6431db882a4cca5bb1de5d0
molpopgen <krthornt@uci.edu>
2018-12-11 11:51:29 -0800

    refactor test to make better use of fixture

--- testsuite/integration/sugar_multilocusTest.cc
+++ testsuite/integration/sugar_multilocusTest.cc
@@ -19,46 +19,45 @@
 #include "../util/quick_evolve_sugar.hpp"
 
 using poptype = mlocuspop_popgenmut_fixture::poptype;
+BOOST_FIXTURE_TEST_SUITE(mlocus_tests, mlocuspop_popgenmut_fixture)
 
 BOOST_AUTO_TEST_CASE(mlocuspop_sugar_test1)
 {
-    mlocuspop_popgenmut_fixture f;
-    simulate_mlocuspop(f.pop, f.rng, f.mutmodels, f.recmodels,
-                       mlocuspop_popgenmut_fixture::multilocus_additive(), f.mu,
-                       f.rbw, f.generation);
-    poptype pop2(f.pop);
-    BOOST_CHECK_EQUAL(f.pop == pop2, true);
+    simulate_mlocuspop(pop, rng, mutmodels, recmodels,
+                       mlocuspop_popgenmut_fixture::multilocus_additive(), mu,
+                       rbw, generation);
+    decltype(pop) pop2(pop);
+    BOOST_CHECK_EQUAL(pop == pop2, true);
 }
 
 BOOST_AUTO_TEST_CASE(mlocuspop_sugar_test2)
 {
-    mlocuspop_popgenmut_fixture f;
-    simulate_mlocuspop(f.pop, f.rng, f.mutmodels, f.recmodels,
-                       mlocuspop_popgenmut_fixture::multilocus_additive(), f.mu,
-                       f.rbw, f.generation);
-    poptype pop2(0, decltype(poptype::locus_boundaries)());
+    simulate_mlocuspop(pop, rng, mutmodels, recmodels,
+                       mlocuspop_popgenmut_fixture::multilocus_additive(), mu,
+                       rbw, generation);
+    decltype(pop) pop2(0, decltype(poptype::locus_boundaries)());
     std::stringstream buffer;
-    fwdpp::io::serialize_population(buffer, f.pop);
+    fwdpp::io::serialize_population(buffer, pop);
     fwdpp::io::deserialize_population(buffer, pop2);
-    BOOST_CHECK_EQUAL(f.pop == pop2, true);
+    BOOST_CHECK_EQUAL(pop == pop2, true);
 }
 
 BOOST_AUTO_TEST_CASE(mlocuspop_sugar_test3)
 {
-    mlocuspop_popgenmut_fixture f;
-    simulate_mlocuspop(f.pop, f.rng, f.mutmodels, f.recmodels,
-                       mlocuspop_popgenmut_fixture::multilocus_additive(), f.mu,
-                       f.rbw, f.generation);
-    poptype pop2(std::move(f.pop));
-    BOOST_CHECK_EQUAL(f.pop == pop2, false);
+    simulate_mlocuspop(pop, rng, mutmodels, recmodels,
+                       mlocuspop_popgenmut_fixture::multilocus_additive(), mu,
+                       rbw, generation);
+    auto pop2(std::move(pop));
+    BOOST_CHECK_EQUAL(pop == pop2, false);
 }
 
 BOOST_AUTO_TEST_CASE(mlocuspop_sugar_test4)
 {
-    mlocuspop_popgenmut_fixture f;
-    simulate_mlocuspop(f.pop, f.rng, f.mutmodels, f.recmodels,
-                       mlocuspop_popgenmut_fixture::multilocus_additive(), f.mu,
-                       f.rbw, f.generation);
-    poptype pop2 = std::move(f.pop);
-    BOOST_CHECK_EQUAL(f.pop == pop2, false);
+    simulate_mlocuspop(pop, rng, mutmodels, recmodels,
+                       mlocuspop_popgenmut_fixture::multilocus_additive(), mu,
+                       rbw, generation);
+    auto pop2 = std::move(pop);
+    BOOST_CHECK_EQUAL(pop == pop2, false);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
