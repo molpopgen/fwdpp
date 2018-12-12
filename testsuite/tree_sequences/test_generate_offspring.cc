678586c0e6e7f223229d0a3625fc44663939d3b1
molpopgen <krthornt@uci.edu>
2018-12-11 19:40:51 -0800

    Document the fundamental logic of the test fixture.

--- testsuite/tree_sequences/test_generate_offspring.cc
+++ testsuite/tree_sequences/test_generate_offspring.cc
@@ -15,6 +15,97 @@
 
 #include "../fixtures/multilocus_fixture_deterministic.hpp"
 
+/* Let's describe the fixture setup, expected outputs, etc.:
+ *
+ * The crossover functions in the fixture will assign the following numbers
+ * of xover events w/in and b/w loci:
+ *
+ * Locus w/in b/w*
+ * 0     0    N/A
+ * 1     1    0
+ * 2     0    1
+ * 3     1    2
+ *
+ * *The entry for b/w loci refers to the number of xover events
+ * b/w locus i and i+1.
+ *
+ * An xover w/in locus i occurs at position i + 0.5
+ *
+ * Let's draw the 4 loci in two parent chromosomes:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++++++ ++++++++ ++++++++   
+ * -------- -------- -------- --------   
+ *
+ * The way multi-locus recombination works is that we apply
+ * each recombination event from left to right.  Internally, this
+ * is not literally what happens, but it is our mental image of 
+ * what we're trying to accomplish
+ *
+ * We will consider the case of NO INITIAL SWAPPING of either
+ * parent gamete, and "build" the first offspring gamete.
+ *
+ * There are no x-overs w/in locus 1, so after processing it,
+ * we should have:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++++++ ++++++++ ++++++++   
+ *
+ * Then, there is an xover w/in locus 1, giving:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++---- -------- --------   
+ *
+ * Then, 1 x-over event b/w loci 1 and 2:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++---- ++++++++ ++++++++   
+ *
+ * No recombination events w/in locus 2:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++---- ++++++++ ++++++++   
+ *
+ * Two recombination events b/w loci 2 and 3,
+ * which means double x-over, which means 
+ * no change in the system:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++---- ++++++++ ++++++++   
+ *
+ * Finally, recombination w/in locus 3:
+ *
+ * 0      1 1      2 2      3 3      4
+ * ++++++++ ++++---- ++++++++ ++++----   
+ *
+ * Thus, the parent's first gamete gives 
+ * the following interval's to the offspring:
+ *
+ * [0, 1.5)
+ * [2, 3.5)
+ *
+ * The parent's second gamete passed on the following
+ * intervals:
+ *
+ * [1.5, 2.0)
+ * [3.5, 4.0)
+ *
+ * Thus, the breakpoints we need to send for tree 
+ * sequence recording are:
+ *
+ * [1.5, 2.0, 3.5, DBL_MAX],
+ *
+ * assuming that 4 is set as the "genome length"
+ * of a table_collection.
+ *
+ * For the purpose of generating gametes, that "2.0"
+ * breakpoint never needs to be stored, because parent 
+ * gamete swapping, etc., takes care of that implicitly.
+ *
+ * Currently, we are FAILING to record it as a breakpoint
+ * for ts recording, meaning that our tests below are 
+ * WRONG.
+ */
 BOOST_FIXTURE_TEST_CASE(check_multilocus_deterministic_fixture,
                         multilocus_fixture_deterministic)
 {
