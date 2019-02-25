972068f5366c7e6b0c4f504effdb81cec58223ea
molpopgen <krthornt@uci.edu>
2019-02-25 13:07:58 -0800

    Refactor mark_multiple_roots to use root tracking instead of brute-force algorithm.

--- fwdpp/ts/mark_multiple_roots.hpp
+++ fwdpp/ts/mark_multiple_roots.hpp
@@ -26,46 +26,14 @@ namespace fwdpp
             tree_visitor mti(tables, samples);
             while (mti(std::true_type(), std::false_type()))
                 {
-                    bool single_root = false;
                     auto &tree = mti.tree();
-                    for (auto &s : samples)
+                    if (tree.num_roots() > 1)
                         {
-                            auto p = s;
-                            auto lp = p;
-                            while (p != -1)
+                            auto lr = tree.left_root;
+                            while (lr != TS_NULL_NODE)
                                 {
-                                    lp = p;
-                                    p = tree.parents[p];
-                                }
-                            if (static_cast<std::size_t>(tree.leaf_counts[lp])
-                                == samples.size())
-                                {
-                                    single_root = true;
-                                }
-                            else
-                                {
-                                    auto itr = rv.find(lp);
-                                    auto w = std::make_pair(tree.left,
-                                                            tree.right);
-                                    if (itr == rv.end())
-                                        {
-                                            rv[lp].emplace_back(std::move(w));
-                                        }
-                                    else
-                                        {
-                                            assert(!itr->second.empty());
-                                            if (std::find(itr->second.begin(),
-                                                          itr->second.end(), w)
-                                                == itr->second.end())
-                                                {
-                                                    itr->second.emplace_back(
-                                                        std::move(w));
-                                                }
-                                        }
-                                }
-                            if (single_root)
-                                {
-                                    break;
+                                    rv[lr].emplace_back(tree.left, tree.right);
+                                    lr = tree.right_sib[lr];
                                 }
                         }
                 }
