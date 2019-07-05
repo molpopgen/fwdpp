3451f59142183f201c1069536739b7bfaa9cba5a
molpopgen <krthornt@uci.edu>
2019-07-05 12:25:43 -0700

    add test of roots as as we decaptitate trees.

--- testsuite/tree_sequences/test_node_traversal.cc
+++ testsuite/tree_sequences/test_node_traversal.cc
@@ -2,7 +2,49 @@
 #include <boost/test/unit_test.hpp>
 #include "simple_table_collection.hpp"
 #include <fwdpp/ts/tree_visitor.hpp>
+#include <fwdpp/ts/decapitate.hpp>
 #include <fwdpp/ts/marginal_tree_functions.hpp>
+
+BOOST_FIXTURE_TEST_SUITE(test_root_traversal_simple_table_collection,
+                         simple_table_collection)
+
+BOOST_AUTO_TEST_CASE(test_roots)
+{
+    auto r = fwdpp::ts::get_roots(tv.tree());
+    BOOST_REQUIRE_EQUAL(r.size(), 1);
+    BOOST_REQUIRE_EQUAL(r[0], 6);
+}
+
+BOOST_AUTO_TEST_CASE(test_roots_after_decapitation)
+{
+    fwdpp::ts::decapitate(tables, 0.);
+    std::vector<fwdpp::ts::TS_NODE_INT> expected_roots{ 4, 5 };
+    tv = fwdpp::ts::tree_visitor(tables, samples);
+    tv(std::false_type(), std::false_type());
+    auto r = fwdpp::ts::get_roots(tv.tree());
+    BOOST_REQUIRE_EQUAL(r.size(), 2);
+    BOOST_REQUIRE_EQUAL(r == expected_roots, true);
+
+    fwdpp::ts::decapitate(tables, 1);
+    expected_roots = { 2, 3, 4 };
+    tv = fwdpp::ts::tree_visitor(tables, samples);
+    tv(std::false_type(), std::false_type());
+    r = fwdpp::ts::get_roots(tv.tree());
+    std::sort(begin(r), end(r));
+    BOOST_REQUIRE_EQUAL(r.size(), 3);
+    BOOST_REQUIRE_EQUAL(r == expected_roots, true);
+
+    fwdpp::ts::decapitate(tables, 2);
+    expected_roots = { 0, 1, 2, 3 };
+    tv = fwdpp::ts::tree_visitor(tables, samples);
+    tv(std::false_type(), std::false_type());
+    r = fwdpp::ts::get_roots(tv.tree());
+    std::sort(begin(r), end(r));
+    BOOST_REQUIRE_EQUAL(r.size(), 4);
+    BOOST_REQUIRE_EQUAL(r == expected_roots, true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_FIXTURE_TEST_SUITE(test_simple_table_collection_traversal,
                          simple_table_collection)
