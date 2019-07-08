66efb01ca9a14f206e14c0da173a56ea54eebaa5
molpopgen <krthornt@uci.edu>
2019-07-07 17:11:56 -0700

    Attempt to set CXX.

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -6,7 +6,7 @@ then
     export LD_LIBRARY_PATH=$HOME/miniconda/lib
     export CPPFLAGS="-I$HOME/miniconda/include $CPPFLAGS"
     export LDFLAGS="-L$HOME/miniconda/lib $LDFLAGS"
-    LDFLAGS="-L$HOME/miniconda/lib -Wl,-rpath,$HOME/miniconda/lib" ./configure --prefix=$HOME && make -j 3 &&  make install
+    CXX=$CXX LDFLAGS="-L$HOME/miniconda/lib -Wl,-rpath,$HOME/miniconda/lib" ./configure --prefix=$HOME && make -j 3 &&  make install
 else
     CXXFLAGS="-std=$CXXSTANDARD -O2"
     ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
