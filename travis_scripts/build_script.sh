aa9cf80341b7b995f2fdcd45a396447b40b7ab9a
molpopgen <krthornt@uci.edu>
2019-07-07 17:44:25 -0700

    Set CC

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -5,7 +5,8 @@ then
     echo "details..."
     echo `g++ -v`
     echo "details done..."
-    CXX=g++ ./configure --prefix=$HOME && make -j 3 &&  make install
+    echo `gcc -v`
+    CXX=g++ CC=gcc ./configure --prefix=$HOME && make -j 3 &&  make install
 else
     CXXFLAGS="-std=$CXXSTANDARD -O2"
     ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
