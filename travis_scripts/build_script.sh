b56e78e6a04819bdb374eb95f9430397cd546d2f
molpopgen <krthornt@uci.edu>
2019-07-08 07:29:53 -0700

    Set PATH for conda on travis.

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -6,6 +6,7 @@ then
     echo `g++ -v`
     echo `gcc -v`
     echo "details done..."
+    export PATH="$HOME/miniconda/bin:$PATH"
     CXX=g++ CC=gcc CPPFLAGS=-I$HOME/miniconda/include LDFLAGS="-L$HOME/miniconda/lib -Wl,-rpath,$HOME/miniconda/lib" ./configure --prefix=$HOME && make -j 3 &&  make install
 else
     ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
