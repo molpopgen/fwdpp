7aa791ef51094eacaed96778db63cca9582e0dd8
molpopgen <krthornt@uci.edu>
2019-07-07 17:40:59 -0700

    Set CXX for configure step.

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -4,9 +4,8 @@ if [ "$USECONDA" == "1" ];
 then
     echo "details..."
     echo `g++ -v`
-    echo $CXX
     echo "details done..."
-    ./configure --prefix=$HOME && make -j 3 &&  make install
+    CXX=g++ ./configure --prefix=$HOME && make -j 3 &&  make install
 else
     CXXFLAGS="-std=$CXXSTANDARD -O2"
     ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
