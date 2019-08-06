eac3e1bb5170356d0c6ddece80ad410735af20ee
molpopgen <krthornt@uci.edu>
2019-08-06 13:15:48 -0700

    Fix error indexing evolve function in test.  (Error was found after rebasing in f164349.)  Modify variable names to be less confusing to future self.

--- testsuite/tree_sequences/test_diploid_edge_buffer.cc
+++ testsuite/tree_sequences/test_diploid_edge_buffer.cc
@@ -5,7 +5,7 @@
 
 std::pair<fwdpp::ts::TS_NODE_INT, unsigned>
 diploid_wf(const fwdpp::GSLrng_mt& rng, unsigned N,
-           unsigned starting_generation, unsigned ngenerations,
+           unsigned parental_generation, unsigned ngenerations,
            fwdpp::ts::TS_NODE_INT first_parental_index,
            fwdpp::ts::table_collection& tables,
            fwdpp::ts::diploid_edge_buffer& buffer)
@@ -15,8 +15,8 @@ diploid_wf(const fwdpp::GSLrng_mt& rng, unsigned N,
     fwdpp::ts::TS_NODE_INT next_offspring_node;
     std::vector<double> breakpoints; //No rec.
     const fwdpp::ts::TS_NODE_INT offspring_deme{ 0 };
-    unsigned generation = starting_generation;
-    for (; generation < starting_generation + ngenerations; ++generation)
+    unsigned generation = 1;
+    for (; generation <= ngenerations; ++generation)
         {
             buffer.begin_epoch(N);
             for (unsigned offspring = 0; offspring < N; ++offspring)
@@ -32,7 +32,8 @@ diploid_wf(const fwdpp::GSLrng_mt& rng, unsigned N,
                     next_offspring_node
                         = fwdpp::ts::register_offspring_and_buffer_edges(
                             breakpoints, parent, std::make_tuple(pn0, pn1),
-                            offspring_deme, generation, tables, buffer);
+                            offspring_deme, parental_generation + generation,
+                            tables, buffer);
                     if (next_offspring_node == fwdpp::ts::TS_NULL_NODE)
                         {
                             // NOTE: this is impossible, and really just
@@ -52,7 +53,8 @@ diploid_wf(const fwdpp::GSLrng_mt& rng, unsigned N,
                     next_offspring_node
                         = fwdpp::ts::register_offspring_and_buffer_edges(
                             breakpoints, parent, std::make_tuple(pn0, pn1),
-                            offspring_deme, generation, tables, buffer);
+                            offspring_deme, parental_generation + generation,
+                            tables, buffer);
                     if (next_offspring_node == fwdpp::ts::TS_NULL_NODE)
                         {
                             throw std::runtime_error("NULL offspring node");
@@ -61,7 +63,8 @@ diploid_wf(const fwdpp::GSLrng_mt& rng, unsigned N,
             parent_index_offset += 2 * N;
             buffer.end_epoch();
         }
-    return std::make_pair(parent_index_offset, generation);
+    return std::make_pair(parent_index_offset,
+                          parental_generation + generation - 1);
 }
 
 struct wfnorec
@@ -71,10 +74,12 @@ struct wfnorec
     fwdpp::ts::table_collection tables;
     fwdpp::ts::diploid_edge_buffer buffer;
     fwdpp::GSLrng_mt rng;
-    unsigned generation;
+    unsigned parental_generation;
     wfnorec()
         : L{ 1.0 }, N{ 1000 },
-          tables(2 * N, 0.0, 0, L), buffer{}, rng{ 42 }, generation{ 0 }
+          tables(2 * N, 0.0, 0, L), buffer{}, rng{ 42 }, parental_generation{
+              0
+          }
     {
     }
 };
@@ -83,24 +88,27 @@ BOOST_AUTO_TEST_SUITE(test_diploid_edge_buffer)
 
 BOOST_FIXTURE_TEST_CASE(test_sort_order, wfnorec)
 {
-    auto p = diploid_wf(rng, N, generation, 100, 0, tables, buffer);
-    generation = p.second;
+    auto p = diploid_wf(rng, N, parental_generation, 100, 0, tables, buffer);
+    parental_generation = p.second;
     buffer.prepare_tables_for_simplification(tables);
-    BOOST_REQUIRE_EQUAL(tables.edge_table.size(), generation * 2 * N);
-    BOOST_REQUIRE_EQUAL(tables.node_table.size(), (generation + 1) * 2 * N);
+    BOOST_REQUIRE_EQUAL(tables.edge_table.size(), parental_generation * 2 * N);
+    BOOST_REQUIRE_EQUAL(tables.node_table.size(),
+                        (parental_generation + 1) * 2 * N);
     BOOST_REQUIRE_EQUAL(tables.edges_are_sorted(), true);
 }
 
 BOOST_FIXTURE_TEST_CASE(test_two_rounds, wfnorec)
 {
-    auto p = diploid_wf(rng, N, generation, 100, 0, tables, buffer);
+    auto p = diploid_wf(rng, N, parental_generation, 100, 0, tables, buffer);
     buffer.prepare_tables_for_simplification(tables);
-    generation = p.second;
-    p = diploid_wf(rng, N, generation, 100, p.first, tables, buffer);
-    generation = p.second;
+    tables.edge_offset = tables.edge_table.size();
+    parental_generation = p.second;
+    p = diploid_wf(rng, N, parental_generation, 100, p.first, tables, buffer);
+    parental_generation = p.second;
     buffer.prepare_tables_for_simplification(tables);
-    BOOST_REQUIRE_EQUAL(tables.edge_table.size(), generation * 2 * N);
-    BOOST_REQUIRE_EQUAL(tables.node_table.size(), (generation + 1) * 2 * N);
+    BOOST_REQUIRE_EQUAL(tables.edge_table.size(), parental_generation * 2 * N);
+    BOOST_REQUIRE_EQUAL(tables.node_table.size(),
+                        (parental_generation + 1) * 2 * N);
     BOOST_REQUIRE_EQUAL(tables.edges_are_sorted(), true);
 }
 
