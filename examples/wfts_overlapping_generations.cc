d2622277e047689dbb3fbbc9f762bac0b61b88dc
molpopgen <krthornt@uci.edu>
2019-12-05 11:23:25 -0800

    Fix usage line for example program.

--- examples/wfts_overlapping_generations.cc
+++ examples/wfts_overlapping_generations.cc
@@ -11,7 +11,7 @@ main(int argc, char** argv)
         {
             std::cerr
                 << "usage: " << argv[0]
-                << "popsize ngenerations psurvival recrate simplify seed\n";
+                << " popsize ngenerations psurvival recrate simplify seed\n";
             std::exit(1);
         }
     int argnext = 1;
