1ff81cffbe058dd55161f9bced0f493fe5a0cb48
molpopgen <krthornt@uci.edu>
2020-04-24 14:07:21 -0700

    Remove object constructors allowing the creation of populations from vectors of lower-level types.
    Closes #256

--- fwdpp/poptypes/diploid_population.hpp
+++ fwdpp/poptypes/diploid_population.hpp
@@ -22,46 +22,28 @@ namespace fwdpp
         template <typename mutation_type, typename mcont, typename gcont,
                   typename dipvector, typename mvector, typename ftvector,
                   typename lookup_table_type>
-        class diploid_population
-            : public popbase<mutation_type, mcont, gcont, mvector, ftvector,
-                             lookup_table_type>
+        class diploid_population : public popbase<mutation_type, mcont, gcont, mvector,
+                                                  ftvector, lookup_table_type>
         {
-          private:
-            void
-            process_individual_input()
-            {
-                std::vector<uint_t> gcounts(this->haploid_genomes.size(), 0);
-                for (auto &&dip : diploids)
-                    {
-                        this->validate_individual_keys(dip.first);
-                        this->validate_individual_keys(dip.second);
-                        gcounts[dip.first]++;
-                        gcounts[dip.second]++;
-                    }
-                this->validate_haploid_genome_counts(gcounts);
-            }
-
           public:
             virtual ~diploid_population() = default;
             diploid_population(diploid_population &&) = default;
             diploid_population(const diploid_population &) = default;
             diploid_population &operator=(diploid_population &&) = default;
-            diploid_population &operator=(const diploid_population &)
-                = default;
+            diploid_population &operator=(const diploid_population &) = default;
             //! Population size
             uint_t N;
 
             using dipvector_t = dipvector;
             using diploid_t = typename dipvector::value_type;
             //! Typedef for base class
-            using popbase_t = popbase<mutation_type, mcont, gcont, mvector,
-                                      ftvector, lookup_table_type>;
+            using popbase_t = popbase<mutation_type, mcont, gcont, mvector, ftvector,
+                                      lookup_table_type>;
             //! Dispatch tag
             using popmodel_t = poptypes::DIPLOID_TAG;
             //! Fitness function signature compatible with this type
             using fitness_t
-                = fwdpp::traits::fitness_fxn_t<dipvector_t,
-                                               typename popbase_t::gcont_t,
+                = fwdpp::traits::fitness_fxn_t<dipvector_t, typename popbase_t::gcont_t,
                                                typename popbase_t::mcont_t>;
 
             //! Container of diploids
@@ -79,24 +61,10 @@ namespace fwdpp
             {
             }
 
-            template <typename diploids_input, typename haploid_genomes_input,
-                      typename mutations_input>
-            explicit diploid_population(diploids_input &&d, haploid_genomes_input &&g,
-                                        mutations_input &&m)
-                : popbase_t(std::forward<haploid_genomes_input>(g),
-                            std::forward<mutations_input>(m), 100),
-                  N{ static_cast<decltype(N)>(d.size()) },
-                  diploids(std::forward<diploids_input>(d))
-            //! Constructor for pre-determined population status
-            {
-                this->process_individual_input();
-            }
-
             bool
             operator==(const diploid_population &rhs) const
             {
-                return this->diploids == rhs.diploids
-                       && popbase_t::is_equal(rhs);
+                return this->diploids == rhs.diploids && popbase_t::is_equal(rhs);
             }
 
             //! Empty all the containers
--- testsuite/unit/test_mutate_recombine.cc
+++ testsuite/unit/test_mutate_recombine.cc
@@ -10,57 +10,58 @@ BOOST_AUTO_TEST_SUITE(test_mutate_recombine)
 
 BOOST_FIXTURE_TEST_CASE(test_boundary, diploid_population_objects)
 {
-    poptype pop(diploids, haploid_genomes, mutations);
-
+    std::vector<fwdpp::uint_t> mcounts;
     // We are going to manually add a neutral mutation at position 1.5
-    pop.mutations.emplace_back(1.5, 0.0, 1.0, 1);
-    pop.mcounts.push_back(1); // mock the mutation count
+    mutations.emplace_back(1.5, 0.0, 1.0, 1);
+    mcounts.push_back(1); // mock the mutation count
     // We add the mutation to the second haploid_genome of the pop
     fwdpp::fwdpp_internal::insert_new_mutation(
-        pop.haploid_genomes[1].mutations.begin(), pop.haploid_genomes[1].mutations.end(),
-        pop.mutations.size() - 1, pop.mutations, pop.haploid_genomes[1].mutations);
+        haploid_genomes[1].mutations.begin(), haploid_genomes[1].mutations.end(),
+        mutations.size() - 1, mutations, haploid_genomes[1].mutations);
 
-    BOOST_REQUIRE_EQUAL(pop.mutations.back().pos, 1.5);
+    BOOST_REQUIRE_EQUAL(mutations.back().pos, 1.5);
     std::vector<fwdpp::uint_t> new_mutations;
-    std::vector<double> breakpoints{ 1.5, std::numeric_limits<double>::max() };
-    auto q = fwdpp::make_haploid_genome_queue(pop.haploid_genomes);
+    std::vector<double> breakpoints{1.5, std::numeric_limits<double>::max()};
+    auto q = fwdpp::make_haploid_genome_queue(haploid_genomes);
     BOOST_REQUIRE_EQUAL(q.get().empty(), true);
 
-    auto new_haploid_genome_index = fwdpp::mutate_recombine(
-        new_mutations, breakpoints, 0, 1, pop.haploid_genomes, pop.mutations, q,
-        pop.neutral, pop.selected);
+    auto new_haploid_genome_index
+        = fwdpp::mutate_recombine(new_mutations, breakpoints, 0, 1, haploid_genomes,
+                                  mutations, q, neutral, selected);
 
     // The test is that the new haploid_genome DOES contain a mutation
     // at pos 1.5
-    auto itr = std::find(pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
-                         pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
-                         pop.mutations.size() - 1);
-    auto is_end = (itr == pop.haploid_genomes[new_haploid_genome_index].mutations.end());
+    auto itr = std::find(haploid_genomes[new_haploid_genome_index].mutations.begin(),
+                         haploid_genomes[new_haploid_genome_index].mutations.end(),
+                         mutations.size() - 1);
+    auto is_end = (itr == haploid_genomes[new_haploid_genome_index].mutations.end());
     BOOST_REQUIRE_EQUAL(is_end, false);
 
     // But, if we switch the parent IDs,
     // then this mutation WILL appear in the offspring haploid_genome
-    new_haploid_genome_index = fwdpp::mutate_recombine(new_mutations, breakpoints, 1,
-                                               0, pop.haploid_genomes, pop.mutations,
-                                               q, pop.neutral, pop.selected);
+    new_haploid_genome_index
+        = fwdpp::mutate_recombine(new_mutations, breakpoints, 1, 0, haploid_genomes,
+                                  mutations, q, neutral, selected);
 
     // The test is that the new haploid_genome DOES NOT contain a mutation
     // at pos 1.5
-    itr = std::find(pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
-                    pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
-                    pop.mutations.size() - 1);
-    is_end = (itr == pop.haploid_genomes[new_haploid_genome_index].mutations.end());
+    itr = std::find(haploid_genomes[new_haploid_genome_index].mutations.begin(),
+                    haploid_genomes[new_haploid_genome_index].mutations.end(),
+                    mutations.size() - 1);
+    is_end = (itr == haploid_genomes[new_haploid_genome_index].mutations.end());
     BOOST_REQUIRE_EQUAL(is_end, true);
 }
 
 BOOST_FIXTURE_TEST_CASE(test_boundary_with_recurrent_mutation,
                         diploid_population_objects)
-    /// This test is similar to the above, but we use the xtra field of
-    /// fwdpp::popgenmut for extra resolution about how mutation vs
-    /// inherited mutations are treated vis-a-vis crossover breakpoints
+/// This test is similar to the above, but we use the xtra field of
+/// fwdpp::popgenmut for extra resolution about how mutation vs
+/// inherited mutations are treated vis-a-vis crossover breakpoints
 {
-    poptype pop(diploids, haploid_genomes, mutations);
-
+    poptype pop(diploids.size());
+    pop.diploids = diploids;
+    pop.haploid_genomes = haploid_genomes;
+    pop.mutations = mutations;
     // We are going to manually add a neutral mutation at position 1.5
     pop.mutations.emplace_back(1.5, 0.0, 1.0, 1, 7);
     pop.mcounts.push_back(1); // mock the mutation count
@@ -76,13 +77,13 @@ BOOST_FIXTURE_TEST_CASE(test_boundary_with_recurrent_mutation,
     BOOST_REQUIRE_EQUAL(pop.mutations.back().pos, 1.5);
     // The new mutation at 1.5 is at the end
     std::vector<fwdpp::uint_t> new_mutations(1, pop.mutations.size() - 1);
-    std::vector<double> breakpoints{ 1.5, std::numeric_limits<double>::max() };
+    std::vector<double> breakpoints{1.5, std::numeric_limits<double>::max()};
     auto q = fwdpp::make_haploid_genome_queue(pop.haploid_genomes);
     BOOST_REQUIRE_EQUAL(q.get().empty(), true);
 
-    auto new_haploid_genome_index = fwdpp::mutate_recombine(
-        new_mutations, breakpoints, 0, 1, pop.haploid_genomes, pop.mutations, q,
-        pop.neutral, pop.selected);
+    auto new_haploid_genome_index
+        = fwdpp::mutate_recombine(new_mutations, breakpoints, 0, 1, pop.haploid_genomes,
+                                  pop.mutations, q, pop.neutral, pop.selected);
 
     // The test is that the new haploid_genome DOES contain a mutation
     // at pos 1.5
@@ -95,19 +96,17 @@ BOOST_FIXTURE_TEST_CASE(test_boundary_with_recurrent_mutation,
     //Further, there must be two mutations at position 1.5, because one
     //is inherited from the second parental haploid_genome and the other is
     //a new mutation.
-    auto count_new_mutation
-        = std::count_if(pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
-                        pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
-                        [&pop](const fwdpp::uint_t key) {
-                            return pop.mutations[key].pos == 1.5;
-                        });
+    auto count_new_mutation = std::count_if(
+        pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
+        pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
+        [&pop](const fwdpp::uint_t key) { return pop.mutations[key].pos == 1.5; });
     BOOST_REQUIRE_EQUAL(count_new_mutation, 2);
 
     // But, if we switch the parent IDs,
     // then this mutation WILL NOT appear in the offspring haploid_genome
-    new_haploid_genome_index = fwdpp::mutate_recombine(new_mutations, breakpoints, 1,
-                                               0, pop.haploid_genomes, pop.mutations,
-                                               q, pop.neutral, pop.selected);
+    new_haploid_genome_index
+        = fwdpp::mutate_recombine(new_mutations, breakpoints, 1, 0, pop.haploid_genomes,
+                                  pop.mutations, q, pop.neutral, pop.selected);
 
     // The test is that the new haploid_genome DOES contain a mutation
     // at pos 1.5
@@ -116,15 +115,13 @@ BOOST_FIXTURE_TEST_CASE(test_boundary_with_recurrent_mutation,
                     pop.mutations.size() - 1);
     is_end = (itr == pop.haploid_genomes[new_haploid_genome_index].mutations.end());
     BOOST_REQUIRE_EQUAL(is_end, false);
-    // However, it must only contain one copy, and it must be the 
+    // However, it must only contain one copy, and it must be the
     // new mutation, whose xtra field is set to 9
     BOOST_REQUIRE_EQUAL(static_cast<int>(pop.mutations[*itr].xtra), 9);
-    count_new_mutation
-        = std::count_if(pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
-                        pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
-                        [&pop](const fwdpp::uint_t key) {
-                            return pop.mutations[key].pos == 1.5;
-                        });
+    count_new_mutation = std::count_if(
+        pop.haploid_genomes[new_haploid_genome_index].mutations.begin(),
+        pop.haploid_genomes[new_haploid_genome_index].mutations.end(),
+        [&pop](const fwdpp::uint_t key) { return pop.mutations[key].pos == 1.5; });
     BOOST_REQUIRE_EQUAL(count_new_mutation, 1);
 }
 
