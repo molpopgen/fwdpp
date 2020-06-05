44b9df69a9f53c68c666792c220d1da0ad27bae8
molpopgen <krthornt@uci.edu>
2020-06-05 14:08:10 -0700

    Remove preprocessor guard around template type trait variables.

--- fwdpp/type_traits.hpp
+++ fwdpp/type_traits.hpp
@@ -12,17 +12,16 @@ namespace fwdpp
     namespace traits
     {
         //! Wraps a static constant allowing a test that T is a haploid_genome
-        template <typename T, typename = void>
-        struct is_haploid_genome : std::false_type
+        template <typename T, typename = void> struct is_haploid_genome : std::false_type
         {
         };
 
         template <typename T>
-        struct is_haploid_genome<T, typename traits::internal::void_t<
-                                        typename T::mutation_container>::type>
+        struct is_haploid_genome<
+            T, typename traits::internal::void_t<typename T::mutation_container>::type>
             : std::integral_constant<
-                  bool, std::is_integral<
-                            typename T::mutation_container::value_type>::value>
+                  bool,
+                  std::is_integral<typename T::mutation_container::value_type>::value>
         {
         };
 
@@ -33,14 +32,13 @@ namespace fwdpp
         //! Wraps a static constant allowing a test that T is a mutation
         template <typename T>
         struct is_mutation
-            : std::integral_constant<
-                  bool, std::is_base_of<fwdpp::mutation_base, T>::value>
+            : std::integral_constant<bool,
+                                     std::is_base_of<fwdpp::mutation_base, T>::value>
         {
         };
 
         //! Convenience wrapper for fwdpp::traits::is_mutation<T>::Type.
-        template <typename T>
-        using is_mutation_t = typename is_mutation<T>::type;
+        template <typename T> using is_mutation_t = typename is_mutation<T>::type;
     } // namespace traits
 } // namespace fwdpp
 
@@ -51,16 +49,14 @@ namespace fwdpp
     namespace traits
     {
         //! Wraps a static constant allowing a test that T is a diploid
-        template <typename T>
-        using is_diploid = traits::internal::is_diploid<T>;
+        template <typename T> using is_diploid = traits::internal::is_diploid<T>;
 
         //! Wraps a static constant allowing a test that T is a custom diploid
         template <typename T>
         using is_custom_diploid = traits::internal::is_custom_diploid<T>;
 
         //! Convenience wrapper for fwdpp::traits::is_diploid<T>::type
-        template <typename T>
-        using is_diploid_t = typename is_diploid<T>::type;
+        template <typename T> using is_diploid_t = typename is_diploid<T>::type;
 
         //! Convenience wrapper for fwdpp::traits::is_custom_diploid<T>::type
         template <typename T>
@@ -86,21 +82,19 @@ namespace fwdpp
          * Wraps a static constant to test that recmodel_t is a valid
          * recombination function/policy
          */
-        template <typename recmodel_t, typename diploid_t,
-                  typename haploid_genome_t, typename mcont_t>
-        using is_rec_model
-            = traits::internal::is_rec_model<recmodel_t, diploid_t,
-                                             haploid_genome_t, mcont_t>;
+        template <typename recmodel_t, typename diploid_t, typename haploid_genome_t,
+                  typename mcont_t>
+        using is_rec_model = traits::internal::is_rec_model<recmodel_t, diploid_t,
+                                                            haploid_genome_t, mcont_t>;
 
         /*!
          * Convenience wrapper for
          * fwdpp::traits::is_rec_model<recmodel_t,haploid_genome_c,mcont_t>::type
          */
-        template <typename recmodel_t, typename diploid_t,
-                  typename haploid_genome_t, typename mcont_t>
-        using is_rec_model_t =
-            typename is_rec_model<recmodel_t, diploid_t, haploid_genome_t,
-                                  mcont_t>::type;
+        template <typename recmodel_t, typename diploid_t, typename haploid_genome_t,
+                  typename mcont_t>
+        using is_rec_model_t = typename is_rec_model<recmodel_t, diploid_t,
+                                                     haploid_genome_t, mcont_t>::type;
 
         /*!
          * Defines a struct with a single member typedef called type.
@@ -111,32 +105,27 @@ namespace fwdpp
          * mcont_t)>
          */
         template <typename dipvector_t, typename gcont_t, typename mcont_t>
-        using fitness_fxn
-            = traits::internal::fitness_fxn<dipvector_t, gcont_t, mcont_t>;
+        using fitness_fxn = traits::internal::fitness_fxn<dipvector_t, gcont_t, mcont_t>;
 
         //! Convenience wrapper for
         //! fitness_fxn<dipvector_t,gcont_t,mcont_t>::type
         template <typename dipvector_t, typename gcont_t, typename mcont_t>
-        using fitness_fxn_t =
-            typename fitness_fxn<dipvector_t, gcont_t, mcont_t>::type;
+        using fitness_fxn_t = typename fitness_fxn<dipvector_t, gcont_t, mcont_t>::type;
 
         /*!
          * Wrap a static constant if ff is a valid fitness function
          * whose argument types are const references to the other
          * three template parameters
          */
-        template <typename ff, typename dipvector_t, typename gcont_t,
-                  typename mcont_t>
+        template <typename ff, typename dipvector_t, typename gcont_t, typename mcont_t>
         using is_fitness_fxn
-            = traits::internal::is_fitness_fxn<ff, dipvector_t, gcont_t,
-                                               mcont_t>;
+            = traits::internal::is_fitness_fxn<ff, dipvector_t, gcont_t, mcont_t>;
 
         /*!
          * Conveneince wrapper for
          * fwdpp::traits::is_fitness_fxn<ff,dipvector_t,gcont_t,mcont_t>::type
          */
-        template <typename ff, typename dipvector_t, typename gcont_t,
-                  typename mcont_t>
+        template <typename ff, typename dipvector_t, typename gcont_t, typename mcont_t>
         using is_fitness_fxn_t =
             typename is_fitness_fxn<ff, dipvector_t, gcont_t, mcont_t>::type;
 
@@ -157,8 +146,7 @@ namespace fwdpp
          */
         // clang-format on
         template <typename mcont_t>
-        using mutation_model =
-            typename traits::internal::mutation_model<mcont_t>::type;
+        using mutation_model = typename traits::internal::mutation_model<mcont_t>::type;
 
         // clang-format off
 		/*!
@@ -178,8 +166,8 @@ namespace fwdpp
         // clang-format on
         template <typename mcont_t, typename gcont_t>
         using mutation_model_haploid_genome =
-            typename traits::internal::mutation_model_haploid_genome<
-                mcont_t, gcont_t>::type;
+            typename traits::internal::mutation_model_haploid_genome<mcont_t,
+                                                                     gcont_t>::type;
 
         // clang-format off
 		/*!
@@ -199,29 +187,26 @@ namespace fwdpp
         // clang-format on
         template <typename diploid_t, typename mcont_t, typename gcont_t>
         using mutation_model_diploid =
-            typename traits::internal::mutation_model_diploid<
-                diploid_t, mcont_t, gcont_t>::type;
+            typename traits::internal::mutation_model_diploid<diploid_t, mcont_t,
+                                                              gcont_t>::type;
 
-/*! \defgroup Cpp14 C++14 features
- * \brief C++14 features
- */
+        /*! \defgroup Cpp14 C++14 features
+         * \brief C++14 features
+         */
 
-/* Template variables are provided for easier type trait
- * checking.  This feature requires compiling with the C++14
- * standard
- */
-#if __cplusplus >= 201402L
+        /* Template variables are provided for easier type trait
+         * checking.  This feature requires compiling with the C++14
+         * standard.
+         */
         //! \ingroup Cpp14
-        template <typename T>
-        constexpr bool is_diploid_v = is_diploid<T>::value;
+        template <typename T> constexpr bool is_diploid_v = is_diploid<T>::value;
 
         //! \ingroup Cpp14
         template <typename T>
         constexpr bool is_custom_diploid_v = is_custom_diploid<T>::value;
 
         //! \ingroup Cpp14
-        template <typename T>
-        constexpr bool is_mutation_v = is_mutation<T>::value;
+        template <typename T> constexpr bool is_mutation_v = is_mutation<T>::value;
 
         //! \ingroup Cpp14
         template <typename T>
@@ -233,18 +218,15 @@ namespace fwdpp
             = is_mutation_model<mmodel_t, mcont_t, gcont_t>::value;
 
         //! \ingroup Cpp14
-        template <typename recmodel_t, typename diploid_t,
-                  typename haploid_genome_t, typename mcont_t>
+        template <typename recmodel_t, typename diploid_t, typename haploid_genome_t,
+                  typename mcont_t>
         constexpr bool is_rec_model_v
-            = is_rec_model<recmodel_t, diploid_t, haploid_genome_t,
-                           mcont_t>::value;
+            = is_rec_model<recmodel_t, diploid_t, haploid_genome_t, mcont_t>::value;
 
         //! \ingroup Cpp14
-        template <typename ff, typename dipvector_t, typename gcont_t,
-                  typename mcont_t>
+        template <typename ff, typename dipvector_t, typename gcont_t, typename mcont_t>
         constexpr bool is_fitness_fxn_v
             = is_fitness_fxn<ff, dipvector_t, gcont_t, mcont_t>::value;
-#endif
     } // namespace traits
 } // namespace fwdpp
 #endif
