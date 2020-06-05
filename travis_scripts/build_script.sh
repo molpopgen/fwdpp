a049d6add2d42f7e868a4b3e6ae8e71a406ec81f
molpopgen <krthornt@uci.edu>
2020-06-05 12:09:46 -0700

    Refactor Travis/Linux build matrix:
    * Update to bionic
    * Test cpp14 and cpp17 where appropriate

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -9,7 +9,12 @@ then
     export PATH="$HOME/miniconda/bin:$PATH"
     CXX=g++ CC=gcc CPPFLAGS=-I$HOME/miniconda/include LDFLAGS="-L$HOME/miniconda/lib -Wl,-rpath,$HOME/miniconda/lib" ./configure --prefix=$HOME && make -j 3 &&  make install
 else
-    ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
+    if [ "$CXXSTANDARD" == "c++17" ];
+    then
+        ./configure --enable-cpp17 CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
+    else
+        ./configure CXXFLAGS="$CXXFLAGS" --prefix=$HOME && make -j 3 && make install
+    fi
 fi
 
 fwdppConfig --version
