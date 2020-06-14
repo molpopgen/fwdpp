c652e8ad3a66755010d295c49259d712fdc73cb3
molpopgen <krthornt@uci.edu>
2020-06-14 13:43:40 -0700

    Add namespace fwdpp::enum_bitflags.

---
+++ fwdpp/util/enum_bitflags.hpp
@@ -0,0 +1,84 @@
+#ifndef FWDPP_UTIL_ENUM_BITFLAGS_HPP
+#define FWDPP_UTIL_ENUM_BITFLAGS_HPP
+
+#include <type_traits>
+
+namespace fwdpp
+{
+    namespace enum_bitflags
+    {
+        template <typename E>
+        auto
+        operator|(E a, E b)
+        {
+            using T = std::underlying_type_t<E>;
+            static_assert(std::is_integral<T>::value,
+                          "underlying type must be integral");
+            static_assert(std::is_unsigned<T>::value,
+                          "underlying type must be unsigned");
+            return static_cast<E>(static_cast<T>(a) | static_cast<T>(b));
+        }
+
+        template <typename E>
+        auto&
+        operator|=(E& a, E b)
+        {
+            a = a | b;
+            return a;
+        }
+
+        template <typename E>
+        auto
+        operator&(E a, E b)
+        {
+            using T = std::underlying_type_t<E>;
+            static_assert(std::is_integral<T>::value,
+                          "underlying type must be integral");
+            static_assert(std::is_unsigned<T>::value,
+                          "underlying type must be unsigned");
+            return static_cast<E>(static_cast<T>(a) & static_cast<T>(b));
+        }
+
+        template <typename E>
+        auto&
+        operator&=(E& a, E b)
+        {
+            a = a & b;
+            return a;
+        }
+
+        template <typename E>
+        auto
+        operator^(E a, E b)
+        {
+            using T = std::underlying_type_t<E>;
+            static_assert(std::is_integral<T>::value,
+                          "underlying type must be integral");
+            static_assert(std::is_unsigned<T>::value,
+                          "underlying type must be unsigned");
+            return static_cast<E>(static_cast<T>(a) ^ static_cast<T>(b));
+        }
+
+        template <typename E>
+        auto&
+        operator^=(E& a, E b)
+        {
+            a = a ^ b;
+            return a;
+        }
+
+        template <typename E>
+        auto
+        operator~(E a)
+        {
+            using T = std::underlying_type_t<E>;
+            static_assert(std::is_integral<T>::value,
+                          "underlying type must be integral");
+            static_assert(std::is_unsigned<T>::value,
+                          "underlying type must be unsigned");
+            return static_cast<E>(~static_cast<T>(a));
+        }
+    }
+}
+
+#endif
---
+++ testsuite/unit/test_enum_bitflags.cc
@@ -0,0 +1,90 @@
+#include <type_traits>
+#include <boost/test/unit_test.hpp>
+#include <fwdpp/util/enum_bitflags.hpp>
+
+enum class E : unsigned
+{
+    a = 1 << 0,
+    b = 1 << 1
+};
+
+BOOST_AUTO_TEST_SUITE(test_enum_bitflags)
+
+BOOST_AUTO_TEST_CASE(test_zero_initialization)
+{
+    E e{};
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_OR)
+{
+    using fwdpp::enum_bitflags::operator|;
+    E e{E::a};
+    e = e | E::b;
+    unsigned x = 1 << 0;
+    x |= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_OR_EQ)
+{
+    using fwdpp::enum_bitflags::operator|=;
+    E e{E::a};
+    e |= E::b;
+    unsigned x = 1 << 0;
+    x |= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_XOR)
+{
+    using fwdpp::enum_bitflags::operator^;
+    E e{E::a};
+    e = e ^ E::b;
+    unsigned x = 1 << 0;
+    x ^= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_XOR_EQ)
+{
+    using fwdpp::enum_bitflags::operator^=;
+    E e{E::a};
+    e ^= E::b;
+    unsigned x = 1 << 0;
+    x ^= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_AND)
+{
+    using fwdpp::enum_bitflags::operator&;
+    E e{E::a};
+    e = e & E::b;
+    unsigned x = 1 << 0;
+    x &= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_AND_EQ)
+{
+    using fwdpp::enum_bitflags::operator&=;
+    E e{E::a};
+    e &= E::b;
+    unsigned x = 1 << 0;
+    x &= 1 << 1;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_CASE(test_bitwise_NEGATE)
+{
+    using fwdpp::enum_bitflags::operator~;
+    E e{E::b};
+    e = ~e;
+    unsigned x = 1 << 1;
+    x = ~x;
+    BOOST_REQUIRE_EQUAL(static_cast<std::underlying_type_t<E>>(e), x);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
