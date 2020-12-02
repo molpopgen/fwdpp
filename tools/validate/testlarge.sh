33d382dbee908b5a8bfcc41f414ed183e262e960
molpopgen <krthornt@uci.edu>
2020-12-02 09:42:05 -0800

    remove outdated tools/

--- tools/validate/testlarge.sh
+++
@@ -1,9 +0,0 @@
-#!bash
-
-parallel --jobs 4 "../../examples/diploid_ind 5000 500 500 50000 100 25 {} > testout.large.{}.txt" ::: $RANDOM $RANDOM $RANDOM $RANDOM
-
-cat testout.large.*.txt | msstats > testout.large.stats
-
-ms 100 1000 -t 500 -r 500 1000 | msstats > testout.large.ms.stats
-
-Rscript plot.R testout.large.stats testout.large.ms.stats testout.large.pdf
--- tools/validate/testsmall.sh
+++
@@ -1,9 +0,0 @@
-#!bash
-
-parallel --jobs 4 "../../examples/diploid_ind 1000 250 250 10000 10 125 {} > testout.small.{}.txt" ::: $RANDOM $RANDOM $RANDOM $RANDOM
-
-cat testout.small.*.txt | msstats > testout.small.stats
-
-ms 10 500 -t 250 -r 250 1000 | msstats > testout.small.ms.stats
-
-Rscript plot.R testout.small.stats testout.small.ms.stats testout.small.pdf
