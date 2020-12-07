7435f3c1bc515f0d1a5bb573c2f5c8df65ff9074
molpopgen <krthornt@uci.edu>
2020-12-07 07:14:44 -0800

    Add data round trip test thru nested_forward_lists.

--- testsuite/unit/test_nested_forward_lists.cc
+++ testsuite/unit/test_nested_forward_lists.cc
@@ -48,5 +48,31 @@ BOOST_AUTO_TEST_CASE(test_unsigned_overflow)
         fwdpp::nested_forward_lists_overflow);
 }
 
+BOOST_AUTO_TEST_CASE(test_data_round_trip)
+{
+    using buffer_t
+        = fwdpp::nested_forward_lists<int, std::uint8_t,
+                                      std::numeric_limits<std::uint8_t>::max()>;
+    buffer_t buffer;
+    buffer.reset(1);
+    for (int i = 0; i < 3; ++i)
+        {
+            buffer.extend(0, 2 * i);
+        }
+    std::vector<int> output;
+    auto x = buffer.head(0);
+    while (x != buffer_t::null)
+        {
+            auto val = buffer.fetch(x);
+            output.push_back(val);
+            x = buffer.next(x);
+        }
+    BOOST_REQUIRE_EQUAL(output.size(), 3);
+    for (int i = 0; i < 3; ++i)
+        {
+            BOOST_REQUIRE_EQUAL(output[i], 2 * i);
+        }
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
